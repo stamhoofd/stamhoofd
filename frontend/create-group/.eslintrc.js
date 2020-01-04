e139e365d4f4a8eb42fbe7a4bbd9dbad3d380132
Simon Backx <hi@simonbackx.com>
2020-01-04 14:49:03 +0100

    Experimenting with layout and scss structure

--- frontend/create-group/.eslintrc.js
+++ frontend/create-group/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
--- frontend/create-group/src/App.vue
+++ frontend/create-group/src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div id="app">
-    <header>
-        <h1>Jouw vereniging aansluiten</h1>
-    </header>
-  </div>
+	<div id="app" class="steps-layout">
+		<header>
+			<div>
+                <div class="left">
+                    <transition name="move" mode="out-in">
+                        <a href="/" id="logo" alt="Stamhoofd" v-if="!showNext" v-on:click.prevent="showNext = true;"></a>
+                        <div v-else v-on:click.prevent="showNext = false;">Terug</div>
+                    </transition>
+                </div>
+				<div class="center">
+                    <span class="style-caption">Stap 1 / 3</span>
+                </div>
+				<div class="right">
+                    Right
+                </div>
+			</div>
+		</header>
+		<main>
+            <article>
+                <h1>Jouw vereniging aansluiten</h1>
+                <p>Blablabla</p>
+            </article>
+		</main>
+	</div>
 </template>
 
 <script lang="ts">
@@ -11,22 +30,28 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: {
+		HelloWorld,
+	},
 })
-export default class App extends Vue {}
+
+export default class App extends Vue {
+    // Data property
+    showNext: boolean = false;
+}
 </script>
 
 <style lang="scss">
 @import "~@shared/scss/fonts.scss";
+@import "~@shared/scss/text-styles.scss";
+@import "~@shared/scss/body.scss";
+@import "~@shared/scss/layout/steps.scss";
+@import "~@shared/scss/components/logo.scss";
 
-#app {
-  font-family: 'Metropolis', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $dark-color;
-  margin-top: 60px;
+body {
+	background: $color-white-shade;
 }
+
+
+
 </style>
