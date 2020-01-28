5435558a8783d25f266a1a333fdd12921f3a1ccf
Simon Backx <hi@simonbackx.com>
2020-01-28 08:14:20 +0100

    Added navigation controller

--- frontend/create-group/src/components/EditGroup.vue
+++ frontend/create-group/src/components/EditGroup.vue
@@ -3,48 +3,59 @@
         <h1>Nieuwe groep toevoegen</h1>
         <SegmentedControl :items="tabs" v-model="tab"></SegmentedControl>
 
-        <button class="button primary" v-on:click="$emit('dismiss')" id="save-button">Opslaan</button>
+        <button class="button primary" v-on:click="next" id="save-button">
+            Volgende
+        </button>
     </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-import SegmentedControl from '@shared/components/inputs/SegmentedControl.vue';
+import SegmentedControl from "@shared/components/inputs/SegmentedControl.vue";
+import { ComponentWithProperties } from "stamhoofd-shared/classes/ComponentWithProperties";
+import EditGroupDetail from "./EditGroupDetail.vue";
 
 @Component({
-  props: {
-      text: String
-  },
-  components: {
-      SegmentedControl,
-  }
+    props: {
+        text: String
+    },
+    components: {
+        SegmentedControl
+    }
 })
 export default class EditGroup extends Vue {
-    tabs = ["Algemeen", "Inschrijven", "Toegang", "Test"]
-    tab = this.tabs[0]
+    tabs = ["Algemeen", "Inschrijven", "Toegang", "Test"];
+    tab = this.tabs[0];
+
+    next() {
+        this.$emit(
+            "push",
+            new ComponentWithProperties(EditGroupDetail, { text: "test" })
+        );
+    }
 }
 </script>
 
 <style scoped lang="scss">
-    // This should be @use, but this won't work with webpack for an unknown reason? #bullshit
-    @use '~@shared/scss/layout/split-inputs.scss';
-    @use '~@shared/scss/base/text-styles.scss';
-    @use '~@shared/scss/components/inputs.scss';
-    @use '~@shared/scss/components/buttons.scss';
+// This should be @use, but this won't work with webpack for an unknown reason? #bullshit
+@use '~@shared/scss/layout/split-inputs.scss';
+@use '~@shared/scss/base/text-styles.scss';
+@use '~@shared/scss/components/inputs.scss';
+@use '~@shared/scss/components/buttons.scss';
 
-    h1 {
-        @extend .style-title-1;
-        margin-bottom: 20px;
-    }
+h1 {
+    @extend .style-title-1;
+    margin-bottom: 20px;
+}
 
-    p {
-        @extend .style-description;
-        margin: 10px 0;
-    }
+p {
+    @extend .style-description;
+    margin: 10px 0;
+}
 
-    #save-button {
-        margin-top: 30px;
-    }
+#save-button {
+    margin-top: 30px;
+}
 </style>
