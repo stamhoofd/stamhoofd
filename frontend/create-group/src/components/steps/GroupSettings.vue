fbb2ebaea72c814f75f14c00dfbb987323afe849
Simon Backx <hi@simonbackx.com>
2020-01-30 21:32:45 +0100

    Implemented model stack component poc

--- frontend/create-group/src/components/steps/GroupSettings.vue
+++ frontend/create-group/src/components/steps/GroupSettings.vue
@@ -2,12 +2,14 @@
     <article>
         <h1>Inschrijven & groepen</h1>
 
-        <hr>
+        <hr />
         <h2>Samenstelling</h2>
 
         <div class="split-inputs">
             <div>
-                <label class="style-label" for="organization-name">Soort vereniging</label>
+                <label class="style-label" for="organization-name"
+                    >Soort vereniging</label
+                >
                 <select class="input" ref="firstInput">
                     <option>Scoutsgroep</option>
                     <option>Chirogroep</option>
@@ -15,15 +17,16 @@
             </div>
 
             <div>
-                <label class="style-label" for="organization-name">Koepelorganisatie</label>
+                <label class="style-label" for="organization-name"
+                    >Koepelorganisatie</label
+                >
                 <select class="input">
                     <option>Scouts & Gidsen Vlaanderen</option>
                     <option>FOS</option>
                 </select>
-                
             </div>
         </div>
-        
+
         <label class="style-label">Jongens en meisjes</label>
         <RadioGroup>
             <Radio>Gemengd</Radio>
@@ -32,39 +35,59 @@
             <Radio>Enkel meisjes</Radio>
         </RadioGroup>
 
-
         <label class="style-label">Extra onderverdelingen</label>
         <Checkbox>Woutlopers</Checkbox>
 
-        <hr>
+        <hr />
         <h2>Werkjaar</h2>
 
         <div class="split-inputs">
             <div>
-                <label class="style-label" for="organization-name">Start werkjaar</label>
+                <label class="style-label" for="organization-name"
+                    >Start werkjaar</label
+                >
                 <select class="input">
-                    <option v-for="(month, index) in months" :value="index" :key="index">{{ month }}</option>
+                    <option
+                        v-for="(month, index) in months"
+                        :value="index"
+                        :key="index"
+                        >{{ month }}</option
+                    >
                 </select>
             </div>
 
             <div>
-                <label class="style-label" for="organization-name">Inschrijvingsperiode</label>
+                <label class="style-label" for="organization-name"
+                    >Inschrijvingsperiode</label
+                >
                 <div class="mixed-input">
                     <select>
-                        <option v-for="(month, index) in months" :value="index" :key="index">{{ month }}</option>
+                        <option
+                            v-for="(month, index) in months"
+                            :value="index"
+                            :key="index"
+                            >{{ month }}</option
+                        >
                     </select>
                     <span>tot</span>
                     <select>
-                        <option v-for="(month, index) in months" :value="index" :key="index">{{ month }}</option>
+                        <option
+                            v-for="(month, index) in months"
+                            :value="index"
+                            :key="index"
+                            >{{ month }}</option
+                        >
                     </select>
                 </div>
             </div>
-           
         </div>
 
-        <hr>
+        <hr />
         <h2>Prijzen</h2>
-        <p>Je kan uitzonderingen voor bepaalde groepen toevoegen in de volgende stap.</p>
+        <p>
+            Je kan uitzonderingen voor bepaalde groepen toevoegen in de volgende
+            stap.
+        </p>
 
         <div class="split-inputs">
             <div>
@@ -78,76 +101,116 @@
             </div>
         </div>
 
-        <Checkbox>Verminder het lidgeld voor leden met financiële moeilijkheden</Checkbox>
+        <Checkbox
+            >Verminder het lidgeld voor leden met financiële
+            moeilijkheden</Checkbox
+        >
 
         <label class="style-label">Verminderd lidgeld</label>
         <PriceInput></PriceInput>
 
-        <button class="button primary" v-on:click="$emit('next')" id="next-button">Verder</button>
-        
+        <button
+            class="button primary"
+            v-on:click="$emit('next')"
+            id="next-button"
+        >
+            Verder
+        </button>
+
+        <button class="button secundary" @click="editGroup">Test</button>
     </article>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import Slider from '@shared/components/inputs/Slider.vue';
-import Checkbox from '@shared/components/inputs/Checkbox.vue';
-import Radio from '@shared/components/inputs/Radio.vue';
-import RadioGroup from '@shared/components/inputs/RadioGroup.vue';
-import PriceInput from '@shared/components/inputs/PriceInput.vue';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import Slider from "@shared/components/inputs/Slider.vue";
+import Checkbox from "@shared/components/inputs/Checkbox.vue";
+import Radio from "@shared/components/inputs/Radio.vue";
+import RadioGroup from "@shared/components/inputs/RadioGroup.vue";
+import PriceInput from "@shared/components/inputs/PriceInput.vue";
+
+import { eventBus } from "stamhoofd-shared/classes/event-bus/EventBus";
+import { PresentComponentEvent } from "stamhoofd-shared/classes/PresentComponentEvent";
+import EditGroup from "../EditGroup.vue";
+import { ComponentWithProperties } from "stamhoofd-shared/classes/ComponentWithProperties";
+import NavigationController from "stamhoofd-shared/components/layout/NavigationController.vue";
 
 @Component({
-  // All component options are allowed in here
-  components: {
-      Slider,
-      Checkbox,
-      Radio,
-      RadioGroup,
-      PriceInput
-  }
+    // All component options are allowed in here
+    components: {
+        Slider,
+        Checkbox,
+        Radio,
+        RadioGroup,
+        PriceInput
+    }
 })
 export default class General extends Vue {
-    months: string[] = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"];
+    months: string[] = [
+        "Januari",
+        "Februari",
+        "Maart",
+        "April",
+        "Mei",
+        "Juni",
+        "Juli",
+        "Augustus",
+        "September",
+        "Oktober",
+        "November",
+        "December"
+    ];
 
     mounted() {
         // Focus first input automatically
         let input = this.$refs.firstInput as HTMLElement;
         input.focus();
     }
+
+    editGroup() {
+        eventBus.send(
+            "show-modal",
+            new ComponentWithProperties(NavigationController, {
+                root: new ComponentWithProperties(EditGroup, {
+                    text: "Custom text"
+                })
+            })
+        );
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-    // This should be @use, but this won't work with webpack for an unknown reason? #bullshit
-    @use '~@shared/scss/layout/split-inputs.scss';
-    @use '~@shared/scss/base/text-styles.scss';
-    @use '~@shared/scss/components/inputs.scss';
-    @use '~@shared/scss/components/mixed-input.scss';
-    @use '~@shared/scss/components/buttons.scss';
-    @use '~@shared/scss/elements/hr.scss';
+// This should be @use, but this won't work with webpack for an unknown reason? #bullshit
+@use '~@shared/scss/layout/split-inputs.scss';
+@use '~@shared/scss/base/text-styles.scss';
+@use '~@shared/scss/components/inputs.scss';
+@use '~@shared/scss/components/mixed-input.scss';
+@use '~@shared/scss/components/buttons.scss';
+@use '~@shared/scss/elements/hr.scss';
 
-    h1 {
-        @extend .style-title-1;
-        margin-bottom: 20px;
-    }
+h1 {
+    @extend .style-title-1;
+    margin-bottom: 20px;
+}
 
-    p {
-        @extend .style-description;
-        margin: 10px 0;
-    }
+p {
+    @extend .style-description;
+    margin: 10px 0;
+}
 
-    h2 {
-        @extend .style-title-2;
-        margin-bottom: 20px;
-    }
+h2 {
+    @extend .style-title-2;
+    margin-bottom: 20px;
+}
 
-    hr {
-        @extend .hr;
-        margin-top: 40px;
-    }
+hr {
+    @extend .hr;
+    margin-top: 40px;
+}
 
-    #next-button {
-        margin-top: 30px;
-    }
+#next-button {
+    margin-top: 30px;
+}
 </style>
---
+++ frontend/shared/components/layout/ModalStackComponent.vue
@@ -0,0 +1,211 @@
+<template>
+    <transition-group
+        tag="div"
+        name="fade"
+        v-on:after-enter="afterEnter"
+        v-on:before-enter="beforeEnter"
+        class="modal-stack"
+    >
+        <div
+            class="modal"
+            v-for="component in components"
+            @click="removeAt(component.key)"
+            :key="component.key"
+            :data-hide="component.hide"
+            :modal-type="component.type"
+        >
+            <div @click.stop="">
+                <component
+                    :is="component.component"
+                    v-bind="component.properties"
+                    @dismiss="removeAt(component.key)"
+                ></component>
+            </div>
+        </div>
+    </transition-group>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { eventBus } from "../../classes/event-bus/EventBus";
+import { PresentComponentEvent } from "../../classes/PresentComponentEvent";
+import { EventBusListener } from "../../classes/event-bus/EventBusListener";
+import { ComponentWithProperties } from "../../classes/ComponentWithProperties";
+import Modal from "@shared/components/layout/Modal.vue";
+
+@Component
+export default class ModalStackComponent extends Vue {
+    components: ComponentWithProperties[] = [];
+    listener: EventBusListener | null = null;
+    counter: number = 0;
+
+    show(component: ComponentWithProperties) {
+        component.type = this.components.length <= 0 ? "normal" : "normal";
+        component.key = this.counter++;
+        this.components.push(component);
+    }
+
+    beforeEnter(insertedElement) {
+        // Manually set position fixed during animation, so we kan keep it one tick longer
+        insertedElement.setAttribute("data-extended-enter", "true");
+    }
+
+    afterEnter(insertedElement) {
+        console.log("Updated hide");
+        this.updateHidden();
+
+        // Restore scroll position
+        window.scrollTo(0, 0);
+
+        // Manually remove the fixed position
+        console.log("After enter");
+        console.log(insertedElement);
+        window.setTimeout(() => {
+            insertedElement.removeAttribute("data-extended-enter");
+        }, 200);
+    }
+
+    updateHidden() {
+        var found = false;
+        for (let index = this.components.length - 1; index >= 0; index--) {
+            const element = this.components[index];
+            if (found) {
+                element.hide = true;
+                continue;
+            }
+            if (element.type == "normal") {
+                found = true;
+            }
+            element.hide = false;
+        }
+    }
+
+    removeAt(key) {
+        console.log("Dismiss");
+
+        for (let index = this.components.length - 1; index >= 0; index--) {
+            const element = this.components[index];
+            if (element.key == key) {
+                this.components.splice(index, 1);
+                this.updateHidden();
+                return;
+            }
+        }
+
+        console.error("Not found!");
+    }
+
+    mounted() {
+        this.listener = eventBus.listen("show-modal", this.show.bind(this));
+    }
+
+    beforeDestroy() {
+        eventBus.removeListener(this.listener);
+        this.listener = null;
+        this.components = [];
+    }
+
+    get visibleComponents() {
+        return this.components.filter(component => {
+            return !component.hide;
+        });
+    }
+}
+</script>
+
+<style lang="scss">
+.modal-stack {
+    position: relative;
+}
+.modal {
+    &[modal-type="popup"] {
+        // DO NOT ADD MAX HEIGHT HERE! Always add it to the children of the navigation controllers!
+        background: rgba(black, 0.7);
+        position: fixed;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        z-index: 10000;
+        padding: 20px;
+
+        & > div {
+            max-width: 800px;
+            flex-basis: 100%;
+            background: white;
+            border-radius: 5px;
+
+            max-height: 100vh;
+            overflow-y: auto;
+            box-sizing: border-box;
+        }
+    }
+
+    &.fade-enter-active,
+    &.fade-leave-active,
+    &[data-extended-enter="true"] {
+        position: fixed;
+        transition: opacity 0.3s;
+
+        & > div {
+            transition: opacity 0.3s, transform 0.3s;
+        }
+    }
+    &.fade-enter, &.fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+        opacity: 0;
+
+        & > div {
+            transform: translate(0, 50vh);
+        }
+    }
+
+    &.fade-enter-active,
+    &.fade-leave-active {
+        z-index: 10000;
+    }
+
+    &[modal-type="normal"] {
+        padding: 0px;
+        display: block;
+        position: relative;
+        background: white;
+
+        &.fade-enter-active,
+        &.fade-leave-active,
+        &[data-extended-enter="true"] {
+            position: fixed;
+            left: 0;
+            top: 0;
+            right: 0;
+
+            // During animatoin: min height
+            min-height: 100vh;
+            min-height: calc(var(--vh, 1vh) * 100);
+        }
+
+        & > div {
+            border-radius: 0;
+            max-width: none;
+            max-height: none;
+            width: 100%;
+            height: 100%;
+
+            & > .navigation-controller {
+                width: 100% !important;
+            }
+        }
+    }
+
+    &[data-hide="true"] {
+        position: absolute;
+        z-index: -1;
+        visibility: hidden;
+        left: 0;
+        right: 0;
+        top: 0;
+    }
+}
+</style>
