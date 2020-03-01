bb57ad75ac80d612060ec66ff2ebb27e7bf0769e
Simon Backx <hi@simonbackx.com>
2020-03-01 11:44:14 +0100

    Added group to members

--- frontend/src/shared/factories/OrganizationFactory.ts
+++ frontend/src/shared/factories/OrganizationFactory.ts
@@ -27,24 +27,52 @@ export class OrganizationFactory extends Factory<Organization> {
         switch (this.options.type) {
             case "scouts":
                 groupNames = [
-                    { name: "Kapoenen", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Wouters", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Welpen", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Jonggidsen", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Gidsen", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Verkenners", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Jin", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Akabe", options: { minAge: 6, maxAge: 8 } }
+                    {
+                        name: "Kapoenen",
+                        options: { maxMembers: 50, minMembers: 30 },
+                        members: { minAge: 6, maxAge: 8 }
+                    },
+                    { name: "Wouters", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } },
+                    { name: "Welpen", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } },
+                    {
+                        name: "Jonggidsen",
+                        options: { maxMembers: 50, minMembers: 30 },
+                        members: { minAge: 6, maxAge: 8 }
+                    },
+                    { name: "Gidsen", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } },
+                    {
+                        name: "Verkenners",
+                        options: { maxMembers: 50, minMembers: 30 },
+                        members: { minAge: 6, maxAge: 8 }
+                    },
+                    { name: "Jin", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } },
+                    { name: "Akabe", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } }
                 ];
                 break;
             case "chiro":
                 groupNames = [
-                    { name: "Ribbels", options: { minAge: 6, maxAge: 8 } },
-                    { name: "Speelclub", options: { minAge: 8, maxAge: 10 } },
-                    { name: "Rakwi's", options: { minAge: 10, maxAge: 12 } },
-                    { name: "Tito's", options: { minAge: 12, maxAge: 14 } },
-                    { name: "Keti's", options: { minAge: 14, maxAge: 16 } },
-                    { name: "Aspi's", options: { minAge: 16, maxAge: 18 } }
+                    { name: "Ribbels", options: { maxMembers: 50, minMembers: 30 }, members: { minAge: 6, maxAge: 8 } },
+                    {
+                        name: "Speelclub",
+                        options: { maxMembers: 50, minMembers: 30 },
+                        members: { minAge: 8, maxAge: 10 }
+                    },
+                    {
+                        name: "Rakwi's",
+                        options: { maxMembers: 40, minMembers: 20 },
+                        members: { minAge: 10, maxAge: 12 }
+                    },
+                    {
+                        name: "Tito's",
+                        options: { maxMembers: 30, minMembers: 15 },
+                        members: { minAge: 12, maxAge: 14 }
+                    },
+                    {
+                        name: "Keti's",
+                        options: { maxMembers: 30, minMembers: 10 },
+                        members: { minAge: 14, maxAge: 16 }
+                    },
+                    { name: "Aspi's", options: { maxMembers: 30, minMembers: 10 }, members: { minAge: 16, maxAge: 18 } }
                 ];
                 break;
             default:
@@ -56,8 +84,17 @@ export class OrganizationFactory extends Factory<Organization> {
             var group = new Group();
             group.name = details.name;
             group.id = Math.floor(Math.random() * 99999999999);
-            var factory = new MemberFactory(details.options);
-            group.members = factory.createMultiple(50);
+            var factory = new MemberFactory(details.members);
+            group.members = factory.createMultiple(
+                Math.floor(
+                    1 +
+                        details.options.minMembers +
+                        Math.random() * (details.options.maxMembers - details.options.minMembers)
+                )
+            );
+            group.members.forEach(member => {
+                member.group = group;
+            });
             organization.groups.push(group);
         });
 
