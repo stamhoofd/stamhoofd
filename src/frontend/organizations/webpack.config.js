55a9a954cc97ca262bf7b6c650f8c57e70942303
Simon Backx <hi@simonbackx.com>
2020-04-21 21:06:19 +0200

    Moved frontend into src

--- src/frontend/organizations/webpack.config.js
+++ src/frontend/organizations/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 var merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-var common = require("../../webpack.config.js");
+var common = require("../../../webpack.config.js");
 
 module.exports = merge(common, {
     entry: {
