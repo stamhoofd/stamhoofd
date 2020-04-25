1b2788b9254fb8e588f7c4155e68cdb9d814a1b0
Simon Backx <hi@simonbackx.com>
2020-04-25 15:14:05 +0200

    Visual viewport api test

--- src/frontend/organizations/src/login.ts
+++ src/frontend/organizations/src/login.ts
@@ -13,33 +13,61 @@ let currentVh = document.documentElement.clientHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty("--vh", `${currentVh}px`);
 
-// We listen to the resize event
-window.addEventListener(
-    "resize",
-    () => {
-        // We execute the same script as before
-        const vh = document.documentElement.clientHeight * 0.01;
-        if (vh == currentVh) {
-            return;
-        }
-        currentVh = vh;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-    },
-    { passive: true } as EventListenerOptions
-);
 
-// We listen to the resize event
-window.addEventListener(
-    "focus",
-    () => {
-        // We execute the same script as before
-        const vh = document.documentElement.clientHeight * 0.01;
 
-        if (vh == currentVh) {
-            return;
-        }
-        currentVh = vh;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-    },
-    { passive: true } as EventListenerOptions
-);
+const w = window as any;
+if (w.visualViewport) {
+    let pendingUpdate = false;
+    const viewportHandler = (event) => {
+        if (pendingUpdate) return;
+        pendingUpdate = true;
+
+        requestAnimationFrame(() => {
+            pendingUpdate = false;
+            const viewport = event.target;
+            const height = viewport.height;
+
+            // We execute the same script as before
+            const vh = height * 0.01;
+
+            if (vh == currentVh) {
+                return;
+            }
+            currentVh = vh;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        });
+    };
+    w.visualViewport.addEventListener('scroll', viewportHandler);
+    w.visualViewport.addEventListener('resize', viewportHandler);
+} else {
+    // We listen to the resize event
+    window.addEventListener(
+        "resize",
+        () => {
+            // We execute the same script as before
+            const vh = document.documentElement.clientHeight * 0.01;
+            if (vh == currentVh) {
+                return;
+            }
+            currentVh = vh;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        },
+        { passive: true } as EventListenerOptions
+    );
+
+    // We listen to the resize event
+    window.addEventListener(
+        "focus",
+        () => {
+            // We execute the same script as before
+            const vh = document.documentElement.clientHeight * 0.01;
+
+            if (vh == currentVh) {
+                return;
+            }
+            currentVh = vh;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        },
+        { passive: true } as EventListenerOptions
+    );
+}
