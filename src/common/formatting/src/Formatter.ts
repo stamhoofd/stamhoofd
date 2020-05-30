98e91ded2abefbaaa43d2e4fb3281ce31af203da
Simon Backx <hi@simonbackx.com>
2020-05-30 11:39:52 +0200

    Fix: slug

--- src/common/formatting/src/Formatter.ts
+++ src/common/formatting/src/Formatter.ts
@@ -1,5 +1,5 @@
 export class Formatter {
     static slug(name: string): string {
-        return name.toLowerCase().replace(/[^a-z0-9]+/, "-").replace(/^-+/, "").replace(/-+$/, "")
+        return name.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-+/, "").replace(/-+$/, "")
     }
 }
