e0c2835a739e2ad52ccbf238f12467e4ae999a22
SimonBackx <hi@simonbackx.com>
2020-07-03 18:00:24 +0200

    Added missing exports

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
+// General
 export * from "./src/OrganizationMetaData"
 export * from "./src/OrganizationType"
 export * from "./src/UmbrellaOrganization"
+export * from "./src/Organization"
+export * from "./src/User"
+export * from "./src/Token"
+export * from "./src/KeychainItem"
+
+// Endpoints
+export * from "./src/endpoints/CreateOrganization"
 
 // Latest version of the structures
 export const Version = 1
