dcee5180c3bed4382ca2faa2de267cfeece91a66
SimonBackx <hi@simonbackx.com>
2020-07-03 13:09:11 +0200

    Initial project setup

---
+++ tests/jest.global.setup.ts
@@ -0,0 +1,13 @@
+import { Database } from "@stamhoofd-backend/database";
+
+import { User } from "../src/users/models/User";
+
+export default async () => {
+    await Database.delete("DELETE FROM " + User.table);
+    await Database.delete("DELETE FROM `tokens`");
+    await Database.delete("DELETE FROM `users`");
+    await Database.delete("DELETE FROM `members`");
+    await Database.delete("DELETE FROM `organizations`");
+
+    await Database.end();
+};
