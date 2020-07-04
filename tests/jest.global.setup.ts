b086bcc4b36928ad278c3790ccf8f94301723028
SimonBackx <hi@simonbackx.com>
2020-07-04 10:02:22 +0200

    Run migrations in tests

--- tests/jest.global.setup.ts
+++ tests/jest.global.setup.ts
@@ -1,8 +1,9 @@
-import { Database } from "@simonbackx/simple-database";
+import { Database, Migration } from "@simonbackx/simple-database";
 
 import { User } from "../src/models/User";
 
 export default async () => {
+    await Migration.runAll(__dirname + "/../src/migrations");
     await Database.delete("DELETE FROM " + User.table);
     await Database.delete("DELETE FROM `tokens`");
     await Database.delete("DELETE FROM `users`");
