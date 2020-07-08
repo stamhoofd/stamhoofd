e4b9221fc9aaa61b17f930ad0aade7a747f3cf5b
SimonBackx <hi@simonbackx.com>
2020-07-08 14:32:48 +0200

    Added keychain model

--- src/factories/UserFactory.ts
+++ src/factories/UserFactory.ts
@@ -3,9 +3,10 @@ import { KeyConstantsHelper, SensitivityLevel,Sodium } from '@stamhoofd/crypto';
 
 import { Organization } from "../models/Organization";
 import { User, UserWithOrganization } from "../models/User";
+import { OrganizationFactory } from './OrganizationFactory';
 
 class Options {
-    organization: Organization;
+    organization?: Organization;
     email?: string;
     password?: string;
     /**
@@ -16,6 +17,7 @@ class Options {
 
 export class UserFactory extends Factory<Options, User> {
     async create(): Promise<UserWithOrganization> {
+        const organization = this.options.organization ?? await new OrganizationFactory({}).create()
         const email = this.options.email ?? "generated-email-" + this.randomString(20) + "@domain.com";
         const password = this.options.password ?? this.randomString(20);
 
@@ -25,7 +27,7 @@ export class UserFactory extends Factory<Options, User> {
         const authSignKeyPair = await KeyConstantsHelper.getSignKeyPair(authSignKeyConstants, password)
         const authEncryptionSecretKey = await KeyConstantsHelper.getEncryptionKey(authEncryptionKeyConstants, password)
         
-        const user = await User.register(this.options.organization, email, userKeyPair.publicKey, authSignKeyPair.publicKey, await Sodium.encryptMessage(userKeyPair.privateKey, authEncryptionSecretKey), authSignKeyConstants, authEncryptionKeyConstants);
+        const user = await User.register(organization, email, userKeyPair.publicKey, authSignKeyPair.publicKey, await Sodium.encryptMessage(userKeyPair.privateKey, authEncryptionSecretKey), authSignKeyConstants, authEncryptionKeyConstants);
         if (!user) {
             throw new Error("Unexpected failure when creating user in factory");
         }
