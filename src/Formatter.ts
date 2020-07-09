83037cac4bd640b0b905f253fe995a88aeb3cbfb
SimonBackx <hi@simonbackx.com>
2020-07-09 18:49:16 +0200

    Month formatting

--- src/Formatter.ts
+++ src/Formatter.ts
@@ -2,4 +2,23 @@ export class Formatter {
     static slug(name: string): string {
         return name.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(/^-+/, "").replace(/-+$/, "")
     }
+
+    /**
+     * 1 = january
+     */
+    static month(index: number): string {
+        const monthNames = ["januari", "februari", "maart", "april", "mei", "juni",
+            "juli", "augustus", "september", "oktober", "november", "december"
+        ];
+        return monthNames[index - 1]
+    }
+
+    /**
+     * 1 januari (2020). Year only in different year
+     */
+    static date(date: Date): string {
+        const currentYear = new Date().getFullYear()
+        const year = date.getFullYear()
+        return date.getDate() + " " + this.month(date.getMonth() + 1) + (currentYear != year ? (" "+year) : "")
+    }
 }
