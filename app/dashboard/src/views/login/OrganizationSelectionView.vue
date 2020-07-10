1592eaef6387a7af9e0b3210a0bfcccf3ffbb01f
SimonBackx <hi@simonbackx.com>
2020-07-11 00:15:44 +0200

    Basic search implementation

--- app/dashboard/src/views/login/OrganizationSelectionView.vue
+++ app/dashboard/src/views/login/OrganizationSelectionView.vue
@@ -2,33 +2,49 @@
     <div class="organization-selection-view">
         <h1>Kies je vereniging</h1>
         <p>Selecteer de vereniging waar je wilt inloggen.</p>
-        <input class="input search" placeholder="Zoek op postcode of naam">
+        <input v-model="query" class="input search" placeholder="Zoek op postcode of naam">
 
         <Spinner v-if="loading" class="gray center" />
-
-        <button class="search-result" @click="loginOrganization()">
-            <h1>Scouts Prins Boudewijn</h1>
-            <p>Groene wegel 2, 9230 Wetteren</p>
-        </button>
-
-        <button class="search-result">
-            <h1>Scouts Prins Boudewijn</h1>
-            <p>Groene wegel 2, 9230 Wetteren</p>
-        </button>
-
-        <button class="search-result">
-            <h1>Scouts Prins Boudewijn</h1>
-            <p>Groene wegel 2, 9230 Wetteren</p>
-        </button>
+        <template v-else>
+            <button v-for="organization in filteredResults" :key="organization.id" class="search-result" @click="loginOrganization()">
+                <h1>{{ organization.name }}</h1>
+                <p>{{ organization.address }}</p>
+            </button>
+        </template>
     </div>
 </template>
 
 <script lang="ts">
+import { ArrayDecoder, Decoder } from '@simonbackx/simple-encoding';
 import { ComponentWithProperties,NavigationController,NavigationMixin } from "@simonbackx/vue-app-navigation";
 import Spinner from "@stamhoofd/components/src/Spinner.vue";
+import { NetworkManager,SessionManager } from '@stamhoofd/networking';
+import { Organization } from '@stamhoofd/structures';
 import { Component, Mixins } from "vue-property-decorator";
 
 import LoginView from './LoginView.vue';
+
+const throttle = (func, limit) => {
+    let lastFunc;
+    let lastRan;
+    return function() {
+        const context = this;
+        // eslint-disable-next-line prefer-rest-params
+        const args = arguments;
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if (Date.now() - lastRan >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+};
 
 // The header component detects if the user scrolled past the header position and adds a background gradient in an animation
 @Component({
@@ -38,9 +54,62 @@ import LoginView from './LoginView.vue';
 })
 export default class OrganizationSelectionView extends Mixins(NavigationMixin){
     loading = true;
+    q = ""
+    results: Organization[] = []
+
+    get query() {
+        return this.q
+    }
+
+    set query(query: string) {
+        this.q = query
+
+        // update
+        this.startUpdateResults();
+    }
 
     mounted() {
         console.log("mounted organization selection")
+    }
+
+    throttleUpdateResults = throttle(this.updateResults.bind(this), 1000);
+
+    get defaultOrganizations() {
+        return SessionManager.availableSessions().map(s => s.organization)
+    }
+
+    get filteredResults() {
+        if (this.query.length == 0) {
+            return this.defaultOrganizations
+        }
+        return this.results
+    }
+
+    startUpdateResults() {
+        this.loading = true
+        this.throttleUpdateResults()
+    }
+
+    updateResults() {
+        if (this.query.length == 0) {
+            this.results = []
+            this.loading = false
+            return
+        }
+        this.loading = true
+        NetworkManager.server.request({
+            method: "GET",
+            path: "/organizations/search",
+            query: {query: this.query },
+            decoder: new ArrayDecoder(Organization as Decoder<Organization>)
+        }).then((response) => {
+            this.results = response.data
+        }).catch(e => {
+            console.error(e)
+            this.results = []
+        }).finally(() => {
+            this.loading = false
+        })
     }
 
     loginOrganization() {
