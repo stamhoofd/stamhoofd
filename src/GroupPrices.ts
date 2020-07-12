2f67580673f93596ab1873acf3b699fa1128ea49
Simon Backx <hi@simonbackx.com>
2020-07-12 16:04:02 +0200

    GroupPrices need an id to be patchable

--- src/GroupPrices.ts
+++ src/GroupPrices.ts
@@ -1,9 +1,13 @@
-import { AutoEncoder, DateDecoder,field, IntegerDecoder } from '@simonbackx/simple-encoding';
+import { AutoEncoder, DateDecoder,field, IntegerDecoder, StringDecoder } from '@simonbackx/simple-encoding';
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * A group can have multiple prices, stored in an array. The pricing with the highest date or index is applied.
  */
 export class GroupPrices extends AutoEncoder {
+    @field({ decoder: StringDecoder, version: 2, defaultValue: () => uuidv4() })
+    id: string;
+
     @field({ decoder: DateDecoder, nullable: true })
     startDate: Date | null = null
 
