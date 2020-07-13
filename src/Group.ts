5abde46def2d377057a5647cf53cf41cf9faaf22
Simon Backx <hi@simonbackx.com>
2020-07-13 18:53:55 +0200

    Reverse sorting

--- src/Group.ts
+++ src/Group.ts
@@ -10,32 +10,32 @@ export class Group extends AutoEncoder {
     @field({ decoder: GroupSettings })
     settings: GroupSettings
 
-    static defaultSort(a: Group, b: Group) {
+    static defaultSort(this: unknown, a: Group, b: Group) {
         if (a.settings.minBirthYear && !b.settings.minBirthYear) {
-            return 1
+            return -1
         }
         if (b.settings.minBirthYear && !a.settings.minBirthYear) {
-            return -1
+            return 1
         }
         if (!b.settings.minBirthYear && !a.settings.minBirthYear) {
             // name
             return Group.nameSort(a, b)
         }
         if (a.settings.minBirthYear! > b.settings.minBirthYear!) {
-            return 1
+            return -1
         }
         if (a.settings.minBirthYear! < b.settings.minBirthYear!) {
-            return -1
+            return 1
         }
         return Group.nameSort(a, b)
     }
 
-    static nameSort(a: Group, b: Group) {
+    static nameSort(this: unknown, a: Group, b: Group) {
         if (a.settings.name.toLowerCase() < b.settings.name.toLowerCase()) {
-            return 1
+            return -1
         }
         if (a.settings.name.toLowerCase() > b.settings.name.toLowerCase()) {
-            return -1
+            return 1
         }
         return 0
     }
