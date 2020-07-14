f818945d38d0327a4a7c59ada3d86615ecff74bc
Simon Backx <hi@simonbackx.com>
2020-07-14 19:29:43 +0200

    Update to simple errors and peer dependency

--- src/KeychainItemHelper.ts
+++ src/KeychainItemHelper.ts
@@ -1,4 +1,4 @@
-import { DecodingError } from "@simonbackx/simple-encoding"
+import { SimpleError } from "@simonbackx/simple-errors"
 import { KeychainItem } from "@stamhoofd/structures"
 
 import { Sodium } from "./Sodium"
@@ -9,7 +9,7 @@ export class KeychainItemHelper {
         const pk = Buffer.from(item.publicKey, "base64")
 
         if (pk.length != await Sodium.getBoxPublicKeyBytes()) {
-            throw new DecodingError({
+            throw new SimpleError({
                 code: "invalid_field",
                 message: "The length of the public key is not valid",
                 field: "publicKey"
@@ -18,7 +18,7 @@ export class KeychainItemHelper {
         const esk = Buffer.from(item.encryptedPrivateKey, "base64")
         const expectedBytes = await Sodium.getBoxEncryptedPrivateKeyBytes()
         if (esk.length != expectedBytes) {
-            throw new DecodingError({
+            throw new SimpleError({
                 code: "invalid_field",
                 message: "The length of the encrypted private key is not valid. Expected " + expectedBytes + ", received " + esk.length,
                 field: "encryptedPrivateKey"
