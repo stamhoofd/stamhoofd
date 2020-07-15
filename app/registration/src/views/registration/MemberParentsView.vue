1b7297a6a56b345f74680967dc85844c99615506
Simon Backx <hi@simonbackx.com>
2020-07-15 23:43:23 +0200

    Basic add parent view

--- app/registration/src/views/registration/MemberParentsView.vue
+++ app/registration/src/views/registration/MemberParentsView.vue
@@ -9,11 +9,21 @@
                 Ouders van {{ member.firstName }}
             </h1>
 
-            <p>Voeg alle ouders van {{ member.firstName }} toe. Deze kunnen we contacteren in noodgevallen, maar kunnen ook de gegevens tijdens het jaar wijzigen.</p>
-        </main>
+            <p v-if="member.age <= 18">Voeg alle ouders van {{ member.firstName }} toe. Deze kunnen we contacteren in noodgevallen, maar kunnen ook de gegevens tijdens het jaar wijzigen.</p>
+            <p v-else>Voeg alle ouders van {{ member.firstName }} toe (of enkel die van de hoofdverblijfplaats). Deze kunnen we contacteren in noodgevallen.</p>
 
+            <p class="warning-box" v-if="member.parents.length == 0">Voeg alle ouders toe met de knop onderaan.</p>
+            <STList>
+                <STListItem v-for="parent in member.parents" :key="parent.id" :selectable="true" @click="editParent(parent)">
+                    {{ parent.firstName }} {{ parent.lastName }}
+                </STListItem>
+            </STList>
+        </main>
         <STToolbar>
-            <button  slot="right" class="button primary" @click="goNext">
+            <button  slot="right" class="button primary" @click="addParent">
+                <span class="icon white add"/>Ouder toevoegen
+            </button>
+            <button  slot="right" class="button secundary" @click="goNext">
                 Volgende
             </button>
         </STToolbar>
@@ -24,28 +34,42 @@
 import { isSimpleError, isSimpleErrors, SimpleError, SimpleErrors } from '@simonbackx/simple-errors';
 import { Server } from "@simonbackx/simple-networking";
 import { ComponentWithProperties, NavigationMixin } from "@simonbackx/vue-app-navigation";
-import { ErrorBox, Slider, STErrorsDefault, STInputBox, STNavigationBar, STToolbar, BirthDayInput, AddressInput, RadioGroup, Radio, PhoneInput, Checkbox, Validator } from "@stamhoofd/components"
-import { Address, Country, Organization, OrganizationMetaData, OrganizationType, Gender, MemberDetails } from "@stamhoofd/structures"
+import { ErrorBox, STErrorsDefault, STInputBox, STNavigationBar, STToolbar, Validator, STList, STListItem } from "@stamhoofd/components"
+import { Address, Country, Organization, OrganizationMetaData, OrganizationType, Gender, MemberDetails, Parent } from "@stamhoofd/structures"
 import { Component, Mixins, Prop } from "vue-property-decorator";
+import ParentView from './ParentView.vue';
 
 @Component({
     components: {
         STToolbar,
         STNavigationBar,
-        Slider,
         STErrorsDefault,
         STInputBox,
-        AddressInput,
-        BirthDayInput,
-        RadioGroup,
-        Radio,
-        PhoneInput,
-        Checkbox
+        STList,
+        STListItem
     }
 })
 export default class MemberParentsView extends Mixins(NavigationMixin) {
     @Prop({ required: true })
     member: MemberDetails
+
+    editParent(parent: Parent) {
+        this.show(new ComponentWithProperties(ParentView, {
+            parent,
+            handler: (parent: Parent) => {
+                // todo
+                //this.member.parents.push(parent)
+            }
+        }))
+    }
+
+    addParent() {
+        this.show(new ComponentWithProperties(ParentView, {
+            handler: (parent: Parent) => {
+                this.member.parents.push(parent)
+            }
+        }))
+    }
 
     
     async goNext() {
--- app/registration/src/views/registration/ParentView.vue
+++ app/registration/src/views/registration/ParentView.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="member-general-view" class="st-view">
-        <STNavigationBar title="Inschrijven">
+    <div id="parent-view" class="st-view">
+        <STNavigationBar title="Ouder">
             <button slot="right" class="button icon gray close" @click="pop"></button>
         </STNavigationBar>
         
         <main>
             <h1>
-                Wie ga je inschrijven?
+                Gegevens van een ouder
             </h1>
 
             <STErrorsDefault :error-box="errorBox" />
@@ -23,23 +23,13 @@
                         </div>
                     </STInputBox>
 
-                    <BirthDayInput title="Geboortedatum" :validator="validator" v-model="birthDay" />
+                    <PhoneInput title="GSM-nummer" v-model="phone" :validator="validator" placeholder="GSM-nummer van ouder" />
 
-                    <STInputBox title="Identificeert zich als..." error-fields="gender" :error-box="errorBox">
-                        <RadioGroup>
-                            <Radio v-model="gender" value="Male" autocomplete="sex" name="sex">Man</Radio>
-                            <Radio v-model="gender" value="Female" autocomplete="sex" name="sex">Vrouw</Radio>
-                            <Radio v-model="gender" value="Other" autocomplete="sex" name="sex">Andere</Radio>
-                        </RadioGroup>
-                    </STInputBox>
-
-                    <Checkbox v-model="livesAtParents" v-if="livesAtParents || (age >= 18 && age <= 27)">Woont bij ouders</Checkbox>
                 </div>
 
                 <div>
-                    <AddressInput title="Adres van dit lid" v-model="address" v-if="age >= 18 && !livesAtParents" :validator="validator"/>
+                    <AddressInput title="Adres" v-model="address" :validator="validator"/>
 
-                    <PhoneInput title="GSM-nummer van dit lid" v-model="phone" :validator="validator" :required="age >= 18" :placeholder="age >= 18 ? 'Enkel van lid zelf': 'Optioneel. Enkel van lid zelf'" v-if="age >= 12"/>
 
 
                 </div>
@@ -48,7 +38,7 @@
 
         <STToolbar>
             <button  slot="right" class="button primary" @click="goNext">
-                Volgende
+                Toevoegen
             </button>
         </STToolbar>
     </div>
@@ -59,9 +49,8 @@ import { isSimpleError, isSimpleErrors, SimpleError, SimpleErrors } from '@simon
 import { Server } from "@simonbackx/simple-networking";
 import { ComponentWithProperties, NavigationMixin } from "@simonbackx/vue-app-navigation";
 import { ErrorBox, Slider, STErrorsDefault, STInputBox, STNavigationBar, STToolbar, BirthDayInput, AddressInput, RadioGroup, Radio, PhoneInput, Checkbox, Validator } from "@stamhoofd/components"
-import { Address, Country, Organization, OrganizationMetaData, OrganizationType, Gender } from "@stamhoofd/structures"
-import { Component, Mixins } from "vue-property-decorator";
-import { MemberDetails } from '@stamhoofd/structures';
+import { Address, Country, Organization, OrganizationMetaData, OrganizationType, Gender, MemberDetails, Parent } from "@stamhoofd/structures"
+import { Component, Mixins, Prop } from "vue-property-decorator";
 import MemberParentsView from './MemberParentsView.vue';
 
 @Component({
@@ -79,31 +68,20 @@ import MemberParentsView from './MemberParentsView.vue';
         Checkbox
     }
 })
-export default class MemberGeneralView extends Mixins(NavigationMixin) {
+export default class ParentView extends Mixins(NavigationMixin) {
+    @Prop({ default: null })
+    parent: Parent | null // tood
+
+    @Prop({ required: true })
+    handler: (parent: Parent) => void;
+
     firstName = ""
     lastName = ""
     phone: string | null = null
     errorBox: ErrorBox | null = null
 
-    // todo: replace with Addres and new input component
     address: Address | null = null
-    birthDay: Date | null = null
-    gender = Gender.Male
-    livesAtParents = false
     validator = new Validator()
-
-    get age() {
-        if (!this.birthDay) {
-            return 0
-        }
-        const today = new Date();
-        let age = today.getFullYear() - this.birthDay.getFullYear();
-        const m = today.getMonth() - this.birthDay.getMonth();
-        if (m < 0 || (m === 0 && today.getDate() < this.birthDay.getDate())) {
-            age--;
-        }
-        return age;
-    }
 
     async goNext() {
         const errors = new SimpleErrors()
@@ -133,33 +111,19 @@ export default class MemberGeneralView extends Mixins(NavigationMixin) {
         valid = valid && await this.validator.validate()
 
         if (valid) {
-            const memberDetails = MemberDetails.create({
+            const parent = Parent.create({
                 firstName: this.firstName,
                 lastName: this.lastName,
-                gender: this.gender,
                 phone: this.phone,
                 mail: null,
-                birthDay: this.birthDay!,
-                address: this.livesAtParents ? null : this.address
+                address: this.address
             })
 
             // todo: Get possible groups
 
-            // todo: check age before asking parents
-            if (memberDetails.age < 18 || this.livesAtParents) {
-                this.show(new ComponentWithProperties(MemberParentsView, { member: memberDetails }))
-            } else {
-                // Noodcontacten
-                alert("noodcontacten")
-            }
+           this.handler(parent)
+           this.pop()
         }
-    }
-
-    shouldNavigateAway() {
-        if (confirm("Ben je zeker dat je dit venster wilt sluiten?")) {
-            return true;
-        }
-        return false;
     }
 }
 </script>
@@ -167,6 +131,6 @@ export default class MemberGeneralView extends Mixins(NavigationMixin) {
 <style lang="scss">
 @use "@stamhoofd/scss/base/variables.scss" as *;
 
-#member-general-view {
+#parent-view {
 }
 </style>
