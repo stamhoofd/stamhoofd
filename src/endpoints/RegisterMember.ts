55093ac0853d263b049877f6586b7f48093d25a1
Simon Backx <hi@simonbackx.com>
2020-07-19 16:32:45 +0200

    Added reduced prop

--- src/endpoints/RegisterMember.ts
+++ src/endpoints/RegisterMember.ts
@@ -1,4 +1,4 @@
-import { AutoEncoder, EnumDecoder,field, StringDecoder } from '@simonbackx/simple-encoding';
+import { AutoEncoder, BooleanDecoder,EnumDecoder,field, StringDecoder } from '@simonbackx/simple-encoding';
 
 import { PaymentMethod } from '../PaymentMethod';
 
@@ -8,6 +8,9 @@ export class RegisterMember extends AutoEncoder {
 
     @field({ decoder: StringDecoder })
     groupId: string
+
+    @field({ decoder: BooleanDecoder })
+    reduced: boolean
 
     @field({ decoder: new EnumDecoder(PaymentMethod) })
     paymentMethod: PaymentMethod
