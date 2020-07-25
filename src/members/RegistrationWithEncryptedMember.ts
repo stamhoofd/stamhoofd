1f7c4f34f7732c9181a3dbde6d4fe50382a17f12
Simon Backx <hi@simonbackx.com>
2020-07-25 10:47:11 +0200

    Removed payment relation from RegistrationWithEncryptedMember

--- src/members/RegistrationWithEncryptedMember.ts
+++ src/members/RegistrationWithEncryptedMember.ts
@@ -1,9 +1,31 @@
-import { field } from "@simonbackx/simple-encoding"
+import { AutoEncoder,DateDecoder,field, IntegerDecoder, StringDecoder } from "@simonbackx/simple-encoding"
 
 import { EncryptedMember } from "./EncryptedMember"
 import { Registration } from "./Registration"
 
-export class RegistrationWithEncryptedMember extends Registration {
+export class RegistrationWithEncryptedMember extends AutoEncoder  {
+    @field({ decoder: StringDecoder })
+    id: string
+
+    /// You need to look up the group yourself in the organization
+    @field({ decoder: StringDecoder })
+    groupId: string
+
+    @field({ decoder: IntegerDecoder })
+    cycle: number
+
+    @field({ decoder: DateDecoder, nullable: true })
+    registeredAt: Date | null = null
+
+    @field({ decoder: DateDecoder, nullable: true })
+    deactivatedAt: Date | null = null
+
+    @field({ decoder: DateDecoder })
+    createdAt: Date
+
+    @field({ decoder: DateDecoder })
+    updatedAt: Date
+
     @field({ decoder: EncryptedMember })
     member: EncryptedMember
 }
