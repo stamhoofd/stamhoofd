24b90b2436c7fb73cdb810a334f0e22568749cf6
Simon Backx <hi@simonbackx.com>
2020-07-25 10:44:32 +0200

    Removed gropu from registration with encrypted member

--- src/members/RegistrationWithEncryptedMember.ts
+++ src/members/RegistrationWithEncryptedMember.ts
@@ -1,13 +1,9 @@
 import { field } from "@simonbackx/simple-encoding"
 
-import { Group } from '../Group'
 import { EncryptedMember } from "./EncryptedMember"
 import { Registration } from "./Registration"
 
 export class RegistrationWithEncryptedMember extends Registration {
     @field({ decoder: EncryptedMember })
     member: EncryptedMember
-
-    @field({ decoder: Group })
-    group: Group
 }
