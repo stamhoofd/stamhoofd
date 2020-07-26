4a57949afc45fa9b4cde9b9b0dd3e1d565aacd5a
Simon Backx <hi@simonbackx.com>
2020-07-26 22:09:09 +0200

    Replaced optional with version

--- src/OrganizationPrivateMetaData.ts
+++ src/OrganizationPrivateMetaData.ts
@@ -6,18 +6,18 @@ export class OrganizationPrivateMetaData extends AutoEncoder {
     /**
      * DNS records that need to be set in order to activate mail domain and registration domain
      */
-    @field({ decoder: new ArrayDecoder(DNSRecord), optional: true })
+    @field({ decoder: new ArrayDecoder(DNSRecord), version: 6 })
     dnsRecords: DNSRecord[] = [];
 
     /**
      * Mail domain that is awaiting validation
      */
-    @field({ decoder: StringDecoder, nullable: true, optional: true })
+    @field({ decoder: StringDecoder, nullable: true, version: 6 })
     pendingMailDomain: string | null = null
 
     /**
      * Mail domain that is used to send e-mails. You can't set this directly, the server will set this value as soon as the domain has been validated.
      */
-    @field({ decoder: StringDecoder, nullable: true, optional: true })
+    @field({ decoder: StringDecoder, nullable: true, version: 6 })
     mailDomain: string | null = null
 }
