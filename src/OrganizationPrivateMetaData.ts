7a44804e13d384cbd2041a63ae432e0173476889
Simon Backx <hi@simonbackx.com>
2020-07-26 21:37:49 +0200

    Added extra fields

--- src/OrganizationPrivateMetaData.ts
+++ src/OrganizationPrivateMetaData.ts
@@ -1,8 +1,23 @@
-import { ArrayDecoder, AutoEncoder, field } from '@simonbackx/simple-encoding';
+import { ArrayDecoder, AutoEncoder, field, StringDecoder } from '@simonbackx/simple-encoding';
 
 import { DNSRecord } from "./DNSRecord"
 
 export class OrganizationPrivateMetaData extends AutoEncoder {
+    /**
+     * DNS records that need to be set in order to activate mail domain and registration domain
+     */
     @field({ decoder: new ArrayDecoder(DNSRecord) })
     dnsRecords: DNSRecord[] = [];
+
+    /**
+     * Mail domain that is awaiting validation
+     */
+    @field({ decoder: StringDecoder, nullable: true })
+    pendingMailDomain: string | null = null
+
+    /**
+     * Mail domain that is used to send e-mails. You can't set this directly, the server will set this value as soon as the domain has been validated.
+     */
+    @field({ decoder: StringDecoder, nullable: true })
+    mailDomain: string | null = null
 }
