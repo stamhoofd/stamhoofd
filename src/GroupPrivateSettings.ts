cd429b2efac5a803e2d84287d63feef6e5236bd1
Simon Backx <hi@simonbackx.com>
2020-07-27 14:09:55 +0200

    Group private settings version internal

--- src/GroupPrivateSettings.ts
+++ src/GroupPrivateSettings.ts
@@ -2,7 +2,7 @@ import { AutoEncoder, field, StringDecoder } from '@simonbackx/simple-encoding';
 
 
 export class GroupPrivateSettings extends AutoEncoder {
-    @field({ decoder: StringDecoder, nullable: true })
+    @field({ decoder: StringDecoder, nullable: true, version: 10 })
     defaultEmailId: string | null = null
 }
 
