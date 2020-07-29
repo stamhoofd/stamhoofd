ee6b875f530e7e58fd6d8e37ea4018962368e8f9
Simon Backx <hi@simonbackx.com>
2020-07-29 15:35:15 +0200

    Added permisison level number

--- src/Permissions.ts
+++ src/Permissions.ts
@@ -14,6 +14,19 @@ export enum PermissionLevel {
     Full = "Full",
 }
 
+export function getPermissionLevelNumber(level: PermissionLevel): number {
+    switch (level) {
+        case PermissionLevel.None: return 0;
+        case PermissionLevel.Read: return 1;
+        case PermissionLevel.Write: return 2;
+        case PermissionLevel.Full: return 3;
+        default: {
+            const l: never = level; // will throw compile error if new levels are added without editing this method
+            throw new Error("Unknown permission level "+l);
+        }
+    }
+}
+
 export class GroupPermissions extends AutoEncoder {
     @field({ decoder: StringDecoder })
     groupId: string
