d0527828a77c1d7446d95706f3facafcea8272cd
Simon Backx <hi@simonbackx.com>
2020-08-01 14:28:36 +0200

    Adjust payment status in member view

--- app/dashboard/src/views/dashboard/member/MemberViewPayments.vue
+++ app/dashboard/src/views/dashboard/member/MemberViewPayments.vue
@@ -1,49 +1,58 @@
 <template>
     <div class="view-payments">
-        <h2>Betaling</h2>
         <main>
-            <dl class="details-grid" v-for="payment in payments">
-                <dt>Bedrag</dt>
-                <dd>{{ payment.price | price }}</dd>
+            <div v-for="payment in payments" >
+                <h2>Betaling <span class="icon success green" v-if="payment.status == 'Succeeded'"/></h2>
+                <dl class="details-grid" >
+                    <dt>Bedrag</dt>
+                    <dd>{{ payment.price | price }}</dd>
 
-                <dt>Bankrekening</dt>
-                <dd>{{ organization.meta.iban }}</dd>
+                    <dt>Bankrekening</dt>
+                    <dd>{{ organization.meta.iban }}</dd>
 
-                <dt>Mededeling</dt>
-                <dd>{{ payment.transferDescription }}</dd>
+                    <dt>Mededeling</dt>
+                    <dd>{{ payment.transferDescription }}</dd>
 
-                <dt>Status</dt>
-                <dd v-if="payment.status == 'Succeeded'">
-                    Betaald
-                </dd>
-                <dd v-else>
-                    Nog niet betaald
-                </dd>
-            </dl>
+                    <dt>Status</dt>
+                    <dd v-if="payment.status == 'Succeeded'">
+                        Betaald
+                    </dd>
+                    <dd v-else>
+                        Nog niet betaald
+                    </dd>
+                </dl>
+            </div>
         </main>
 
-        <STToolbar v-if="!member.paid">
+        <STToolbar>
             <template #left />
             <template #right>
-                <button class="button primary">
-                    Markeer als betaald
-                    <div class="dropdown" @click.stop="" />
-                </button>
+                <LoadingButton :loading="loading">
+                    <button class="button primary" v-if="!member.paid" @click="markPaid">
+                        Markeer als betaald
+                    </button>
+                    <button class="button secundary" v-else @click="markNotPaid">
+                        Toch niet betaald
+                    </button>
+                </LoadingButton>
             </template>
         </STToolbar>
     </div>
 </template>
 
 <script lang="ts">
-import { STToolbar } from "@stamhoofd/components";
+import { STToolbar, LoadingButton } from "@stamhoofd/components";
 import { Component, Prop,Vue } from "vue-property-decorator";
-import { MemberWithRegistrations } from '@stamhoofd/structures';
+import { MemberWithRegistrations, PaymentStatus, PaymentPatch, EncryptedPaymentDetailed } from '@stamhoofd/structures';
 import { Formatter } from '@stamhoofd/utility';
 import { OrganizationManager } from '../../../classes/OrganizationManager';
+import { SessionManager } from '@stamhoofd/networking';
+import { Decoder, ArrayDecoder } from '@simonbackx/simple-encoding';
 
 @Component({ 
     components: { 
-        STToolbar 
+        STToolbar,
+        LoadingButton
     },
     filters: {
         price: Formatter.price
@@ -52,11 +61,96 @@ import { OrganizationManager } from '../../../classes/OrganizationManager';
 export default class MemberViewPayments extends Vue {
     @Prop()
     member!: MemberWithRegistrations;
+    loading = false
 
     organization = OrganizationManager.organization
 
     get payments() {
         return this.member.registrations.map(r => r.payment)
+    }
+
+    async markPaid() {
+        if (this.loading) {
+            return;
+        }
+
+        const data: PaymentPatch[] = []
+        for (const payment of this.payments) {
+            if (payment.status != PaymentStatus.Succeeded) {
+                data.push(PaymentPatch.create({
+                    id: payment.id,
+                    status: PaymentStatus.Succeeded
+                }))
+            }
+        }
+
+        if (data.length > 0) {
+            if (!confirm("Ben je zeker dat deze overschrijving uitgevoerd is?")) {
+                return;
+            }
+
+            this.loading = true
+            const session = SessionManager.currentSession!
+
+            try {
+                const response = await session.authenticatedServer.request({
+                    method: "PATCH",
+                    path: "/organization/payments",
+                    body: data,
+                    decoder: new ArrayDecoder(EncryptedPaymentDetailed as Decoder<EncryptedPaymentDetailed>)
+                })
+                for (const payment of this.payments) {
+                    payment.status = PaymentStatus.Succeeded
+                    // todo: improve this for related payments
+                }
+            } finally {
+                this.loading = false
+            }
+            
+        }
+    }
+
+    async markNotPaid() {
+        if (this.loading) {
+            return;
+        }
+
+        const data: PaymentPatch[] = []
+        for (const payment of this.payments) {
+            if (payment.status == PaymentStatus.Succeeded) {
+                data.push(PaymentPatch.create({
+                    id: payment.id,
+                    status: PaymentStatus.Pending
+                }))
+            }
+        }
+
+        if (data.length > 0) {
+            if (!confirm("Ben je zeker dat deze overschrijving niet uitgevoerd is?")) {
+                return;
+            }
+
+            this.loading = true
+            const session = SessionManager.currentSession!
+
+            try {
+                const response = await session.authenticatedServer.request({
+                    method: "PATCH",
+                    path: "/organization/payments",
+                    body: data,
+                    decoder: new ArrayDecoder(EncryptedPaymentDetailed as Decoder<EncryptedPaymentDetailed>)
+                })
+                for (const payment of this.payments) {
+                    if (payment.status == PaymentStatus.Succeeded) {
+                        payment.status = PaymentStatus.Pending
+                    }
+                    // todo: improve this for related payments
+                }
+            } finally {
+                this.loading = false
+            }
+            
+        }
     }
 }
 </script>
@@ -71,7 +165,7 @@ export default class MemberViewPayments extends Vue {
     display: flex;
     flex-direction: column;
 
-    > h2 {
+    > main > div > h2 {
         @extend .style-title-2;
         margin: 20px 0;
     }
