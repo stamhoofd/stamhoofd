d25dcdf5845d4a064923fd2a6961aef1da717ecc
Simon Backx <hi@simonbackx.com>
2020-10-26 08:26:52 +0100

    MyUser

--- shared/structures/src/User.ts
+++ shared/structures/src/User.ts
@@ -1,5 +1,6 @@
-import { AutoEncoder, EmailDecoder,field, StringDecoder } from '@simonbackx/simple-encoding';
+import { ArrayDecoder, AutoEncoder, EmailDecoder,field, StringDecoder } from '@simonbackx/simple-encoding';
 import { v4 as uuidv4 } from "uuid";
+import { Invite, TradedInvite } from './Invite';
 
 import { KeyConstants } from './KeyConstants';
 import { Permissions } from './Permissions';
@@ -41,4 +42,9 @@ export class NewUser extends User {
     // this is the private key that is associated with publicKey
     @field({ decoder: StringDecoder })
     encryptedPrivateKey: string;
+}
+
+export class MyUser extends NewUser {
+    @field({ decoder: new ArrayDecoder(TradedInvite)  })
+    incomingInvites: TradedInvite[] = []
 }
