c9867d3140e743fa2331ab64cb009a68c846a306
Simon Backx <hi@simonbackx.com>
2020-10-27 07:35:21 +0100

    Added extra columns to export

--- frontend/app/dashboard/src/classes/MemberExcelExport.ts
+++ frontend/app/dashboard/src/classes/MemberExcelExport.ts
@@ -1,5 +1,6 @@
 import XLSX from "xlsx";
 import { MemberWithRegistrations } from '@stamhoofd/structures';
+import { Formatter } from '@stamhoofd/utility';
 
 export class MemberExcelExport {
     static export(members: MemberWithRegistrations[]) {
@@ -14,9 +15,12 @@ export class MemberExcelExport {
                 "Geslacht",
                 "Geboortedatum",
                 "GSM",
+                "E-mailadres",
+                "Adres",
                 "Naam ouder",
                 "GSM ouder",
                 "E-mailadres ouder",
+                "Adres ouder",
             ],
         ];
 
@@ -31,15 +35,15 @@ export class MemberExcelExport {
                 member.firstName,
                 member.lastName,
                 member.gender,
-                member.birthDay.getDate() +
-                "/" +
-                (member.birthDay.getMonth() + 1) +
-                "/" +
-                member.birthDay.getFullYear(),
+                Formatter.dateNumber(member.birthDay),
                 member.phone ? member.phone : "",
+                member.email ? member.email : "",
+                member.address ? member.address.toString() : "",
+
                 firstParent ? firstParent.name : "/",
                 firstParent && firstParent.phone ? firstParent.phone : "/",
                 firstParent && firstParent.email ? firstParent.email : "/",
+                firstParent && firstParent.address ? firstParent.address.toString() : "/",
             ]);
 
             for (let index = 1; index < member.parents.length; index++) {
@@ -50,14 +54,52 @@ export class MemberExcelExport {
                     "",
                     "",
                     "",
+                    "",
+                    "",
                     parent.name,
                     parent.phone ? parent.phone : "/",
                     parent.email ? parent.email : "/",
+                    parent.address ? parent.address.toString() : "/",
                 ]);
             }
         });
 
+        // Delete empty columns
+        for (let index = wsData[0].length - 1; index >= 0; index--) {
+            let empty = true
+            for (const row of wsData.slice(1)) {
+                if (row[index].length == 0 || row[index] == "/") {
+                    continue;
+                }
+                empty = false
+                break
+            }
+            if (empty) {
+               for (const row of wsData) {
+                    row.splice(index, 1)
+                } 
+            }
+        }
+
         const ws = XLSX.utils.aoa_to_sheet(wsData);
+
+        // Set column width
+        ws['!cols'] = []
+        for (const column of wsData[0]) {
+            if (column.toLowerCase().startsWith("naam")) {
+                ws['!cols'].push({width: 20});
+            } else if (column.toLowerCase().includes("naam")) {
+                ws['!cols'].push({width: 15});
+            } else if (column.toLowerCase().includes("e-mail")) {
+                ws['!cols'].push({width: 25});
+            } else if (column.toLowerCase().includes("adres")) {
+                ws['!cols'].push({width: 30});
+            } else if (column.toLowerCase().includes("gsm")) {
+                ws['!cols'].push({width: 16});
+            } else {
+                ws['!cols'].push({width: 13});
+            }
+        }
 
         /* Add the worksheet to the workbook */
         XLSX.utils.book_append_sheet(wb, ws, wsName);
