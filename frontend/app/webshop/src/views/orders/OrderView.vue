b38edd7d1778d8e76ab3f7b465fb85f057913cf2
Simon Backx <hi@simonbackx.com>
2020-11-07 17:35:25 +0100

    Improved order view

--- frontend/app/webshop/src/views/orders/OrderView.vue
+++ frontend/app/webshop/src/views/orders/OrderView.vue
@@ -1,11 +1,41 @@
 <template>
     <LoadingView v-if="!order" />
     <div class="boxed-view" v-else>
-        <div class="st-view">
+        <div class="st-view order-view">
             <main>
                 <h1>Jouw bestelling {{ order.id }}</h1>
 
                 <p>{{ order.validAt || "Not valid" }}</p>
+
+                <STList>
+                    <STListItem>
+                        Afhaaldatum
+
+                        <template slot="right">{{ order.data.timeSlot.date | date }}</template>
+                    </STListItem>
+                </STList>
+
+                <hr>
+                <h2>Jouw bestelling</h2>
+
+                <STList>
+                    <STListItem v-for="cartItem in order.data.cart.items" :key="cartItem.id" class="cart-item-row">
+                        <h3>
+                            {{ cartItem.product.name }}
+                        </h3>
+                        <p v-if="cartItem.description" class="description" v-text="cartItem.description" />
+
+                        <footer>
+                            <p class="price">
+                                {{ cartItem.amount }} x {{ cartItem.unitPrice | price }}
+                            </p>
+                        </footer>
+
+                        <figure v-if="imageSrc(cartItem)" slot="right">
+                            <img :src="imageSrc(cartItem)">
+                        </figure>
+                    </STListItem>
+                </STList>
                 
             </main>
         </div>
@@ -18,7 +48,7 @@ import { SimpleError } from '@simonbackx/simple-errors';
 import { ComponentWithProperties,HistoryManager,NavigationController,NavigationMixin } from "@simonbackx/vue-app-navigation";
 import { ErrorBox, LoadingButton, Radio, STErrorsDefault,STList, STListItem, STNavigationBar, STToolbar, LoadingView, PaymentHandler, CenteredMessage } from "@stamhoofd/components"
 import { SessionManager } from '@stamhoofd/networking';
-import { Group, KeychainedResponse, MemberWithRegistrations, Order, OrderData, OrderResponse, Payment, PaymentMethod, PaymentStatus, Record, RecordType, RegisterMember, RegisterMembers, RegisterResponse, SelectedGroup, WebshopTakeoutMethod, WebshopTimeSlot, WebshopTimeSlots } from '@stamhoofd/structures';
+import { CartItem, Group, KeychainedResponse, MemberWithRegistrations, Order, OrderData, OrderResponse, Payment, PaymentMethod, PaymentStatus, Record, RecordType, RegisterMember, RegisterMembers, RegisterResponse, SelectedGroup, WebshopTakeoutMethod, WebshopTimeSlot, WebshopTimeSlots } from '@stamhoofd/structures';
 import { Formatter } from '@stamhoofd/utility';
 import { Component, Mixins,  Prop,Vue } from "vue-property-decorator";
 
@@ -36,6 +66,11 @@ import MemberGeneralView from '../registration/MemberGeneralView.vue';
         LoadingButton,
         STErrorsDefault,
         LoadingView
+    },
+    filters: {
+        price: Formatter.price.bind(Formatter),
+        priceChange: Formatter.priceChange.bind(Formatter),
+        date: Formatter.dateWithDay.bind(Formatter)
     }
 })
 export default class OrderView extends Mixins(NavigationMixin){
@@ -90,13 +125,11 @@ export default class OrderView extends Mixins(NavigationMixin){
                     this.pop({ force: true })
                 })
         }
-        
-
     }
 
-
-
-   
+    imageSrc(cartItem: CartItem) {
+        return cartItem.product.images[0]?.getPathForSize(100, 100)
+    }
 }
 </script>
 
@@ -104,5 +137,38 @@ export default class OrderView extends Mixins(NavigationMixin){
 @use "@stamhoofd/scss/base/variables.scss" as *;
 @use "@stamhoofd/scss/base/text-styles.scss" as *;
 
+.order-view {
+    .cart-item-row {
+        h3 {
+            padding-top: 5px;
+            @extend .style-title-3;
+        }
 
+        .description {
+            @extend .style-description-small;
+            padding-top: 5px;
+            white-space: pre-wrap;
+        }
+
+        .price {
+            font-size: 14px;
+            line-height: 1.4;
+            font-weight: 600;
+            padding-top: 10px;
+            color: $color-primary;
+        }
+
+        footer {
+            display: flex;
+            justify-content: space-between;
+            align-items: flex-end;
+        }
+
+        img {
+            width: 100px;
+            height: 100px;
+            border-radius: $border-radius;
+        }
+    }
+}
 </style>
