eb7741045de8d5f8f69d7c6e368848748e8b7a7f
Simon Backx <hi@simonbackx.com>
2021-03-05 18:02:35 +0100

    Fix onboarding flow

--- frontend/app/dashboard/src/views/dashboard/settings/modules/members/MembersPriceSetupView.vue
+++ frontend/app/dashboard/src/views/dashboard/settings/modules/members/MembersPriceSetupView.vue
@@ -2,7 +2,7 @@
     <div id="personalize-settings-view" class="st-view background">
         <STNavigationBar title="Lidgeld">
             <BackButton v-if="canPop" slot="left" @click="pop" />
-            <button v-else class="button icon close gray" @click="pop" slot="right" />
+            <button v-else slot="right" class="button icon close gray" @click="pop" />
         </STNavigationBar>
 
         <main>
@@ -12,7 +12,6 @@
             <STErrorsDefault :error-box="errorBox" />
 
             <EditGroupPriceBox :validator="validator" :prices="getPrices()" @patch="addPricesPatch" />
-
         </main>
 
         <STToolbar>
@@ -31,17 +30,18 @@
 import { AutoEncoder, AutoEncoderPatchType, Decoder, PartialWithoutMethods, PatchableArray,PatchableArrayAutoEncoder,patchContainsChanges } from '@simonbackx/simple-encoding';
 import { SimpleError, SimpleErrors } from '@simonbackx/simple-errors';
 import { ComponentWithProperties, HistoryManager,NavigationController, NavigationMixin } from "@simonbackx/vue-app-navigation";
-import { TimeInput, BackButton, CenteredMessage, Checkbox, ColorInput, DateSelection, ErrorBox, FileInput,IBANInput, ImageInput, LoadingButton, Radio, RadioGroup, STErrorsDefault,STInputBox, STNavigationBar, STToolbar, Toast, Validator} from "@stamhoofd/components";
+import { BackButton, CenteredMessage, Checkbox, ColorInput, DateSelection, ErrorBox, FileInput,IBANInput, ImageInput, LoadingButton, Radio, RadioGroup, STErrorsDefault,STInputBox, STNavigationBar, STToolbar, TimeInput, Toast, Validator} from "@stamhoofd/components";
 import { SessionManager } from '@stamhoofd/networking';
-import { Address, File, GroupPrices, Image, Organization, OrganizationMetaData, OrganizationModules, OrganizationPatch, OrganizationPrivateMetaData,PaymentMethod, ResolutionFit, ResolutionRequest, Version } from "@stamhoofd/structures"
+import { Address, File, GroupCategory, GroupCategorySettings, GroupCategoryTree, GroupPrices, Image, Organization, OrganizationMetaData, OrganizationModules, OrganizationPatch, OrganizationPrivateMetaData,OrganizationTypeHelper,PaymentMethod, ResolutionFit, ResolutionRequest, Version } from "@stamhoofd/structures"
 import { Component, Mixins } from "vue-property-decorator";
 
 import { OrganizationManager } from "../../../../../classes/OrganizationManager"
+import EditCategoryGroupsView from '../../../groups/EditCategoryGroupsView.vue';
+import EditGroupPriceBox from '../../../groups/EditGroupPriceBox.vue';
+import EditGroupsView from '../../../groups/EditGroupsView.vue';
 import DNSRecordsView from './DNSRecordsView.vue';
 import DomainSettingsView from './DomainSettingsView.vue';
 import EmailSettingsView from './EmailSettingsView.vue';
-import EditGroupPriceBox from '../../../groups/EditGroupPriceBox.vue';
-import EditGroupsView from '../../../groups/EditGroupsView.vue';
 
 @Component({
     components: {
@@ -113,12 +113,90 @@ export default class MembersPriceSetupView extends Mixins(NavigationMixin) {
             await OrganizationManager.patch(this.organizationPatch)
             this.organizationPatch = OrganizationPatch.create({ id: OrganizationManager.organization.id })
             new Toast('De ledenadministratie module is nu actief', "success green").show()
-            this.navigationController!.push(new ComponentWithProperties(EditGroupsView, {}), true, this.navigationController!.components.length)
+            this.manageGroups(true)
         } catch (e) {
             this.errorBox = new ErrorBox(e)
         }
 
         this.saving = false
+    }
+
+    get enableActivities() {
+        return this.organization.meta.modules.useActivities
+    }
+
+    manageGroups(animated = true) {
+        if (!this.organization.meta.rootCategory) {
+            // Auto restore missing root category
+            const category = GroupCategory.create({})
+            const meta = OrganizationMetaData.patch({
+                rootCategoryId: category.id
+            })
+            meta.categories.addPut(category)
+
+            const p = Organization.patch({
+                id: this.organization.id,
+                meta
+            })
+            
+            this.navigationController!.push(
+                new ComponentWithProperties(EditCategoryGroupsView, { 
+                    category: category, 
+                    organization: this.organization.patch(p), 
+                    saveHandler: async (patch: AutoEncoderPatchType<Organization>) => {
+                        patch.id = this.organization.id
+                        await OrganizationManager.patch(p.patch(patch))
+                    }
+                })
+            , true, this.navigationController!.components.length)
+            return
+        }
+
+        let cat = this.organization.meta.rootCategory
+
+        let p = Organization.patch({
+            id: this.organization.id
+        })
+
+        if (!this.enableActivities) {
+            const full = GroupCategoryTree.build(cat, this.organization.meta.categories, this.organization.groups)
+            if (full.categories.length === 0) {
+
+                // Create a new one and open that one instead
+                const defaultCategories = OrganizationTypeHelper.getDefaultGroupCategories(this.organization.meta.type, this.organization.meta.umbrellaOrganization ?? undefined)
+                const category = defaultCategories[0] ?? GroupCategory.create({
+                    settings: GroupCategorySettings.create({
+                        name: "Leeftijdsgroepen",
+                        
+                    })
+                })
+                category.groupIds = this.organization.groups.map(g => g.id)
+                
+                const meta = OrganizationMetaData.patch({})
+                meta.categories.addPut(category)
+
+                const me = GroupCategory.patch({ id: cat.id })
+                me.categoryIds.addPut(category.id)
+                meta.categories.addPatch(me)
+
+                p = p.patch({
+                    meta
+                })
+
+                cat = category
+
+            } else {
+                cat = full.categories[0]
+            }
+        }
+        this.navigationController!.push(new ComponentWithProperties(EditCategoryGroupsView, {
+            category: cat,
+            organization: this.organization.patch(p),
+            saveHandler: async (patch) => {
+                patch.id = this.organization.id
+                await OrganizationManager.patch(p.patch(patch))
+            }
+        }), true, this.navigationController!.components.length)
     }
 
     async shouldNavigateAway() {
