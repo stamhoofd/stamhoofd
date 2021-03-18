f864150e4cb6154587484b0546a6ff141fed2589
Simon Backx <hi@simonbackx.com>
2021-03-18 15:12:51 +0100

    Remove write and read general access from all invites

---
+++ backend/src/migrations/1616075962-fix-read-access.ts
@@ -0,0 +1,57 @@
+import { Migration } from '@simonbackx/simple-database';
+import { PermissionLevel} from '@stamhoofd/structures';
+import { Organization } from '../models/Organization';
+import { User } from '../models/User';
+import { Invite } from '../models/Invite';
+
+export default new Migration(async () => {
+    if (process.env.NODE_ENV == "test") {
+        console.log("skipped in tests")
+        return;
+    }
+
+    const organizations = await Organization.all();
+
+    for (const organization of organizations) {
+        const d = new Date()
+
+        const invites = (await Invite.where({ 
+            organizationId: organization.id, 
+            permissions: {
+                value: null,
+                sign: "!="
+            }
+        }))
+
+         // Get all admins
+        const users = [
+            ...await User.where({ 
+                organizationId: organization.id, 
+                permissions: {
+                    value: null,
+                    sign: "!="
+                }
+            }), 
+            ...invites
+        ]
+        
+        for (const user of users) {
+            if (!user.permissions) {
+                console.error("PERMISSIONS MISSING!")
+                continue
+            }
+
+            if (user.permissions.level === PermissionLevel.Read) {
+                if (user instanceof Invite) {
+                    console.warn("Invite had read permissions: "+(user.userDetails?.firstName ?? "")+" @"+organization.name )
+                } else {
+                    console.warn("User had read permissions: "+(user.firstName ?? "")+" @"+organization.name )
+                }
+                user.permissions.level = PermissionLevel.None
+                await user.save()
+            }
+        }        
+    }
+});
+
+
