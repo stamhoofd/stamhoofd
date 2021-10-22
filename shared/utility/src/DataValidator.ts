07be8b2d00b23517dd6d1ce02ab551e72fbcca1f
Simon Backx <hi@simonbackx.com>
2021-10-22 14:35:42 +0200

    Block icloud.be email domain

--- shared/utility/src/DataValidator.ts
+++ shared/utility/src/DataValidator.ts
@@ -1,7 +1,7 @@
 
 export class DataValidator {
     static isEmailValid(str: string) {
-        const blockList = ["gmail.be", "gmail.nl", "hotmail.c", "hotmail.co", "gmail.co", "gmail.c", "gmail.co", "gmal.com", "glail.com", "gmail.col", "gamil.com", "gmail.con"]
+        const blockList = ["gmail.be", "gmail.nl", "hotmail.c", "hotmail.co", "gmail.co", "gmail.c", "gmail.co", "gmal.com", "glail.com", "gmail.col", "gamil.com", "gmail.con", "icloud.be"]
         const regex = /^[a-zA-Z0-9.!#$%&*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
         
         if (!regex.test(str)) {
