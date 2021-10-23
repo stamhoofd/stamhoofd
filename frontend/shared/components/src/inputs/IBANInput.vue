b5787d31d3772f356e9ba801b3a542026d05bdd5
Simon Backx <hi@simonbackx.com>
2021-10-23 11:56:51 +0200

    IBAN placeholder localized

--- frontend/shared/components/src/inputs/IBANInput.vue
+++ frontend/shared/components/src/inputs/IBANInput.vue
@@ -1,6 +1,6 @@
 <template>
     <STInputBox :title="title" error-fields="iban" :error-box="errorBox">
-        <input v-model="ibanRaw" class="input" type="text" :class="{ error: !valid }" :placeholder="placeholder" :autocomplete="autocomplete" @change="validate">
+        <input v-model="ibanRaw" class="input" type="text" :class="{ error: !valid }" :placeholder="placeholder !== null ? placeholder : $t('shared.inputs.iban.placeholder')" :autocomplete="autocomplete" @change="validate">
     </STInputBox>
 </template>
 
@@ -30,8 +30,8 @@ export default class IBANInput extends Vue {
     @Prop({ default: true })
     required!: boolean
 
-    @Prop({ default: "Bv. BE71 6316 1793 1969" })
-    placeholder!: string
+    @Prop({ default: null })
+    placeholder!: string | null
 
     @Prop({ default: "email" })
     autocomplete!: string
