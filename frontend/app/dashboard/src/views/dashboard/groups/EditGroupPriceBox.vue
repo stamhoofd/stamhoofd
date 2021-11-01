04f9efe1d4a7d089ffd617c525d1b745ddcc0022
Simon Backx <hi@simonbackx.com>
2021-11-01 12:42:12 +0100

    Clean up

--- frontend/app/dashboard/src/views/dashboard/groups/EditGroupPriceBox.vue
+++ frontend/app/dashboard/src/views/dashboard/groups/EditGroupPriceBox.vue
@@ -59,7 +59,7 @@
 
 
             <p class="style-description-small">
-                * Verlaagd tarief voor leden met financiële moeilijkheden. Laat leeg indien je die niet wilt gebruiken.
+                * Verlaagd tarief voor leden die financiële ondersteuning hebben aangevraagd. Laat leeg indien je die niet wilt gebruiken.
             </p>
 
             <template v-if="(priceGroup.sameMemberOnlyDiscount || priceGroup.prices.length <= 1) && canRegisterMultipleGroups">
@@ -103,11 +103,10 @@
 <script lang="ts">
 import { PatchableArray, PatchableArrayAutoEncoder } from '@simonbackx/simple-encoding';
 import { NavigationMixin } from "@simonbackx/vue-app-navigation";
-import { CenteredMessage, Checkbox, DateSelection, ErrorBox, PriceInput, Radio,RadioGroup, STErrorsDefault,STInputBox, STList, STListItem, STNavigationBar, STToolbar, TimeInput, Validator } from "@stamhoofd/components";
-import { Group, GroupCategory, GroupPrice, GroupPrices, Organization } from "@stamhoofd/structures"
+import { Checkbox, DateSelection, ErrorBox, PriceInput, Radio, RadioGroup, STErrorsDefault, STInputBox, STList, STListItem, STNavigationBar, STToolbar, TimeInput, Validator } from "@stamhoofd/components";
+import { Group, GroupPrice, GroupPrices, Organization } from "@stamhoofd/structures";
 import { Formatter } from '@stamhoofd/utility';
-import { v4 as uuidv4 } from "uuid";
-import { Component, Mixins,Prop } from "vue-property-decorator";
+import { Component, Mixins, Prop } from "vue-property-decorator";
 
 import { OrganizationManager } from '../../../classes/OrganizationManager';
 
@@ -277,183 +276,5 @@ export default class EditGroupPriceBox extends Mixins(NavigationMixin) {
         patch.addPatch(gp)
         this.addPatch(patch)
     }
-
-    /*get price() {
-        return this.prices.find(p => p.startDate == null)?.price ?? 0
-    }
-
-    set price(price: number) {
-        const p = this.prices.find(p => p.startDate == null)
-
-        if (!p) {
-            const patch = this.getPricesPatch()
-            patch.addPut(GroupPrices.create({ price }))
-            this.addPatch(patch)
-        } else {
-            const patch = this.getPricesPatch()
-            patch.addPatch(GroupPrices.patchType().create({ id: p.id, price }))
-            this.addPatch(patch)
-        }
-    }
-
-    get enableReducedPrice() {
-        return !!this.prices.find(p => p.reducedPrice !== null)
-    }
-
-    set enableReducedPrice(enable: boolean) {
-        const patch = this.getPricesPatch()
-
-        for (const price of this.prices) {
-            if (!enable) {
-                patch.addPatch(GroupPrices.patchType().create({ id: price.id, reducedPrice: null}))
-            } else {
-                patch.addPatch(GroupPrices.patchType().create({ id: price.id, reducedPrice: price.price}))
-            }
-        }
-
-        this.addPatch(patch)
-    }
-
-    get enableFamilyPrice() {
-        return !!this.prices.find(p => p.familyPrice !== null)
-    }
-
-    set enableFamilyPrice(enable: boolean) {
-        const patch = this.getPricesPatch()
-
-        for (const price of this.prices) {
-            if (!enable) {
-                patch.addPatch(GroupPrices.patchType().create({ id: price.id, familyPrice: null, extraFamilyPrice: null }))
-            } else {
-                patch.addPatch(GroupPrices.patchType().create({ id: price.id, familyPrice: price.price, extraFamilyPrice: price.price }))
-            }
-        }
-
-        this.addPatch(patch)
-    }
-
-    get familyPrice() {
-        return this.prices.find(p => p.startDate == null)?.familyPrice ?? 0
-    }
-
-    set familyPrice(familyPrice: number) {
-        const patch = this.getPricesPatch()
-
-        for (const price of this.prices) {
-            patch.addPatch(GroupPrices.patchType().create({ id: price.id, familyPrice }))
-        }
-
-        this.addPatch(patch)
-    }
-
-    get extraFamilyPrice() {
-        return this.prices.find(p => p.startDate == null)?.extraFamilyPrice ?? 0
-    }
-
-    set extraFamilyPrice(extraFamilyPrice: number) {
-        const patch = this.getPricesPatch()
-
-        for (const price of this.prices) {
-            patch.addPatch(GroupPrices.patchType().create({ id: price.id, extraFamilyPrice }))
-        }
-
-        this.addPatch(patch)
-    }
-
-    get reducedPrice() {
-        return this.prices.find(p => p.startDate == null)?.reducedPrice ?? 0
-    }
-
-    set reducedPrice(reducedPrice: number) {
-        const p = this.prices.find(p => p.startDate == null)
-
-        if (!p) {
-            const patch = this.getPricesPatch()
-            patch.addPut(GroupPrices.create({ reducedPrice }))
-            this.addPatch(patch)
-        } else {
-            const patch = this.getPricesPatch()
-            patch.addPatch(GroupPrices.patchType().create({ id: p.id, reducedPrice }))
-            this.addPatch(patch)
-        }
-    }
-
-    get enableLatePrice() {
-        return !!this.prices.find(p => p.startDate !== null)
-    }
-
-    set enableLatePrice(enable: boolean) {
-        const p = this.prices.find(p => p.startDate !== null)
-
-        if (!enable) {
-            if (p) {
-                const patch = this.getPricesPatch()
-                patch.addDelete(p.id)
-                this.addPatch(patch)
-            }
-            return
-        } else {
-            if (!p) {
-                const patch = this.getPricesPatch()
-                // todo: better default date
-                patch.addPut(GroupPrices.create({ 
-                    startDate: new Date(), 
-                    price: this.price, 
-                    reducedPrice: this.enableReducedPrice ? this.reducedPrice : null, 
-                    familyPrice: this.enableFamilyPrice ? this.familyPrice : null, 
-                    extraFamilyPrice: this.enableFamilyPrice ? this.extraFamilyPrice : null
-                }))
-                this.addPatch(patch)
-            }
-        }
-    }
-
-    get latePrice() {
-        return this.prices.find(p => p.startDate !== null)?.price ?? 0
-    }
-
-    set latePrice(price: number) {
-        const p = this.prices.find(p => p.startDate !== null)
-
-        if (!p) {
-            // shouldn't happen
-        } else {
-            const patch = this.getPricesPatch()
-            patch.addPatch(GroupPrices.patchType().create({ id: p.id, price }))
-            this.addPatch(patch)
-        }
-    }
-
-    get reducedLatePrice() {
-        return this.prices.find(p => p.startDate !== null)?.reducedPrice ?? 0
-    }
-
-    set reducedLatePrice(reducedPrice: number) {
-        const p = this.prices.find(p => p.startDate !== null)
-
-        if (!p) {
-            // shouldn't happen
-        } else {
-            const patch = this.getPricesPatch()
-            patch.addPatch(GroupPrices.patchType().create({ id: p.id, reducedPrice }))
-            this.addPatch(patch)
-        }
-    }
-
-    get latePriceDate() {
-        return this.prices.find(p => p.startDate !== null)?.startDate ?? new Date()
-    }
-
-    set latePriceDate(startDate: Date) {
-        const p = this.prices.find(p => p.startDate !== null)
-
-        if (!p) {
-            // shouldn't happen
-        } else {
-            const patch = this.getPricesPatch()
-            patch.addPatch(GroupPrices.patchType().create({ id: p.id, startDate }))
-            this.addPatch(patch)
-        }
-    }*/
 }
 </script>
