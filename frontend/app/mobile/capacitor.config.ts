694c80c8ac9b053860d70f5ac5f2d591820632c1
Simon Backx <hi@simonbackx.com>
2021-11-24 16:11:21 +0100

    tmp

--- frontend/app/mobile/capacitor.config.ts
+++ frontend/app/mobile/capacitor.config.ts
@@ -4,25 +4,25 @@ import { KeyboardResize } from '@capacitor/keyboard';
 // This is de development capacitor configuration
 
 const config: CapacitorConfig = {
-  appId: 'com.stamhoofd.stamhoofd',
-  appName: 'Stamhoofd',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  loggingBehavior: "none",
-  hideLogs: true,
-  plugins: {
-    Keyboard: {
-      resize: KeyboardResize.None,
+    appId: 'com.stamhoofd.stamhoofd',
+    appName: 'Stamhoofd',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+    loggingBehavior: "none",
+    hideLogs: true,
+    plugins: {
+        Keyboard: {
+            resize: KeyboardResize.None,
+        },
+        PushNotifications: {
+            presentationOptions: ["badge", "sound", "alert"]
+        }
     },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
+    // Uncomment to enable live reload
+    server: {
+        url: "https://dashboard.stamhoofd.dev",
+        cleartext: true
     }
-  },
-  // Uncomment to enable live reload
-  server: {
-    url: "https://dashboard.stamhoofd.dev",
-    cleartext: true
-  }
 };
 
 export default config;
