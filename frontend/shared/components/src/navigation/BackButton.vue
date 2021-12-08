aa04a01e9d5ccc81a49949c589ead4fdbfa8d3a5
Simon Backx <hi@simonbackx.com>
2021-12-09 00:57:12 +0100

    Platform dependent back buttons

--- frontend/shared/components/src/navigation/BackButton.vue
+++ frontend/shared/components/src/navigation/BackButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button class="button text only-icon-tiny-smartphone" type="button" @click="$emit('click', $event)">
-        <span class="icon arrow-left" />
+        <span class="icon arrow-back" />
         <span class="hide-android">Terug</span>
     </button>
 </template>
