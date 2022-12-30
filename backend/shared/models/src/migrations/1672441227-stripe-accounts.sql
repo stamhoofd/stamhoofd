f17063f20037de27b3e92d3725ede155d86c38b9
Simon Backx <hi@simonbackx.com>
2022-12-31 00:15:42 +0100

    Updated stripe migrations

--- backend/shared/models/src/migrations/1672441227-stripe-accounts.sql
+++ backend/shared/models/src/migrations/1672441227-stripe-accounts.sql
@@ -10,21 +10,3 @@ CREATE TABLE `stripe_accounts` (
   KEY `organizationId` (`organizationId`),
   CONSTRAINT `stripe_accounts_ibfk_1` FOREIGN KEY (`organizationId`) REFERENCES `organizations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `stripe_payment_intents` (
-  `id` varchar(36) NOT NULL,
-  `paymentId` varchar(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
-  `stripeIntentId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `organizationId` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `stripeIntentId` (`stripeIntentId`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `stripe_checkout_sessions` (
-  `id` varchar(36) NOT NULL,
-  `paymentId` varchar(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
-  `stripeSessionId` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `organizationId` varchar(36) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `stripeIntentId` (`stripeSessionId`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
