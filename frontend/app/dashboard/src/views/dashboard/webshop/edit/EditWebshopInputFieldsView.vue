92757d7f7511cd0ebefd77af43b567cc90f81d6b
Simon Backx <simon@stamhoofd.be>
2023-11-19 10:48:58 +0100

    Added question list warning and optional description

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/EditWebshopInputFieldsView.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/EditWebshopInputFieldsView.vue
@@ -2,6 +2,11 @@
     <SaveView :title="viewTitle" :loading="saving" :disabled="!hasChanges" @save="save">
         <h1>{{ viewTitle }}</h1>
         <p>Je kan zelf nog open vragen stellen (bv. 'naam lid') op bestelniveau (je kan dat ook doen per artikel, maar daarvoor moet je het artikel bewerken). Kies dus verstandig of je het bij een artikel ofwel op bestelniveau toevoegt! Op bestelniveau wordt het maar één keer gevraagd voor de volledige bestelling.</p>
+        
+        <p class="warning-box">
+            Deze functie is verouderd. Als je alle vrije invoervelden wist, en daarna opslaat, kan je gebruik maken van uitgebreidere vragenlijsten.
+        </p>
+
         <STErrorsDefault :error-box="errorBox" />
         <WebshopFieldsBox :fields="fields" @patch="addFieldsPatch" />
     </SaveView>
