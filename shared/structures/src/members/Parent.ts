59a6c24eb264d48cf95a724a090e485355fa106a
Simon Backx <simon@stamhoofd.be>
2023-11-19 10:48:58 +0100

    Improved importing members

--- shared/structures/src/members/Parent.ts
+++ shared/structures/src/members/Parent.ts
@@ -48,18 +48,23 @@ export class Parent extends AutoEncoder {
      */
     cleanData() {
         if (StringCompare.isFullCaps(this.firstName)) {
-            this.firstName = Formatter.capitalizeWords(this.firstName.toLowerCase())
+            this.firstName = Formatter.capitalizeWords(Formatter.removeDuplicateSpaces(this.firstName.toLowerCase()))
         }
         if (StringCompare.isFullCaps(this.lastName)) {
-            this.lastName = Formatter.capitalizeWords(this.lastName.toLowerCase())
+            this.lastName = Formatter.capitalizeWords(Formatter.removeDuplicateSpaces(this.lastName.toLowerCase()))
         }
 
         if (this.email) {
             this.email = this.email.toLowerCase()
         }
 
-        this.firstName = Formatter.capitalizeFirstLetter(this.firstName.trim())
-        this.lastName = this.lastName.trim()
+        this.firstName = Formatter.capitalizeFirstLetter(Formatter.removeDuplicateSpaces(this.firstName.trim()))
+        this.lastName = Formatter.removeDuplicateSpaces(this.lastName.trim())
+
+        if (this.lastName === this.lastName.toLocaleLowerCase()) {
+            // Add auto capitals
+            this.lastName = Formatter.capitalizeWords(this.lastName)
+        }
 
         this.address?.cleanData()
     }
