3eeb065bcb319d517af41080ab87c5698a00f9c7
Simon Backx <simon@stamhoofd.be>
2024-01-27 13:02:18 +0100

    Improved local development with dns

--- shared/structures/src/webshops/Webshop.ts
+++ shared/structures/src/webshops/Webshop.ts
@@ -71,11 +71,7 @@ export class WebshopPreview extends AutoEncoder {
 
         return this.getDefaultUrl(organization)
     }
-
-    getCanonicalUrl(organization: Organization): string {
-        return (STAMHOOFD.domains.marketing[organization.address.country] ?? STAMHOOFD.domains.marketing[""])+"/"+STAMHOOFD.domains.webshopPrefix+this.getDefaultSuffix()
-    }
-
+    
     getLegacyUrl(organization: Organization): string | null {
         if (this.legacyUri === null) {
             return null
@@ -231,10 +227,6 @@ export class Webshop extends AutoEncoder {
         return this.getDefaultUrl(organization)
     }
     
-    getCanonicalUrl(organization: Organization): string {
-        return (STAMHOOFD.domains.marketing[organization.address.country] ?? STAMHOOFD.domains.marketing[""])+"/"+STAMHOOFD.domains.webshopPrefix+this.getDefaultSuffix()
-    }
-
     getLegacyUrl(organization: Organization): string | null {
         if (this.legacyUri === null) {
             return null
