c80b8dda9fd888c9d45afe2587b07ef5196a4388
Simon Backx <simon@stamhoofd.be>
2024-05-19 19:05:48 +0200

    Added PriceInput emit type

--- frontend/shared/components/src/inputs/PriceInput.vue
+++ frontend/shared/components/src/inputs/PriceInput.vue
@@ -30,7 +30,11 @@
 <script lang="ts">
 import { Component, Prop,Vue, Watch } from "@simonbackx/vue-app-navigation/classes";
 
-@Component
+@Component({
+    emits: {
+        'update:modelValue': (_value: number | null) => true
+    }
+})
 export default class PriceInput extends Vue {
     /** Price in cents */
     @Prop({ default: 0 })
