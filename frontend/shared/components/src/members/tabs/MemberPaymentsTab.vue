df090447ecd084998fdfd793c7194d24e4695add
Simon Backx <simon@stamhoofd.be>
2024-05-20 21:24:47 +0200

    Added member tabs

---
+++ frontend/shared/components/src/members/tabs/MemberPaymentsTab.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="member-payments-view">
+        <main class="container">
+            Betalingen (todo)
+        </main>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
