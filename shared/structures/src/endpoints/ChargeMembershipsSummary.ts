a4cb2ac236a45dc49657713e36262646432eb595
Simon Backx <simon@stamhoofd.be>
2024-07-15 12:50:07 +0200

    Added charge membership per type details

--- shared/structures/src/endpoints/ChargeMembershipsSummary.ts
+++ shared/structures/src/endpoints/ChargeMembershipsSummary.ts
@@ -1,4 +1,18 @@
-import { AutoEncoder, IntegerDecoder, field } from "@simonbackx/simple-encoding";
+import { AutoEncoder, IntegerDecoder, MapDecoder, StringDecoder, field } from "@simonbackx/simple-encoding";
+
+export class ChargeMembershipsTypeSummary extends AutoEncoder {
+    @field({ decoder: IntegerDecoder })
+    memberships = 0;
+
+    @field({ decoder: IntegerDecoder })
+    members = 0;
+
+    @field({ decoder: IntegerDecoder })
+    price = 0;
+
+    @field({ decoder: IntegerDecoder })
+    organizations = 0;
+}
 
 export class ChargeMembershipsSummary extends AutoEncoder {
     @field({ decoder: IntegerDecoder })
@@ -12,4 +26,7 @@ export class ChargeMembershipsSummary extends AutoEncoder {
 
     @field({ decoder: IntegerDecoder })
     organizations = 0;
+
+    @field({ decoder: new MapDecoder(StringDecoder, ChargeMembershipsTypeSummary) })
+    membershipsPerType = new Map<string, ChargeMembershipsTypeSummary>();
 }
