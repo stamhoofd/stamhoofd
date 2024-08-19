7ca0760de5a5e522e7a7fa03e763f12957c24e2e
Simon Backx <simon@stamhoofd.be>
2024-08-19 15:26:21 +0200

    Fixed all excel columns enabled by default

--- frontend/shared/excel-export/src/SelectableColumn.ts
+++ frontend/shared/excel-export/src/SelectableColumn.ts
@@ -1,17 +1,17 @@
 export class SelectableColumn {
-    enabled: boolean;
+    enabled: boolean = true
     id: string;
 
     name: string;
-    description: string;
-    category?: string
+    description: string = ''
+    category?: string|null = null
 
     constructor(data: {
         enabled?: boolean;
         id: string;
         name: string;
         description?: string;
-        category?: string;
+        category?: string|null;
     }) {
         Object.assign(this, data);
     }
