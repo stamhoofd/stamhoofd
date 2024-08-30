0d5d2dd39ffff98dff3808bf6b9fafc6d04815a4
Simon Backx <simon@stamhoofd.be>
2024-08-30 20:56:55 +0200

    Made update membership seed silent

--- backend/app/api/src/seeds/1722344160-update-membership.ts
+++ backend/app/api/src/seeds/1722344160-update-membership.ts
@@ -1,4 +1,5 @@
 import { Migration } from '@simonbackx/simple-database';
+import { logger } from '@simonbackx/simple-logging';
 import { Member } from '@stamhoofd/models';
 
 export default new Migration(async () => {
@@ -16,38 +17,40 @@ export default new Migration(async () => {
     let c = 0;
     let id: string = '';
 
-    while(true) {
-        const rawMembers = await Member.where({
-            id: {
-                value: id,
-                sign: '>'
+    await logger.setContext({tags: ['silent-seed', 'seed']}, async () => {
+        while(true) {
+            const rawMembers = await Member.where({
+                id: {
+                    value: id,
+                    sign: '>'
+                }
+            }, {limit: 500, sort: ['id']});
+
+            if (rawMembers.length === 0) {
+                break;
             }
-        }, {limit: 500, sort: ['id']});
 
-        if (rawMembers.length === 0) {
-            break;
-        }
+            const promises: Promise<any>[] = [];
+            
 
-        const promises: Promise<any>[] = [];
+            for (const member of rawMembers) {
+                promises.push((async () => {
+                    await Member.updateMembershipsForId(member.id, true);
+                    c++;
         
+                    if (c%1000 === 0) {
+                        process.stdout.write('.');
+                    }
+                    if (c%10000 === 0) {
+                        process.stdout.write('\n');
+                    }
+                })())
+            }
 
-        for (const member of rawMembers) {
-            promises.push((async () => {
-                await Member.updateMembershipsForId(member.id, true);
-                c++;
-    
-                if (c%1000 === 0) {
-                    process.stdout.write('.');
-                }
-                if (c%10000 === 0) {
-                    process.stdout.write('\n');
-                }
-            })())
+            await Promise.all(promises);
+            id = rawMembers[rawMembers.length - 1].id;
         }
-
-        await Promise.all(promises);
-        id = rawMembers[rawMembers.length - 1].id;
-    }
+    })
 
     // Do something here
     return Promise.resolve()
