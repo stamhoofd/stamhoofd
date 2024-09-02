18b25bc9df8f4a0d119520a1ed13d491462ce7d3
Bjarne <bjarne.backx@gmail.com>
2024-09-02 13:48:29 +0200

    Removed dollar signs

--- frontend/app/admin/src/views/settings/membership-types/components/PlatformMembershipTypeReduceablePriceEditRow.vue
+++ frontend/app/admin/src/views/settings/membership-types/components/PlatformMembershipTypeReduceablePriceEditRow.vue
@@ -1,5 +1,5 @@
 <template>
-    <ReduceablePriceInput v-model="model" :title="$priceTitle" :validator="validator" :error-box="errorBox">
+    <ReduceablePriceInput v-model="model" :title="priceTitle" :validator="validator" :error-box="errorBox">
         <template #end>
             <slot />
         </template>
@@ -20,9 +20,9 @@ const props = defineProps<{
 
 const model = defineModel<ReduceablePrice>({ required: true });
 
-const $platform = usePlatform();
-const tag = $platform.value.config.tags.find(t => t.id === props.tagId);
-const $priceTitle = computed(() => format(props.showPricePerDay ? 'Vaste prijs' : 'Prijs'));
+const platform = usePlatform();
+const tag = platform.value.config.tags.find(t => t.id === props.tagId);
+const priceTitle = computed(() => format(props.showPricePerDay ? 'Vaste prijs' : 'Prijs'));
 
 function format(base: string) {
     if(!props.tagId) {
