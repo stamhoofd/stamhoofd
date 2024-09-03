127a92ad24ca3f013340bfa6c6e02e8278baee90
Simon Backx <simon@stamhoofd.be>
2024-09-03 13:20:54 +0200

    Fixed message formatting

--- frontend/app/dashboard/src/views/dashboard/groups/GroupOverview.vue
+++ frontend/app/dashboard/src/views/dashboard/groups/GroupOverview.vue
@@ -17,7 +17,7 @@
                 Deze groep staat in een categorie die enkel zichtbaar is voor beheerders of leden met een functie
             </p>
             <p v-if="!isArchive && !isOpen" class="info-box">
-                Inschrijvingen zijn momenteel gesloten via het ledenportaal. <span v-if="hasFullPermissions">Stel een datum in waarop ze moeten openen, of open de inschrijvingen manueel via de knoppen onderaan.</span>
+                Inschrijvingen zijn momenteel gesloten via het ledenportaal. <template v-if="hasFullPermissions">Stel een datum in waarop ze moeten openen, of open de inschrijvingen manueel via de knoppen onderaan.</template>
             </p>
 
             <BillingWarningBox filter-types="members" class="data-table-prefix" />
