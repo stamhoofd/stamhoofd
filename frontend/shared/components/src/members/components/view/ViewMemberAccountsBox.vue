f503938d17fb7ac57aa620c649f77902a575c6d1
Bjarne <bjarne.backx@gmail.com>
2024-09-04 17:26:31 +0200

    Fixed security code not always visible
    Fixes STA-184

--- frontend/shared/components/src/members/components/view/ViewMemberAccountsBox.vue
+++ frontend/shared/components/src/members/components/view/ViewMemberAccountsBox.vue
@@ -1,63 +1,58 @@
 <template>
-    <div v-if="member.patchedMember.users.length > 0" class="hover-box container">
-        <hr>
-        <h2 class="style-with-button">
-            <span class="icon-spacer">Accounts</span>
-            <a 
-                v-if="!$isTouch"
-                class="button icon gray help"
-                target="_blank"
-                :href="$domains.getDocs('leden-beheren-met-meerdere-ouders')"
-            />
-        </h2>
-
-        <STList>
-            <STListItem v-for="user in sortedUsers" :key="user.id" class="hover-box">
-                <template v-if="user.hasAccount && user.verified" #left>
-                    <span class="icon user small" />
-                </template>
-                <template v-else-if="user.hasAccount && !user.verified" #left>
-                    <span v-tooltip="'Deze gebruiker moet het e-mailadres nog verifiëren.'" class="icon email small" />
-                </template>
-                <template v-else #left>
-                    <span v-tooltip="'Deze gebruiker moet eerst registreren op dit emailadres en daarbij een wachtwoord instellen.'" class="icon email small" />
-                </template>
-
-                <template v-if="(user.firstName || user.lastName) && (user.name !== member.patchedMember.name)">
-                    <h3 v-if="user.firstName || user.lastName" class="style-title-list">
-                        {{ user.firstName }} {{ user.lastName }}
-                    </h3>
-                    <p class="style-description-small">
+    <div v-if="member.patchedMember.users.length > 0 || member.patchedMember.details.securityCode" class="hover-box container">
+        <template v-if="member.patchedMember.users.length > 0">
+            <hr>
+            <h2 class="style-with-button">
+                <span class="icon-spacer">Accounts</span>
+                <a
+                    v-if="!$isTouch"
+                    class="button icon gray help"
+                    target="_blank"
+                    :href="$domains.getDocs('leden-beheren-met-meerdere-ouders')"
+                />
+            </h2>
+            <STList>
+                <STListItem v-for="user in sortedUsers" :key="user.id" class="hover-box">
+                    <template v-if="user.hasAccount && user.verified" #left>
+                        <span class="icon user small" />
+                    </template>
+                    <template v-else-if="user.hasAccount && !user.verified" #left>
+                        <span v-tooltip="'Deze gebruiker moet het e-mailadres nog verifiëren.'" class="icon email small" />
+                    </template>
+                    <template v-else #left>
+                        <span v-tooltip="'Deze gebruiker moet eerst registreren op dit emailadres en daarbij een wachtwoord instellen.'" class="icon email small" />
+                    </template>
+                    <template v-if="(user.firstName || user.lastName) && (user.name !== member.patchedMember.name)">
+                        <h3 v-if="user.firstName || user.lastName" class="style-title-list">
+                            {{ user.firstName }} {{ user.lastName }}
+                        </h3>
+                        <p class="style-description-small">
+                            {{ user.email }}
+                        </p>
+                    </template>
+                    <h3 v-else class="style-title-list">
                         {{ user.email }}
+                    </h3>
+                    <p v-if="!user.hasAccount" class="style-description-small">
+                        Kan registreren
                     </p>
-                </template>
-                <h3 v-else class="style-title-list">
-                    {{ user.email }}
-                </h3>
-
-                <p v-if="!user.hasAccount" class="style-description-small">
-                    Kan registreren
-                </p>
-
-                <p v-else-if="!user.verified" class="style-description-small">
-                    E-mailadres nog niet geverifieerd
-                </p>
-
-                <p v-if="user.memberId === member.id" class="style-description-small">
-                    Dit is een account van {{ member.patchedMember.firstName }} zelf
-                </p>
-
-                <p v-if="user.permissions && app !== 'registration'" class="style-description-small">
-                    Heeft toegang tot beheerdersportaal
-                </p>
-
-                <template v-if="app !== 'registration' && hasWrite && user.hasAccount" #right>
-                    <LoadingButton :loading="isDeletingUser(user)" class="hover-show">
-                        <button type="button" class="button icon trash" @click.stop="deleteUser(user)" />
-                    </LoadingButton>
-                </template>
-            </STListItem>
-        </STList>
+                    <p v-else-if="!user.verified" class="style-description-small">
+                        E-mailadres nog niet geverifieerd
+                    </p>
+                    <p v-if="user.memberId === member.id" class="style-description-small">
+                        Dit is een account van {{ member.patchedMember.firstName }} zelf
+                    </p>
+                    <p v-if="user.permissions && app !== 'registration'" class="style-description-small">
+                        Heeft toegang tot beheerdersportaal
+                    </p>
+                    <template v-if="app !== 'registration' && hasWrite && user.hasAccount" #right>
+                        <LoadingButton :loading="isDeletingUser(user)" class="hover-show">
+                            <button type="button" class="button icon trash" @click.stop="deleteUser(user)" />
+                        </LoadingButton>
+                    </template>
+                </STListItem>
+            </STList>
+        </template>
 
         <template v-if="member.patchedMember.details.securityCode">
             <hr>
