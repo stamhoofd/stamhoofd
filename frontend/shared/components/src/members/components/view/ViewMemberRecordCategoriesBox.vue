21bc46737e8188b0ce5bf96a8b25c6444a61c34f
Bjarne <bjarne.backx@gmail.com>
2024-09-11 15:02:36 +0200

    Fixed visibility child record categories
    fixes STA-234

--- frontend/shared/components/src/members/components/view/ViewMemberRecordCategoriesBox.vue
+++ frontend/shared/components/src/members/components/view/ViewMemberRecordCategoriesBox.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { PermissionLevel, PlatformMember } from '@stamhoofd/structures';
+import { PermissionLevel, PlatformMember, RecordCategory } from '@stamhoofd/structures';
 import { computed } from 'vue';
 import { useAuth, useOrganization } from '../../../hooks';
 import ViewMemberRecordCategoryBox from './ViewMemberRecordCategoryBox.vue';
@@ -22,13 +22,13 @@ const organization = useOrganization();
 const auth = useAuth();
 
 const recordCategories = computed(() => {
-    return props.member.getEnabledRecordCategories({
+    return RecordCategory.flattenCategories(props.member.getEnabledRecordCategories({
         checkPermissions: {
             permissions: auth.userPermissions, 
             level: PermissionLevel.Read
         },
         scopeOrganization: organization.value
-    });
+    }), props.member);
 })
 
 </script>
