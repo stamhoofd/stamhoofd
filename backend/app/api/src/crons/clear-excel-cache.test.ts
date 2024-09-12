2a07fd1a80d0b3de0d7d959f4538921db277f7a7
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2024-09-12 14:06:52 +0200

    Added cron to clear excel cache (#224)
    * Added clear excel cache function
    
    * Made clear excel async
    
    * Added run clear cache only once at night
    
    * Bumped types/node
    
    * Replaced path with parentPath

---
+++ backend/app/api/src/crons/clear-excel-cache.test.ts
@@ -0,0 +1,152 @@
+import { Dirent } from "fs";
+import fs from "fs/promises";
+import { clearExcelCacheHelper } from "./clear-excel-cache";
+
+const testPath = '/Users/user/project/backend/app/api/.cache';
+jest.mock("fs/promises");
+const fsMock =  jest.mocked(fs, true)
+
+
+describe("clearExcelCacheHelper", () => {
+
+    it('should only run between 3 and 6 AM', async () => {
+        //#region arrange
+        const shouldFail = [
+            new Date(2024,1,1,0,0),
+            new Date(2024,1,1,2,50),
+            new Date(2024,1,1,2,59, 59, 999),
+            new Date(2024,1,1,6,0)
+        ]
+
+        const shouldPass = [
+            new Date(2024,1,1,3,0),
+            new Date(2024,1,1,3,55),
+            new Date(2024,1,1,5,59, 59, 999)
+        ]
+
+        fsMock.readdir.mockReturnValue(Promise.resolve([]));
+        //#endregion
+
+        //#region act and assert
+        for(const date of shouldFail) {
+            const didClear = await clearExcelCacheHelper({
+                lastExcelClear: null,
+                now: date,
+                cachePath: testPath,
+                environment: 'production'
+            })
+
+            expect(didClear).toBeFalse();
+        }
+
+        for(const date of shouldPass) {
+            const didClear = await clearExcelCacheHelper({
+                lastExcelClear: null,
+                now: date,
+                cachePath: testPath,
+                environment: 'production'
+            })
+
+            expect(didClear).toBeTrue();
+        }
+        //#endregion
+    })
+
+    it('should only run once each day', async () => {
+        //#region arrange
+        const firstTry  = new Date(2024,1,1,3,0);
+        const secondTry = new Date(2024,1,1,3,5);
+        const thirdTry  = new Date(2024,1,2,3,0);
+        const fourthTry  = new Date(2024,1,2,3,5);
+
+        fsMock.readdir.mockReturnValue(Promise.resolve([]));
+        //#endregion
+
+        //#region act and assert
+
+        // second try, should fail because 5 min earlier the cache was cleared
+        const didClearSecondTry = await clearExcelCacheHelper({
+            lastExcelClear: firstTry.getTime(),
+            now: secondTry,
+            cachePath: testPath,
+            environment: 'production'
+        });
+
+        expect(didClearSecondTry).toBeFalse();
+
+        // third try, should pass because the last clear was more than a day ago
+        const didClearThirdTry = await clearExcelCacheHelper({
+            lastExcelClear: secondTry.getTime(),
+            now: thirdTry,
+            cachePath: testPath,
+            environment: 'production'
+        });
+
+        expect(didClearThirdTry).toBeTrue();
+
+        // fourth try, should fail because 5 min earlier the cache was cleared
+        const didClearFourthTry = await clearExcelCacheHelper({
+            lastExcelClear: thirdTry.getTime(),
+            now: fourthTry,
+            cachePath: testPath,
+            environment: 'production'
+        });
+
+        expect(didClearFourthTry).toBeFalse();
+        //#endregion
+    })
+
+    it('should delete old cache only', async () => {
+        //#region arrange
+        const now = new Date(2024,0,5,3,5);
+
+        const dir1 = new Dirent();
+        dir1.name = '2024-01-01';
+
+        const dir2 = new Dirent();
+        dir2.name = '2024-01-02';
+
+        const dir3 = new Dirent();
+        dir3.name = '2024-01-03';
+
+        const dir4 = new Dirent();
+        dir4.name = '2024-01-04';
+
+        const dir5 = new Dirent();
+        dir5.name = '2024-01-05';
+
+        const dir6 = new Dirent();
+        dir6.name = 'not-a-date';
+
+        const dir7 = new Dirent();
+        dir7.name = 'noDate';
+
+        const file1 = new Dirent();
+        file1.name = 'some-file';
+        file1.parentPath = testPath;
+        file1.isDirectory = () => false;
+
+        const directories = [dir1, dir2, dir3, dir4, dir5, dir6, dir7];
+
+        directories.forEach(dir => {
+            dir.parentPath = testPath;
+            dir.isDirectory = () => true;
+        })
+
+        fsMock.readdir.mockReturnValue(Promise.resolve([...directories, file1]));
+        //#endregion
+
+        // act
+        await clearExcelCacheHelper({
+            lastExcelClear: null,
+            now,
+            cachePath: testPath,
+            environment: 'production'
+        });
+
+        // assert
+        expect(fsMock.rm).toHaveBeenCalledTimes(2);
+        expect(fsMock.rm).toHaveBeenCalledWith("/Users/user/project/backend/app/api/.cache/2024-01-01", { recursive: true, force: true });
+        expect(fsMock.rm).toHaveBeenCalledWith("/Users/user/project/backend/app/api/.cache/2024-01-02",{ recursive: true, force: true });
+    })
+})
---
+++ backend/app/api/src/crons/clear-excel-cache.ts
@@ -0,0 +1,92 @@
+import fs from "fs/promises";
+
+const msIn22Hours = 79200000;
+let lastExcelClear: number | null = null;
+
+export async function clearExcelCache() {
+    const now = new Date();
+
+    const didClear = await clearExcelCacheHelper({
+        lastExcelClear,
+        now,
+        cachePath: STAMHOOFD.CACHE_PATH,
+        environment: STAMHOOFD.environment
+    });
+
+    if(didClear) {
+        lastExcelClear = now.getTime();
+    }
+}
+
+export async function clearExcelCacheHelper
+({lastExcelClear, now, cachePath, environment}: {lastExcelClear: number | null, now: Date, cachePath: string, environment: "production" | "development" | "staging" | "test"}): Promise<boolean> {
+    if (environment === "development") {
+        return false;
+    }
+
+    const hour = now.getHours();
+
+    // between 3 and 6 AM
+    if(hour < 3 || hour > 5) {
+        return false;
+    }
+
+    // only run once each day
+    if(lastExcelClear !== null && lastExcelClear + msIn22Hours > now.getTime()) {
+        return false;
+    }
+
+    const currentYear = now.getFullYear();
+    const currentMonth = now.getMonth();
+    const currentDay = now.getDate();
+
+    const maxDaysInCache = 2;
+
+    const dateLimit = new Date(currentYear, currentMonth, currentDay - maxDaysInCache, 0,0,0,0);
+
+    const files = await fs.readdir(cachePath, {withFileTypes: true});
+
+    for (const file of files) {
+        if (file.isDirectory()) {
+            try {
+                const date = getDateFromDirectoryName(file.name);
+                const shouldDelete = date < dateLimit;
+
+                if(shouldDelete) {
+                    const path = file.parentPath + '/' + file.name;
+                    await fs.rm(path, { recursive: true, force: true })
+                    console.log("Removed", path)
+                }
+            } catch(error) {
+                console.error(error);
+            }
+        }
+    }
+
+    return true;
+}
+
+function getDateFromDirectoryName(file: string): Date {
+    const parts = file.split('-');
+
+    if (parts.length != 3) {
+        throw new Error(`Invalid directory ${file} in cache.`);
+    }
+
+    const year = parseInt(parts[0]);
+    if(isNaN(year)) {
+        throw new Error(`Year is NAN for directory ${file} in cache.`);
+    }
+
+    const month = parseInt(parts[1]);
+    if(isNaN(month)) {
+        throw new Error(`Month is NAN for directory ${file} in cache.`);
+    }
+
+    const day = parseInt(parts[2]);
+    if(isNaN(day)) {
+        throw new Error(`Day is NAN for directory ${file} in cache.`);
+    }
+
+    return new Date(year, month - 1, day);
+}
