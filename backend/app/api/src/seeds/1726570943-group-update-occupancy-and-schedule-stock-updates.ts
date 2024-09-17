f56ce496d1ac5ca251e6cd86e06e0e810171f836
Bjarne <bjarne.backx@gmail.com>
2024-09-17 13:17:53 +0200

    Added schedule stock updates to seed
    fixes STA-239

--- backend/app/api/src/seeds/1726570943-group-update-occupancy-and-schedule-stock-updates.ts
+++ backend/app/api/src/seeds/1726570943-group-update-occupancy-and-schedule-stock-updates.ts
@@ -1,5 +1,5 @@
 import { Migration } from '@simonbackx/simple-database';
-import { Group } from '@stamhoofd/models';
+import { Group, Registration } from '@stamhoofd/models';
 
 export default new Migration(async () => {
     if (STAMHOOFD.environment == "test") {
@@ -13,8 +13,44 @@ export default new Migration(async () => {
     }
 
     process.stdout.write('\n');
+
+    //#region update occupancy
     let c = 0;
     let id: string = '';
+
+    while(true) {
+        const rawRegistrations = await Registration.where({
+            id: {
+                value: id,
+                sign: '>'
+            }
+        }, {limit: 100, sort: ['id']});
+
+        const registrations = await Registration.getByIDs(...rawRegistrations.map(r => r.id));
+
+        for (const registration of registrations) {
+            registration.scheduleStockUpdate();
+
+            c++;
+
+            if (c%1000 === 0) {
+                process.stdout.write('.');
+            }
+            if (c%10000 === 0) {
+                process.stdout.write('\n');
+            }
+        }
+
+        if (registrations.length === 0) {
+            break;
+        }
+
+        id = registrations[registrations.length - 1].id;
+    }
+    //#endregion
+
+    //#region schedule stock updates
+    id = '';
 
     while(true) {
         const rawGroups = await Group.where({
@@ -46,6 +82,7 @@ export default new Migration(async () => {
 
         id = groups[groups.length - 1].id;
     }
+    //#endregion
 
     // Do something here
     return Promise.resolve()
