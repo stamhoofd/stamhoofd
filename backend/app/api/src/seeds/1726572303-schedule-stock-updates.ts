1a7a90ae2c132a687a188d4f205aeffd6482349b
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2024-09-19 18:04:02 +0200

    Added seed to schedule stock updates (#228)

---
+++ backend/app/api/src/seeds/1726572303-schedule-stock-updates.ts
@@ -0,0 +1,51 @@
+import { Migration } from '@simonbackx/simple-database';
+import { Registration } from '@stamhoofd/models';
+
+export default new Migration(async () => {
+    if (STAMHOOFD.environment == "test") {
+        console.log("skipped in tests")
+        return;
+    }
+
+    if(STAMHOOFD.userMode !== "platform") {
+        console.log("skipped seed schedule-stock-updates because usermode not platform")
+        return;
+    }
+
+    process.stdout.write('\n');
+    let c = 0;
+    let id: string = '';
+
+    while(true) {
+        const rawRegistrations = await Registration.where({
+            id: {
+                value: id,
+                sign: '>'
+            }
+        }, {limit: 100, sort: ['id']});
+
+        const registrations = await Registration.getByIDs(...rawRegistrations.map(g => g.id));
+
+        for (const registration of registrations) {
+            registration.scheduleStockUpdate();
+
+            c++;
+
+            if (c%1000 === 0) {
+                process.stdout.write('.');
+            }
+            if (c%10000 === 0) {
+                process.stdout.write('\n');
+            }
+        }
+
+        if (registrations.length === 0) {
+            break;
+        }
+
+        id = registrations[registrations.length - 1].id;
+    }
+
+    // Do something here
+    return Promise.resolve()
+})
