14a75dece02b79cf43e410c9927152a24432546e
Bjarne <bjarne.backx@gmail.com>
2024-10-11 13:30:10 +0200

    Refactored add tags to organizations in backend

--- shared/structures/src/helpers/TagHelper.ts
+++ shared/structures/src/helpers/TagHelper.ts
@@ -56,56 +56,6 @@ export class TagHelper {
         return result;
     }
 
-    /**
-     *
-     * @param tagIds the list of tag ids of an organization
-     * @param platformTags the tags of the platform
-     * @returns the new array of tag ids after syncing with the platform:
-     * - new tag ids can be added if they recursively contain a tag from the original array
-     * - tags can be removed if they were
-     */
-    static getTagIdsAfterSyncWithPlatformTags(originalTagIds: string[], newTagIds: string[], platformTags: OrganizationTag[]) {
-        const map = this.createAutoAddTagMap(new Map(platformTags.map(t => [t.id, t])));
-        const manuallyAddedTags = this.getManuallyAddedTags(originalTagIds, map)
-            // filter out tags that were deleted
-            .filter(tagId => newTagIds.includes(tagId));
-
-        // add tags that were added
-        for (const newTag of newTagIds) {
-            if (!manuallyAddedTags.includes(newTag)) {
-                manuallyAddedTags.push(newTag);
-            }
-        }
-
-        return this.automaticallyAddTags(manuallyAddedTags, map);
-    }
-
-    /**
-     * Return an updated list of organization tag ids given the old and new tags
-     * @param organization the organization to update the tags for
-     * @param newAutoAddTagMap a map containing all the tag ids and where the value is a set of tag ids that should be added also if the organization contains the tag id, according to the new tags
-     * @param oldAutoAddTagMap a map containing all the tag ids and where the value is a set of tag ids that should be added also if the organization contains the tag id, according to the old tags
-     */
-    static getUpdatedOrganizationTagIds(tagIds: string[], newAutoAddTagMap: Map<string, Set<string>>, oldAutoAddTagMap: Map<string, Set<string>>) {
-        const manuallyAddedTags = this.getManuallyAddedTags(tagIds, oldAutoAddTagMap);
-        return this.automaticallyAddTags(manuallyAddedTags, newAutoAddTagMap);
-    }
-
-    static automaticallyAddTags(manuallyAddedTags: string[], autoAddTagMap: Map<string, Set<string>>) {
-        const resultSet = new Set<string>();
-
-        for (const tagId of manuallyAddedTags) {
-            const set = autoAddTagMap.get(tagId);
-            if (set) {
-                for (const tagToAdd of set.values()) {
-                    resultSet.add(tagToAdd);
-                }
-            }
-        }
-
-        return Array.from(resultSet);
-    }
-
     static getTagsThatWillBeAddedAutomatically(manuallyAddedTags: string[], autoAddTagMap: Map<string, Set<string>>) {
         const resultSet = new Set<string>();
 
