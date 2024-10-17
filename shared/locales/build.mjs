ebd525651824c374b72baff2a053f54931a3f0cd
Simon Backx <simon@stamhoofd.be>
2024-10-17 11:08:59 +0200

    Added Chiro translation namespace

--- shared/locales/build.mjs
+++ shared/locales/build.mjs
@@ -13,7 +13,7 @@ async function fileExists(file) {
     return false;
 }
 
-const namespaces = ['stamhoofd', 'digit', 'keeo'];
+const namespaces = ['stamhoofd', 'digit', 'keeo', 'chiro'];
 
 for (const namespace of namespaces) {
     // await fs.rm("./dist/"+namespace, { recursive: true, force: true })
