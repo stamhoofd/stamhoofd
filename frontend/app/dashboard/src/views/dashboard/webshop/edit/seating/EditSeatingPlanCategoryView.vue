0900aa349065d81cc885d8cda90a4bf9686d8c38
Bjarne <bjarne.backx@gmail.com>
2024-10-23 17:09:24 +0200

    Refactored EditSeatingPlanCategoryView

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/seating/EditSeatingPlanCategoryView.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/seating/EditSeatingPlanCategoryView.vue
@@ -7,8 +7,8 @@
             Categorie bewerken
         </h1>
 
-        <STErrorsDefault :error-box="errorBox" />
-        <STInputBox title="Naam" error-fields="name" :error-box="errorBox">
+        <STErrorsDefault :error-box="errors.errorBox" />
+        <STInputBox title="Naam" error-fields="name" :error-box="errors.errorBox">
             <input
                 ref="firstInput"
                 v-model="name"
@@ -19,7 +19,7 @@
             >
         </STInputBox>
 
-        <STInputBox title="Meer of minkost" error-fields="price" :error-box="errorBox">
+        <STInputBox title="Meer of minkost" error-fields="price" :error-box="errors.errorBox">
             <PriceInput v-model="price" placeholder="+ 0 euro" :min="null" />
         </STInputBox>
 
@@ -52,122 +52,90 @@
     </SaveView>
 </template>
 
-<script lang="ts">
-import { AutoEncoderPatchType, patchContainsChanges } from '@simonbackx/simple-encoding';
-import { NavigationMixin } from '@simonbackx/vue-app-navigation';
-import { CenteredMessage, Checkbox, ErrorBox, PriceInput, SaveView, STErrorsDefault, STInputBox, STList, STListItem, Validator } from '@stamhoofd/components';
-import { SeatingPlan, SeatingPlanCategory, Version } from '@stamhoofd/structures';
-import { Component, Mixins, Prop } from '@simonbackx/vue-app-navigation/classes';
+<script lang="ts" setup>
+import { AutoEncoderPatchType } from '@simonbackx/simple-encoding';
+import { usePop } from '@simonbackx/vue-app-navigation';
+import { CenteredMessage, Checkbox, PriceInput, SaveView, STErrorsDefault, STInputBox, STList, STListItem, useErrors, usePatch } from '@stamhoofd/components';
+import { SeatingPlan, SeatingPlanCategory } from '@stamhoofd/structures';
+import { computed } from 'vue';
 
-@Component({
-    components: {
-        SaveView,
-        STInputBox,
-        STErrorsDefault,
-        PriceInput,
-        Checkbox,
-        STList,
-        STListItem,
-    },
-})
-export default class EditSeatingPlanCategoryView extends Mixins(NavigationMixin) {
-    errorBox: ErrorBox | null = null;
-    validator = new Validator();
-
-    @Prop({ required: true })
+const props = defineProps<{
     seatingPlan: SeatingPlan;
-
-    @Prop({ required: true })
-    isNew!: boolean;
-
-    @Prop({ required: true })
+    isNew: boolean;
     category: SeatingPlanCategory;
-
-    patchSeatingPlan: AutoEncoderPatchType<SeatingPlan> = SeatingPlan.patch({});
-
-    /**
-     * If we can immediately save this product, then you can create a save handler and pass along the changes.
-     */
-    @Prop({ required: true })
+    // If we can immediately save this product, then you can create a save handler and pass along the changes.
     saveHandler: ((patch: AutoEncoderPatchType<SeatingPlan>) => void);
 
-    get patchedSeatingPlan() {
-        return this.seatingPlan.patch(this.patchSeatingPlan);
-    }
+}>();
 
-    get patchedCategory() {
-        const c = this.patchedSeatingPlan.categories.find(c => c.id === this.category.id);
-        if (c) {
-            return c;
-        }
-        return this.category;
-    }
+const pop = usePop();
+const errors = useErrors();
 
-    get name() {
-        return this.patchedCategory.name;
-    }
+const { patch: patchSeatingPlan, patched: patchedSeatingPlan, addPatch, hasChanges } = usePatch(props.seatingPlan);
 
-    set name(name: string) {
-        this.addCategoryPatch(SeatingPlanCategory.patch({ name }));
+const patchedCategory = computed(() => {
+    const c = patchedSeatingPlan.value.categories.find(c => c.id === props.category.id);
+    if (c) {
+        return c;
     }
+    return props.category;
+});
 
-    get price() {
-        return this.patchedCategory.price;
-    }
+const name = computed({
+    get: () => patchedCategory.value.name,
+    set: (name: string) => {
+        addCategoryPatch(SeatingPlanCategory.patch({ name }));
+    },
+});
 
-    set price(price: number) {
-        this.addCategoryPatch(SeatingPlanCategory.patch({ price }));
-    }
+const price = computed({
+    get: () => patchedCategory.value.price,
+    set: (price: number) => {
+        addCategoryPatch(SeatingPlanCategory.patch({ price }));
+    },
+});
 
-    get adminOnly() {
-        return this.patchedCategory.adminOnly;
-    }
+const adminOnly = computed({
+    get: () => patchedCategory.value.adminOnly,
+    set: (adminOnly: boolean) => {
+        addCategoryPatch(SeatingPlanCategory.patch({ adminOnly }));
+    },
+});
 
-    set adminOnly(adminOnly: boolean) {
-        this.addCategoryPatch(SeatingPlanCategory.patch({ adminOnly }));
-    }
-
-    addCategoryPatch(patch: AutoEncoderPatchType<SeatingPlanCategory>) {
-        const p = SeatingPlan.patch({ id: this.seatingPlan.id });
-        p.categories.addPatch(patch.patch({ id: this.category.id }));
-        this.addPatch(p);
-    }
-
-    addPatch(patch: AutoEncoderPatchType<SeatingPlan>) {
-        this.patchSeatingPlan = this.patchSeatingPlan.patch(patch);
-    }
-
-    async save() {
-        if (!await this.validator.validate()) {
-            return;
-        }
-        this.saveHandler(this.patchSeatingPlan);
-        this.pop({ force: true });
-    }
-
-    get isSingle() {
-        return this.patchedSeatingPlan.categories.length <= 1;
-    }
-
-    async deleteMe() {
-        if (!await CenteredMessage.confirm('Ben je zeker dat je deze categorie wilt verwijderen?', 'Verwijderen')) {
-            return;
-        }
-        const p = SeatingPlan.patch({});
-        p.categories.addDelete(this.category.id);
-        this.saveHandler(p);
-        this.pop({ force: true });
-    }
-
-    get hasChanges() {
-        return patchContainsChanges(this.patchSeatingPlan, this.seatingPlan, { version: Version });
-    }
-
-    async shouldNavigateAway() {
-        if (!this.hasChanges) {
-            return true;
-        }
-        return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
-    }
+function addCategoryPatch(patch: AutoEncoderPatchType<SeatingPlanCategory>) {
+    const p = SeatingPlan.patch({ id: props.seatingPlan.id });
+    p.categories.addPatch(patch.patch({ id: props.category.id }));
+    addPatch(p);
 }
+
+async function save() {
+    if (!await errors.validator.validate()) {
+        return;
+    }
+    props.saveHandler(patchSeatingPlan.value);
+    pop({ force: true })?.catch(console.error);
+}
+
+const isSingle = computed(() => patchedSeatingPlan.value.categories.length <= 1);
+
+async function deleteMe() {
+    if (!await CenteredMessage.confirm('Ben je zeker dat je deze categorie wilt verwijderen?', 'Verwijderen')) {
+        return;
+    }
+    const p = SeatingPlan.patch({});
+    p.categories.addDelete(props.category.id);
+    props.saveHandler(p);
+    pop({ force: true })?.catch(console.error);
+}
+
+async function shouldNavigateAway() {
+    if (!hasChanges.value) {
+        return true;
+    }
+    return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
+}
+
+defineExpose({
+    shouldNavigateAway,
+});
 </script>
