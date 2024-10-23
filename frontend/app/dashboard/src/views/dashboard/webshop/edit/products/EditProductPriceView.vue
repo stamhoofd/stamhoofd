8327ddd57bddfeee6d836866532094f07c0762ee
Bjarne <bjarne.backx@gmail.com>
2024-10-23 13:46:56 +0200

    Refactored EditProductPriceView

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/products/EditProductPriceView.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/products/EditProductPriceView.vue
@@ -7,9 +7,9 @@
             Prijskeuze bewerken
         </h1>
 
-        <STErrorsDefault :error-box="errorBox" />
+        <STErrorsDefault :error-box="errors.errorBox" />
 
-        <ProductPriceBox :product-price="patchedProductPrice" :product="patchedProduct" :error-box="errorBox" @patch="addPatch($event)" />
+        <ProductPriceBox :product-price="patchedProductPrice" :product="patched" :error-box="errors.errorBox" @patch="addPatch($event)" />
 
         <div v-if="!isNew" class="container">
             <hr>
@@ -25,84 +25,58 @@
     </SaveView>
 </template>
 
-<script lang="ts">
-import { AutoEncoderPatchType, patchContainsChanges } from '@simonbackx/simple-encoding';
-import { NavigationMixin } from '@simonbackx/vue-app-navigation';
-import { CenteredMessage, ErrorBox, SaveView, STErrorsDefault, STInputBox, Validator } from '@stamhoofd/components';
-import { Product, ProductPrice, Version } from '@stamhoofd/structures';
-import { Component, Mixins, Prop } from '@simonbackx/vue-app-navigation/classes';
+<script lang="ts" setup>
+import { AutoEncoderPatchType } from '@simonbackx/simple-encoding';
+import { usePop } from '@simonbackx/vue-app-navigation';
+import { CenteredMessage, SaveView, STErrorsDefault, useErrors, usePatch } from '@stamhoofd/components';
+import { Product, ProductPrice } from '@stamhoofd/structures';
 
+import { computed } from 'vue';
 import ProductPriceBox from './ProductPriceBox.vue';
 
-@Component({
-    components: {
-        SaveView,
-        STInputBox,
-        STErrorsDefault,
-        ProductPriceBox,
-    },
-})
-export default class EditProductPriceView extends Mixins(NavigationMixin) {
-    errorBox: ErrorBox | null = null;
-    validator = new Validator();
-
-    @Prop({ required: true })
+const props = defineProps<{
     productPrice: ProductPrice;
-
-    @Prop({ required: true })
-    isNew!: boolean;
-
-    @Prop({ required: true })
+    isNew: boolean;
     product: Product;
-
-    patchProduct: AutoEncoderPatchType<Product> = Product.patch({});
-
-    /**
-     * If we can immediately save this product, then you can create a save handler and pass along the changes.
-     */
-    @Prop({ required: true })
+    // If we can immediately save this product, then you can create a save handler and pass along the changes.
     saveHandler: ((patch: AutoEncoderPatchType<Product>) => void);
+}>();
+const errors = useErrors();
+const pop = usePop();
 
-    get patchedProduct() {
-        return this.product.patch(this.patchProduct);
-    }
+const { patch, hasChanges, patched, addPatch } = usePatch(props.product);
 
-    get patchedProductPrice() {
-        const c = this.patchedProduct.prices.find(c => c.id === this.productPrice.id);
-        if (c) {
-            return c;
-        }
-        return this.productPrice;
+const patchedProductPrice = computed(() => {
+    const c = patched.value.prices.find(c => c.id === props.productPrice.id);
+    if (c) {
+        return c;
     }
+    return props.productPrice;
+});
 
-    addPatch(patch: AutoEncoderPatchType<Product>) {
-        this.patchProduct = this.patchProduct.patch(patch);
-    }
-
-    save() {
-        this.saveHandler(this.patchProduct);
-        this.pop({ force: true });
-    }
-
-    async deleteMe() {
-        if (!await CenteredMessage.confirm('Ben je zeker dat je deze prijskeuze wilt verwijderen?', 'Verwijderen')) {
-            return;
-        }
-        const p = Product.patch({});
-        p.prices.addDelete(this.productPrice.id);
-        this.saveHandler(p);
-        this.pop({ force: true });
-    }
-
-    get hasChanges() {
-        return patchContainsChanges(this.patchProduct, this.product, { version: Version });
-    }
-
-    async shouldNavigateAway() {
-        if (!this.hasChanges) {
-            return true;
-        }
-        return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
-    }
+function save() {
+    props.saveHandler(patch.value);
+    pop({ force: true })?.catch(console.error);
 }
+
+async function deleteMe() {
+    if (!await CenteredMessage.confirm('Ben je zeker dat je deze prijskeuze wilt verwijderen?', 'Verwijderen')) {
+        return;
+    }
+    const p = Product.patch({});
+    p.prices.addDelete(props.productPrice.id);
+    props.saveHandler(p);
+    pop({ force: true })?.catch(console.error);
+}
+
+async function shouldNavigateAway() {
+    if (!hasChanges.value) {
+        return true;
+    }
+    return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
+}
+
+defineExpose({
+    shouldNavigateAway,
+});
 </script>
