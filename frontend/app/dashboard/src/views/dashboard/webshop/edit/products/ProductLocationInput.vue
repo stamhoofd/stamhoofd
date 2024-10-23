3b6f639a0b45110bdde4ec1541c086cbce577140
Bjarne <bjarne.backx@gmail.com>
2024-10-23 18:35:23 +0200

    Refactored ProductLocationInput

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/products/ProductLocationInput.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/products/ProductLocationInput.vue
@@ -1,82 +1,57 @@
 <template>
     <div class="product-location-input">
-        <STInputBox title="Locatienaam" error-fields="name" :error-box="errorBox">
+        <STInputBox title="Locatienaam" error-fields="name" :error-box="errors.errorBox">
             <input v-model="name" placeholder="bv. Gemeentelijke feestzaal" class="input">
         </STInputBox>
         <AddressInput v-model="address" title="Adres (optioneel)" :validator="validator" :required="false" />
     </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { SimpleError } from '@simonbackx/simple-errors';
-import { Component, Prop, VueComponent } from '@simonbackx/vue-app-navigation/classes';
-import { AddressInput, ErrorBox, STInputBox, Validator } from '@stamhoofd/components';
+import { AddressInput, ErrorBox, STInputBox, useErrors, useValidation, Validator } from '@stamhoofd/components';
 import { Address, ProductLocation } from '@stamhoofd/structures';
+import { computed } from 'vue';
 
-@Component({
-    components: {
-        STInputBox,
-        AddressInput,
-    },
-})
-export default class ProductLocationInput extends VueComponent {
-    /**
-     * Assign a validator if you want to offload the validation to components
-     */
-    @Prop({ default: null })
+const props = defineProps<{
+    // Assign a validator if you want to offload the validation to components
     validator: Validator | null;
+}>();
 
-    errorBox: ErrorBox | null = null;
+const errors = useErrors({ validator: props.validator });
+const model = defineModel<ProductLocation | null>({ default: null });
 
-    @Prop({ default: null })
-    modelValue: ProductLocation | null;
+useValidation(errors.validator, () => isValid());
 
-    mounted() {
-        if (this.validator) {
-            this.validator.addValidation(this, () => {
-                return this.isValid();
-            });
+const name = computed({
+    get: () => model.value?.name ?? '',
+    set: (name: string) => {
+        if (model.value) {
+            model.value = model.value.patch({ name });
         }
-    }
+    },
+});
 
-    unmounted() {
-        if (this.validator) {
-            this.validator.removeValidation(this);
+const address = computed({
+    get: () => model.value?.address ?? null,
+    set: (address: Address | null) => {
+        if (model.value) {
+            model.value = model.value.patch({ address });
         }
+    },
+});
+
+async function isValid(): Promise<boolean> {
+    await Promise.resolve();
+    if (name.value.length < 2) {
+        errors.errorBox = new ErrorBox(new SimpleError({
+            code: 'invalid_field',
+            field: 'name',
+            message: 'Vul een locatienaam in',
+        }));
+        return false;
     }
 
-    get name() {
-        return this.modelValue?.name ?? '';
-    }
-
-    set name(name: string) {
-        if (this.modelValue) {
-            this.$emit('update:modelValue', this.modelValue.patch({ name }));
-        }
-    }
-
-    get address() {
-        return this.modelValue?.address ?? null;
-    }
-
-    set address(address: Address | null) {
-        if (this.modelValue) {
-            this.$emit('update:modelValue', this.modelValue.patch({ address }));
-        }
-    }
-
-    async isValid(): Promise<boolean> {
-        await Promise.resolve();
-        if (this.name.length < 2) {
-            this.errorBox = new ErrorBox(new SimpleError({
-                code: 'invalid_field',
-                field: 'name',
-                message: 'Vul een locatienaam in',
-            }));
-            return false;
-        }
-
-        return true;
-    }
+    return true;
 }
 </script>
