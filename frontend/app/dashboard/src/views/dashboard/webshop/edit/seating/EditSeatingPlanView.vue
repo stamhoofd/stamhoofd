ad4c40073b9869ed5bdae8b5d59e56f32e4bb87b
Bjarne <bjarne.backx@gmail.com>
2024-10-23 15:00:42 +0200

    Refactored EditSeatingPlanView

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/seating/EditSeatingPlanView.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/seating/EditSeatingPlanView.vue
@@ -3,13 +3,13 @@
         <h1>
             {{ title }}
         </h1>
-        <STErrorsDefault :error-box="errorBox" />
+        <STErrorsDefault :error-box="errors.errorBox" />
 
         <p v-if="$isMobile" class="warning-box">
             We raden aan om een zaalplan op te bouwen via een computer of tablet met toetsenbord.
         </p>
 
-        <STInputBox title="Naam zaal" error-fields="name" :error-box="errorBox">
+        <STInputBox title="Naam zaal" error-fields="name" :error-box="errors.errorBox">
             <input
                 ref="firstInput"
                 v-model="name"
@@ -68,7 +68,7 @@
                 </div>
             </h2>
 
-            <STInputBox v-if="patchedSeatingPlan.sections.length > 1" title="Naam" :error-box="errorBox" :error-fields="'sections['+section.id+'].name'">
+            <STInputBox v-if="patchedSeatingPlan.sections.length > 1" title="Naam" :error-box="errors.errorBox" :error-fields="'sections['+section.id+'].name'">
                 <input
                     :value="section.name"
                     class="input"
@@ -76,14 +76,14 @@
                     :placeholder="'bv. Middenplein'"
                     autocomplete=""
                     enterkeyhint="next"
-                    @input="setSectionName(section, $event.target.value)"
+                    @input="setSectionName(section, ($event as any).target.value)"
                 >
             </STInputBox>
 
             <EditSeatingPlanSectionBox
                 :seating-plan="patchedSeatingPlan"
                 :seating-plan-section="section"
-                :validator="validator"
+                :validator="errors.validator"
                 @patch="addPatch($event)"
             />
         </div>
@@ -103,7 +103,7 @@
             <h2>Acties</h2>
 
             <STList class="illustration-list">
-                <STListItem :selectable="true" class="left-center" @click="downloadSettings(true)">
+                <STListItem :selectable="true" class="left-center" @click="downloadSettings">
                     <template #left>
                         <img src="@stamhoofd/assets/images/illustrations/box-download.svg">
                     </template>
@@ -126,273 +126,224 @@
     </SaveView>
 </template>
 
-<script lang="ts">
-import { AutoEncoderPatchType, patchContainsChanges, VersionBox } from '@simonbackx/simple-encoding';
+<script lang="ts" setup>
+import { AutoEncoderPatchType, VersionBox } from '@simonbackx/simple-encoding';
 import { SimpleError } from '@simonbackx/simple-errors';
-import { ComponentWithProperties, NavigationMixin } from '@simonbackx/vue-app-navigation';
-import { CenteredMessage, Checkbox, ErrorBox, LoadingButton, Radio, SaveView, STErrorsDefault, STInputBox, STList, STListItem, Toast, Validator } from '@stamhoofd/components';
+import { ComponentWithProperties, usePop, usePresent } from '@simonbackx/vue-app-navigation';
+import { CenteredMessage, Checkbox, ErrorBox, LoadingButton, SaveView, STErrorsDefault, STInputBox, STList, STListItem, Toast, useErrors, usePatch } from '@stamhoofd/components';
 import { PrivateWebshop, SeatingPlan, SeatingPlanCategory, SeatingPlanRow, SeatingPlanSection, Version, WebshopMetaData } from '@stamhoofd/structures';
 import { Formatter } from '@stamhoofd/utility';
-import { Component, Mixins, Prop } from '@simonbackx/vue-app-navigation/classes';
 
+import { computed, ref } from 'vue';
 import EditSeatingPlanCategoryView from './EditSeatingPlanCategoryView.vue';
 import EditSeatingPlanSectionBox from './EditSeatingPlanSectionBox.vue';
 
-@Component({
-    components: {
-        SaveView,
-        STInputBox,
-        STErrorsDefault,
-        Radio,
-        STList,
-        STListItem,
-        EditSeatingPlanSectionBox,
-        LoadingButton,
-        Checkbox,
-    },
-})
-export default class EditSeatingPlanView extends Mixins(NavigationMixin) {
-    errorBox: ErrorBox | null = null;
-    validator = new Validator();
-    saving = false;
-
-    @Prop({ required: true })
-    isNew!: boolean;
-
-    @Prop({ required: true })
-    seatingPlan!: SeatingPlan;
-
-    @Prop({ required: true })
+const props = defineProps<{
+    isNew: boolean;
+    seatingPlan: SeatingPlan;
     webshop: PrivateWebshop;
-
-    /// For now only used to update locations and times of other products that are shared
-    patchWebshop: AutoEncoderPatchType<PrivateWebshop> = PrivateWebshop.patch({});
-    patchSeatingPlan: AutoEncoderPatchType<SeatingPlan> = SeatingPlan.patch({ id: this.seatingPlan.id });
-
-    /**
-     * If we can immediately save this product, then you can create a save handler and pass along the changes.
-     */
-    @Prop({ required: true })
+    // If we can immediately save this product, then you can create a save handler and pass along the changes.
     saveHandler: (patch: AutoEncoderPatchType<PrivateWebshop>) => void | Promise<void>;
+}>();
 
-    get patchedWebshop() {
-        return this.webshop.patch(this.patchWebshop);
+const errors = useErrors();
+const present = usePresent();
+const pop = usePop();
+
+const saving = ref(false);
+
+const { patch: patchWebshop, addPatch: addWebshopPatch, hasChanges: hasWebshopChanges } = usePatch(props.webshop);
+const { patch: patchSeatingPlan, patched: patchedSeatingPlan, addPatch, hasChanges: hasSeatingPlanChanges } = usePatch(props.seatingPlan);
+const hasChanges = computed(() => hasWebshopChanges.value || hasSeatingPlanChanges.value);
+
+const title = props.isNew ? 'Nieuwe zaalplan maken' : 'Zaalplan aanpassen';
+
+const name = computed({
+    get: () => patchedSeatingPlan.value.name,
+    set: (value: string) => addPatch(SeatingPlan.patch({ name: value })),
+});
+
+const requireOptimalReservation = computed({
+    get: () => patchedSeatingPlan.value.requireOptimalReservation,
+    set: (value: boolean) => addPatch(SeatingPlan.patch({ requireOptimalReservation: value })),
+});
+
+function setSectionName(section: SeatingPlanSection, value: string) {
+    const p = SeatingPlan.patch({});
+    p.sections.addPatch(SeatingPlanSection.patch({ id: section.id, name: value }));
+    addPatch(p);
+}
+
+async function save() {
+    const isValid = await errors.validator.validate();
+    if (!isValid) {
+        return;
     }
 
-    get patchedSeatingPlan() {
-        return this.seatingPlan.patch(this.patchSeatingPlan);
+    if (name.value.length < 2) {
+        errors.errorBox = new ErrorBox(
+            new SimpleError({
+                code: 'invalid_field',
+                message: 'De naam van de zaal moet minstens 2 karakters lang zijn',
+                field: 'name',
+            }),
+        );
+        return;
     }
 
-    get organization() {
-        return this.$organization;
-    }
-
-    get title() {
-        if (this.isNew) {
-            return 'Nieuw zaalplan maken';
-        }
-        return 'Zaalplan aanpassen';
-    }
-
-    addPatch(patch: AutoEncoderPatchType<SeatingPlan>) {
-        this.patchSeatingPlan = this.patchSeatingPlan.patch(patch);
-    }
-
-    get name() {
-        return this.patchedSeatingPlan.name;
-    }
-
-    set name(value: string) {
-        this.addPatch(SeatingPlan.patch({ name: value }));
-    }
-
-    get requireOptimalReservation() {
-        return this.patchedSeatingPlan.requireOptimalReservation;
-    }
-
-    set requireOptimalReservation(value: boolean) {
-        this.addPatch(SeatingPlan.patch({ requireOptimalReservation: value }));
-    }
-
-    setSectionName(section: SeatingPlanSection, value: string) {
-        const p = SeatingPlan.patch({});
-        p.sections.addPatch(SeatingPlanSection.patch({ id: section.id, name: value }));
-        this.addPatch(p);
-    }
-
-    async save() {
-        const isValid = await this.validator.validate();
-        if (!isValid) {
-            return;
-        }
-
-        if (this.name.length < 2) {
-            this.errorBox = new ErrorBox(
-                new SimpleError({
-                    code: 'invalid_field',
-                    message: 'De naam van de zaal moet minstens 2 karakters lang zijn',
-                    field: 'name',
-                }),
-            );
-            return;
-        }
-
-        if (this.patchedSeatingPlan.sections.length > 1) {
-            // Check unique names and non empty
-            for (const section of this.patchedSeatingPlan.sections) {
-                if (section.name.length < 1) {
-                    this.errorBox = new ErrorBox(
-                        new SimpleError({
-                            code: 'invalid_field',
-                            message: 'De naam van een zone mag niet leeg zijn',
-                            field: 'sections[' + section.id + '].name',
-                        }),
-                    );
-                    return;
-                }
-                if (this.patchedSeatingPlan.sections.filter(s => s.name === section.name).length > 1) {
-                    this.errorBox = new ErrorBox(
-                        new SimpleError({
-                            code: 'invalid_field',
-                            message: 'De naam van een zone moet uniek zijn',
-                            field: 'sections[' + section.id + '].name',
-                        }),
-                    );
-                    return;
-                }
+    if (patchedSeatingPlan.value.sections.length > 1) {
+        // Check unique names and non empty
+        for (const section of patchedSeatingPlan.value.sections) {
+            if (section.name.length < 1) {
+                errors.errorBox = new ErrorBox(
+                    new SimpleError({
+                        code: 'invalid_field',
+                        message: 'De naam van een zone mag niet leeg zijn',
+                        field: 'sections[' + section.id + '].name',
+                    }),
+                );
+                return;
+            }
+            if (patchedSeatingPlan.value.sections.filter(s => s.name === section.name).length > 1) {
+                errors.errorBox = new ErrorBox(
+                    new SimpleError({
+                        code: 'invalid_field',
+                        message: 'De naam van een zone moet uniek zijn',
+                        field: 'sections[' + section.id + '].name',
+                    }),
+                );
+                return;
             }
         }
-
-        this.saving = true;
-
-        const p = PrivateWebshop.patch(this.patchWebshop);
-        const meta = WebshopMetaData.patch({});
-        meta.seatingPlans.addPatch(this.patchSeatingPlan);
-        const patchedWebshop = p.patch({ meta });
-
-        try {
-            await this.saveHandler(patchedWebshop);
-            this.pop({ force: true });
-        }
-        catch (e) {
-            this.errorBox = new ErrorBox(e);
-        }
-
-        this.saving = false;
     }
 
-    get hasChanges() {
-        return patchContainsChanges(this.patchSeatingPlan, this.seatingPlan, { version: Version }) || patchContainsChanges(this.patchWebshop, this.webshop, { version: Version });
+    saving.value = true;
+    const meta = WebshopMetaData.patch({});
+    meta.seatingPlans.addPatch(patchSeatingPlan.value);
+    addWebshopPatch({ meta });
+
+    try {
+        await props.saveHandler(patchWebshop.value);
+        pop({ force: true })?.catch(console.error);
+    }
+    catch (e) {
+        errors.errorBox = new ErrorBox(e);
     }
 
-    addSection() {
-        const section = SeatingPlanSection.create({
-            rows: [
-                SeatingPlanRow.create({
-                    label: 'A',
-                }),
-            ],
-        });
-        const patch = SeatingPlan.patch({});
-        patch.sections.addPut(section);
-        this.addPatch(patch);
+    saving.value = false;
+}
+
+function addSection() {
+    const section = SeatingPlanSection.create({
+        rows: [
+            SeatingPlanRow.create({
+                label: 'A',
+            }),
+        ],
+    });
+    const patch = SeatingPlan.patch({});
+    patch.sections.addPut(section);
+    addPatch(patch);
+}
+
+async function deleteSection(section: SeatingPlanSection) {
+    if (!(await CenteredMessage.confirm('Ben je zeker dat je deze zone wilt verwijderen?', 'Verwijderen'))) {
+        return;
     }
 
-    async deleteSection(section: SeatingPlanSection) {
-        if (!(await CenteredMessage.confirm('Ben je zeker dat je deze zone wilt verwijderen?', 'Verwijderen'))) {
-            return;
-        }
+    const patch = SeatingPlan.patch({});
+    patch.sections.addDelete(section.id);
+    addPatch(patch);
+}
 
-        const patch = SeatingPlan.patch({});
-        patch.sections.addDelete(section.id);
-        this.addPatch(patch);
+async function shouldNavigateAway() {
+    if (!hasChanges.value) {
+        return true;
     }
+    return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
+}
 
-    async shouldNavigateAway() {
-        if (!this.hasChanges) {
-            return true;
-        }
-        return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
+const downloadingSettings = ref(false);
+
+async function downloadSettings() {
+    if (downloadingSettings.value) {
+        return;
     }
+    downloadingSettings.value = true;
 
-    downloadingSettings = false;
+    try {
+        const versionBox = new VersionBox(patchedSeatingPlan.value);
 
-    async downloadSettings() {
-        if (this.downloadingSettings) {
-            return;
-        }
-        this.downloadingSettings = true;
+        // Create a clean JSON file
+        const string = JSON.stringify(versionBox.encode({ version: Version }), null, 2);
 
-        try {
-            const versionBox = new VersionBox(this.patchedSeatingPlan);
+        // Create a blob
+        const blob = new Blob([string], { type: 'application/json' });
 
-            // Create a clean JSON file
-            const string = JSON.stringify(versionBox.encode({ version: Version }), null, 2);
-
-            // Create a blob
-            const blob = new Blob([string], { type: 'application/json' });
-
-            // Zip the file
-            const JSZip = (await import(/* webpackChunkName: "jszip" */ 'jszip')).default;
-            const saveAs = (await import(/* webpackChunkName: "file-saver" */ 'file-saver')).default.saveAs;
-            const zip = new JSZip();
-            zip.file('plan.json', blob);
-            const zipBlob = await zip.generateAsync({ type: 'blob', compression: 'DEFLATE', compressionOptions: { level: 6 } });
-            saveAs(zipBlob, Formatter.fileSlug(this.patchedSeatingPlan.name) + '.plan');
-        }
-        catch (e) {
-            console.error(e);
-            new Toast('Er is iets misgelopen bij het downloaden van het zaalplan. Probeer om alles eerst op te slaan en daarna de pagina te herladen. Als het probleem zich blijft voordoen, neem dan contact met ons op.', 'error red');
-        }
-        finally {
-            this.downloadingSettings = false;
-        }
+        // Zip the file
+        const JSZip = (await import(/* webpackChunkName: "jszip" */ 'jszip')).default;
+        const saveAs = (await import(/* webpackChunkName: "file-saver" */ 'file-saver')).default.saveAs;
+        const zip = new JSZip();
+        zip.file('plan.json', blob);
+        const zipBlob = await zip.generateAsync({ type: 'blob', compression: 'DEFLATE', compressionOptions: { level: 6 } });
+        saveAs(zipBlob, Formatter.fileSlug(patchedSeatingPlan.value.name) + '.plan');
     }
-
-    addCategory() {
-        // We choose a short id
-        let id = this.patchedSeatingPlan.categories.length + 1;
-        while (this.patchedSeatingPlan.categories.find(c => c.id === id.toString())) {
-            id++;
-        }
-        const category = SeatingPlanCategory.create({
-            id: id.toString(),
-            name: 'Naamloos',
-        });
-        const patch = SeatingPlan.patch({});
-        patch.categories.addPut(category);
-
-        this.present({
-            components: [
-                new ComponentWithProperties(EditSeatingPlanCategoryView, {
-                    seatingPlan: this.patchedSeatingPlan.patch(patch),
-                    isNew: true,
-                    category,
-                    saveHandler: (p: AutoEncoderPatchType<SeatingPlan>) => {
-                        this.addPatch(patch.patch(p));
-                    },
-                }),
-            ],
-            modalDisplayStyle: 'sheet',
-        });
+    catch (e) {
+        console.error(e);
+        new Toast('Er is iets misgelopen bij het downloaden van het zaalplan. Probeer om alles eerst op te slaan en daarna de pagina te herladen. Als het probleem zich blijft voordoen, neem dan contact met ons op.', 'error red');
     }
-
-    editCategory(category: SeatingPlanCategory) {
-        this.present({
-            components: [
-                new ComponentWithProperties(EditSeatingPlanCategoryView, {
-                    seatingPlan: this.patchedSeatingPlan,
-                    isNew: false,
-                    category,
-                    saveHandler: (p: AutoEncoderPatchType<SeatingPlan>) => {
-                        this.addPatch(p);
-                    },
-                }),
-            ],
-            modalDisplayStyle: 'sheet',
-        });
+    finally {
+        downloadingSettings.value = false;
     }
 }
+
+function addCategory() {
+    // We choose a short id
+    let id = patchedSeatingPlan.value.categories.length + 1;
+    while (patchedSeatingPlan.value.categories.find(c => c.id === id.toString())) {
+        id++;
+    }
+    const category = SeatingPlanCategory.create({
+        id: id.toString(),
+        name: 'Naamloos',
+    });
+    const patch = SeatingPlan.patch({});
+    patch.categories.addPut(category);
+
+    present({
+        components: [
+            new ComponentWithProperties(EditSeatingPlanCategoryView, {
+                seatingPlan: patchedSeatingPlan.value.patch(patch),
+                isNew: true,
+                category,
+                saveHandler: (p: AutoEncoderPatchType<SeatingPlan>) => {
+                    addPatch(patch.patch(p));
+                },
+            }),
+        ],
+        modalDisplayStyle: 'sheet',
+    }).catch(console.error);
+}
+
+function editCategory(category: SeatingPlanCategory) {
+    present({
+        components: [
+            new ComponentWithProperties(EditSeatingPlanCategoryView, {
+                seatingPlan: patchedSeatingPlan.value,
+                isNew: false,
+                category,
+                saveHandler: (p: AutoEncoderPatchType<SeatingPlan>) => {
+                    addPatch(p);
+                },
+            }),
+        ],
+        modalDisplayStyle: 'sheet',
+    }).catch(console.error);
+}
+
+defineExpose({
+    shouldNavigateAway,
+});
 </script>
 
 <style lang="scss">
