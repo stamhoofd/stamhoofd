12b3c41976d61ceade0a6eae99433d03eb8d8033
Bjarne <bjarne.backx@gmail.com>
2024-10-28 14:56:54 +0100

    Refactored GraphDateRangeSelector

--- frontend/shared/components/src/views/GraphDateRangeSelector.vue
+++ frontend/shared/components/src/views/GraphDateRangeSelector.vue
@@ -1,43 +1,40 @@
 <template>
     <button class="button text" type="button" @click="chooseRange">
-        <span>{{ value ? value.name : '—' }}</span>
+        <span>{{ model ? model.name : '—' }}</span>
         <span class="icon arrow-down-small" />
     </button>
 </template>
 
-<script lang="ts">
-import { NavigationMixin } from "@simonbackx/vue-app-navigation";
-import { Component, Mixins,Prop } from "@simonbackx/vue-app-navigation/classes";
+<script lang="ts" setup>
 
-import { ContextMenu, ContextMenuItem } from "../overlays/ContextMenu";
-import { DateOption } from "./DateRange";
+import { ContextMenu, ContextMenuItem } from '../overlays/ContextMenu';
+import { DateOption } from './DateRange';
 
-@Component({})
-export default class GraphDateRangeSelector extends Mixins(NavigationMixin) {
-    @Prop({ required: true })
-    value: DateOption | null
+const props = withDefaults(defineProps<{
+    options?: DateOption[] | null;
+}>(), {
+    options: null,
+});
 
-    @Prop({ default: null })
-    options: DateOption[] | null
+const model = defineModel<DateOption | null>({ default: null });
 
-    chooseRange(event) {
-        if (!this.options) {
-            return
-        }
-        const contextMenu = new ContextMenu([
-            [
-                ...this.options.map(option => {
-                    return new ContextMenuItem({
-                        name: option.name,
-                        action: () => {
-                            this.$emit('update:modelValue', option);
-                            return true;
-                        }
-                    })
-                })
-            ]
-        ]);
-        contextMenu.show({ button: event.currentTarget, xPlacement: "left" }).catch(console.error);
+function chooseRange(event: MouseEvent) {
+    if (!props.options) {
+        return;
     }
+    const contextMenu = new ContextMenu([
+        [
+            ...props.options.map((option) => {
+                return new ContextMenuItem({
+                    name: option.name,
+                    action: () => {
+                        model.value = option;
+                        return true;
+                    },
+                });
+            }),
+        ],
+    ]);
+    contextMenu.show({ button: event.currentTarget as HTMLElement, xPlacement: 'left' }).catch(console.error);
 }
 </script>
