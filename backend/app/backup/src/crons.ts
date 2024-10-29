59504c32d86263e39343a1b3057f28a82ab02a09
Simon Backx <simon@stamhoofd.be>
2024-10-29 13:47:12 +0100

    Added single backup per day limit

--- backend/app/backup/src/crons.ts
+++ backend/app/backup/src/crons.ts
@@ -1,9 +1,28 @@
 import { registerCron } from '@stamhoofd/crons';
 import { backup, backupBinlogs } from './helpers/backup';
+import { Formatter } from '@stamhoofd/utility';
+
+let lastFullBackup: Date | null = null;
+const backupStartTime = '02:00';
+const backupEndTime = '05:00';
 
 async function createBackups() {
-    console.log('Creating backups...');
+    const now = new Date();
+
+    if (lastFullBackup && Formatter.date(now, true) === Formatter.date(lastFullBackup, true)) {
+        return;
+    }
+
+    // Check time
+    if (Formatter.timeIso(now) < backupStartTime || Formatter.timeIso(now) > backupEndTime) {
+        return;
+    }
+
+    console.log('Creating full backup...');
     await backup();
+
+    console.log('Full backup created');
+    lastFullBackup = now;
 }
 
 async function backupBinLogs() {
