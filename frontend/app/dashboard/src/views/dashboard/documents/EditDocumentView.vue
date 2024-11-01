6a6cea69b799c56a2f0fcedc41840d4334b74c3b
Bjarne <bjarne.backx@gmail.com>
2024-11-01 17:03:25 +0100

    Refactored EditDocumentsView

--- frontend/app/dashboard/src/views/dashboard/documents/EditDocumentView.vue
+++ frontend/app/dashboard/src/views/dashboard/documents/EditDocumentView.vue
@@ -8,9 +8,9 @@
             Als je hier gegevens wijzigt, zullen die gegevens van dit document niet meer automatisch gekoppeld zijn aan de gegevens van de bijhorende leden en inschrijvingen. Meestal is het beter om de gegevens rechtstreeks bij het lid of de inschrijving te wijzigen.
         </p>
 
-        <STErrorsDefault :error-box="errorBox" />
+        <STErrorsDefault :error-box="errors.errorBox" />
 
-        <STInputBox title="Beschrijving" error-fields="description" :error-box="errorBox">
+        <STInputBox title="Beschrijving" error-fields="description" :error-box="errors.errorBox">
             <input
                 v-model="description"
                 class="input"
@@ -24,160 +24,132 @@
             <h2>{{ category.name }}</h2>
             <p v-if="category.description" class="style-description pre-wrap" v-text="category.description" />
 
-            <RecordAnswerInput v-for="record of category.filterRecords(true)" :key="record.id" :record-settings="record" :record-answers="editingAnswers" :validator="validator" />
+            <!-- todo: should records be filtered? -->
+            <RecordAnswerInput v-for="record of category.records" :key="record.id" :record="record" :answers="editingAnswers" :validator="errors.validator" />
         </div>
     </SaveView>
 </template>
 
-<script lang="ts">
-import { ArrayDecoder, Decoder, PatchableArray, PatchableArrayAutoEncoder, patchContainsChanges } from '@simonbackx/simple-encoding';
-import { Request } from '@simonbackx/simple-networking';
-import { NavigationMixin } from '@simonbackx/vue-app-navigation';
-import { CenteredMessage, ErrorBox, RecordAnswerInput, SaveView, STErrorsDefault, STInputBox, STList, STListItem, Validator } from '@stamhoofd/components';
-import { SessionManager } from '@stamhoofd/networking';
-import { Document, DocumentData, DocumentTemplatePrivate, RecordCategory, Version } from '@stamhoofd/structures';
-import { Component, Mixins, Prop, Watch } from '@simonbackx/vue-app-navigation/classes';
+<script lang="ts" setup>
+import { ArrayDecoder, AutoEncoder, Decoder, PatchableArray, PatchableArrayAutoEncoder } from '@simonbackx/simple-encoding';
+import { useDismiss } from '@simonbackx/vue-app-navigation';
+import { CenteredMessage, ErrorBox, RecordAnswerInput, SaveView, STErrorsDefault, STInputBox, useContext, useErrors, usePatch } from '@stamhoofd/components';
+import { useRequestOwner } from '@stamhoofd/networking';
+import { Document, DocumentData, DocumentTemplatePrivate, RecordCategory } from '@stamhoofd/structures';
+import { computed, ComputedRef, ref, watch } from 'vue';
 
-@Component({
-    components: {
-        STList,
-        STListItem,
-        SaveView,
-        STInputBox,
-        RecordAnswerInput,
-        STErrorsDefault,
-    },
-})
-export default class EditDocumentView extends Mixins(NavigationMixin) {
-    @Prop({ required: true })
-    isNew!: boolean;
+const props = defineProps<{
+    isNew: boolean;
+    document: Document;
+    template: DocumentTemplatePrivate;
+}>();
 
-    @Prop({ required: true })
-    document!: Document;
+const requestOwner = useRequestOwner();
+const context = useContext();
+const { patch: patchDocument, patched: patchedDocument, hasChanges } = usePatch(props.document);
+const errors = useErrors();
+const dismiss = useDismiss();
+const saving = ref(false);
+const editingAnswers = ref(cloneMap(props.document.data.fieldAnswers));
 
-    @Prop({ required: true })
-    template!: DocumentTemplatePrivate;
+// get definitions() {
+//     return [];
+// }
 
-    patchDocument = Document.patch({});
-    validator = new Validator();
-    errorBox: ErrorBox | null = null;
-    saving = false;
-    editingAnswers = this.document.data.fieldAnswers.map(a => a.clone());
+const fieldCategories = computed(() => {
+    return RecordCategory.flattenCategories([...props.template.privateSettings.templateDefinition.documentFieldCategories, ...props.template.privateSettings.templateDefinition.groupFieldCategories], {} as any);
+}) as ComputedRef<RecordCategory[]>;
 
-    get patchedDocument() {
-        return this.document.patch(this.patchDocument);
-    }
-
-    get definitions() {
-        return [];
-    }
-
-    get fieldCategories() {
-        return RecordCategory.flattenCategories([...this.template.privateSettings.templateDefinition.documentFieldCategories, ...this.template.privateSettings.templateDefinition.groupFieldCategories], {} as any);
-    }
-
-    @Watch('editingAnswers')
-    saveAnswers() {
-        for (const answer of this.editingAnswers) {
-            const previousAnswer = this.document.data.fieldAnswers.find(a => a.settings.id === answer.settings.id);
-            if (!previousAnswer || previousAnswer.stringValue !== answer.stringValue) {
-                answer.markReviewed();
-            }
+function saveAnswers() {
+    for (const answer of editingAnswers.value.values()) {
+        const previousAnswer = props.document.data.fieldAnswers.get(answer.settings.id);
+        if (!previousAnswer || previousAnswer.stringValue !== answer.stringValue) {
+            answer.markReviewed();
         }
-
-        this.patchDocument = this.patchDocument.patch({
-            data: DocumentData.patch({
-                fieldAnswers: this.editingAnswers as any,
-            }),
-        });
     }
 
-    get title() {
-        return this.isNew ? 'Nieuw document' : 'Document bewerken';
+    patchDocument.value = patchDocument.value.patch({
+        data: DocumentData.patch({
+            fieldAnswers: editingAnswers.value as any,
+        }),
+    });
+}
+
+watch(editingAnswers, () => saveAnswers());
+
+const title = props.isNew ? 'Nieuw document' : 'Document bewerken';
+const description = computed(() => patchedDocument.value.data.description);
+
+async function shouldNavigateAway() {
+    if (!hasChanges.value) {
+        return true;
+    }
+    return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
+}
+
+async function validate() {
+    // todo: validate information before continueing
+}
+
+async function save() {
+    if (saving.value) {
+        return;
     }
 
-    get description() {
-        return this.patchedDocument.data.description;
-    }
+    saving.value = true;
 
-    set description(value: string) {
-        this.patchDocument = this.patchDocument.patch({
-            data: DocumentData.patch({
-                description: value,
-            }),
-        });
-    }
+    try {
+        // Make sure answers are updated
+        saveAnswers();
 
-    get hasChanges() {
-        return patchContainsChanges(this.patchDocument, this.document, { version: Version });
-    }
-
-    async shouldNavigateAway() {
-        if (!this.hasChanges) {
-            return true;
-        }
-        return await CenteredMessage.confirm('Ben je zeker dat je wilt sluiten zonder op te slaan?', 'Niet opslaan');
-    }
-
-    async validate() {
-        // todo: validate information before continueing
-    }
-
-    beforeUnmount() {
-        Request.cancelAll(this);
-    }
-
-    async save() {
-        if (this.saving) {
+        if (!await errors.validator.validate()) {
+            saving.value = false;
             return;
         }
 
-        this.saving = true;
+        await validate();
 
-        try {
-            // Make sure answers are updated
-            this.saveAnswers();
+        const patch: PatchableArrayAutoEncoder<Document> = new PatchableArray() as PatchableArrayAutoEncoder<Document>;
 
-            if (!await this.validator.validate()) {
-                this.saving = false;
-                return;
-            }
-
-            await this.validate();
-
-            const patch: PatchableArrayAutoEncoder<Document> = new PatchableArray() as PatchableArrayAutoEncoder<Document>;
-
-            if (this.isNew) {
-                patch.addPut(this.patchedDocument);
-            }
-            else {
-                this.patchDocument.id = this.patchedDocument.id;
-                patch.addPatch(this.patchDocument);
-            }
-
-            const response = await this.$context.authenticatedServer.request({
-                method: 'PATCH',
-                path: '/organization/documents',
-                body: patch,
-                decoder: new ArrayDecoder(Document as Decoder<Document>),
-                shouldRetry: false,
-                owner: this,
-            });
-            const updatedDocument = response.data[0];
-            if (updatedDocument) {
-                this.editingAnswers = this.document.data.fieldAnswers.map(a => a.clone());
-                this.patchDocument = Document.patch({});
-                this.document.deepSet(updatedDocument);
-            }
-
-            this.dismiss({ force: true });
+        if (props.isNew) {
+            patch.addPut(patchedDocument.value);
         }
-        catch (e) {
-            console.error(e);
-            this.errorBox = new ErrorBox(e);
+        else {
+            patchDocument.value.id = patchedDocument.value.id;
+            patch.addPatch(patchDocument.value);
         }
 
-        this.saving = false;
+        const response = await context.value.authenticatedServer.request({
+            method: 'PATCH',
+            path: '/organization/documents',
+            body: patch,
+            decoder: new ArrayDecoder(Document as Decoder<Document>),
+            shouldRetry: false,
+            owner: requestOwner,
+        });
+        const updatedDocument = response.data[0];
+        if (updatedDocument) {
+            editingAnswers.value = cloneMap(props.document.data.fieldAnswers);
+            patchDocument.value = Document.patch({});
+            props.document.deepSet(updatedDocument);
+        }
+
+        dismiss({ force: true }).catch(console.error);
     }
+    catch (e) {
+        console.error(e);
+        errors.errorBox = new ErrorBox(e);
+    }
+
+    saving.value = false;
 }
+
+function cloneMap<K, T extends AutoEncoder>(map: Map<K, T>): Map<K, T> {
+    return new Map([...map.entries()]
+        .map(([key, value]) => [key, value.clone()]));
+}
+
+defineExpose({
+    shouldNavigateAway,
+});
 </script>
