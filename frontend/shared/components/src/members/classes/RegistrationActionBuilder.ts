5c217af2659845d475ce48c21e87186585cbeb07
Simon Backx <simon@stamhoofd.be>
2024-12-19 09:12:21 +0100

    Fixed formatting and improved copy

--- frontend/shared/components/src/members/classes/RegistrationActionBuilder.ts
+++ frontend/shared/components/src/members/classes/RegistrationActionBuilder.ts
@@ -1,23 +1,23 @@
-import { usePresent } from '@simonbackx/vue-app-navigation'
-import { SessionContext, useRequestOwner } from '@stamhoofd/networking'
-import { Group, GroupCategoryTree, Organization, PermissionLevel, PlatformMember, RegisterCheckout, RegisterItem, Registration, RegistrationWithMember } from '@stamhoofd/structures'
-import { checkoutRegisterItem, chooseOrganizationMembersForGroup } from '..'
-import { useContext, useOrganization } from '../../hooks'
-import { InMemoryTableAction, MenuTableAction, TableAction } from '../../tables/classes'
-import { PlatformFamilyManager, usePlatformFamilyManager } from '../PlatformFamilyManager'
+import { usePresent } from '@simonbackx/vue-app-navigation';
+import { SessionContext, useRequestOwner } from '@stamhoofd/networking';
+import { Group, GroupCategoryTree, Organization, PermissionLevel, PlatformMember, RegisterCheckout, RegisterItem, Registration, RegistrationWithMember } from '@stamhoofd/structures';
+import { checkoutRegisterItem, chooseOrganizationMembersForGroup } from '..';
+import { useContext, useOrganization } from '../../hooks';
+import { InMemoryTableAction, MenuTableAction, TableAction } from '../../tables/classes';
+import { PlatformFamilyManager, usePlatformFamilyManager } from '../PlatformFamilyManager';
 
 export function useRegistrationActionBuilder() {
-    const present = usePresent()
-    const context = useContext()
-    const platformFamilyManager = usePlatformFamilyManager()
-    const owner = useRequestOwner()
-    const organization = useOrganization()
+    const present = usePresent();
+    const context = useContext();
+    const platformFamilyManager = usePlatformFamilyManager();
+    const owner = useRequestOwner();
+    const organization = useOrganization();
 
-    return (options: {registration: Registration, member: PlatformMember}) => {
+    return (options: { registration: Registration; member: PlatformMember }) => {
         const org = options.member.organizations.find(o => o.id === options.registration.organizationId) ?? organization.value;
 
         if (!org) {
-            throw new Error("Organization not found for registration")
+            throw new Error('Organization not found for registration');
         }
 
         return new RegistrationActionBuilder({
@@ -27,54 +27,54 @@ export function useRegistrationActionBuilder() {
             organization: org,
             registrations: [options.registration],
             platformFamilyManager,
-            owner
-        })
-    }
+            owner,
+        });
+    };
 }
 
 export class RegistrationActionBuilder {
     /**
      * Determines which registrations will get moved or removed
      */
-    registrations: Registration[]
-    members: PlatformMember[]
-    organization: Organization
-    present: ReturnType<typeof usePresent>
-    context: SessionContext
-    platformFamilyManager: PlatformFamilyManager
-    owner: any
+    registrations: Registration[];
+    members: PlatformMember[];
+    organization: Organization;
+    present: ReturnType<typeof usePresent>;
+    context: SessionContext;
+    platformFamilyManager: PlatformFamilyManager;
+    owner: any;
 
     constructor(settings: {
-        present: ReturnType<typeof usePresent>,
-        context: SessionContext,
-        registrations: Registration[],
-        members: PlatformMember[],
-        organization: Organization,
-        platformFamilyManager: PlatformFamilyManager
-        owner: any
+        present: ReturnType<typeof usePresent>;
+        context: SessionContext;
+        registrations: Registration[];
+        members: PlatformMember[];
+        organization: Organization;
+        platformFamilyManager: PlatformFamilyManager;
+        owner: any;
     }) {
-        this.present = settings.present
-        this.context = settings.context
-        this.registrations = settings.registrations
-        this.members = settings.members
-        this.organization = settings.organization
-        this.platformFamilyManager = settings.platformFamilyManager
-        this.owner = settings.owner
+        this.present = settings.present;
+        this.context = settings.context;
+        this.registrations = settings.registrations;
+        this.members = settings.members;
+        this.organization = settings.organization;
+        this.platformFamilyManager = settings.platformFamilyManager;
+        this.owner = settings.owner;
     }
 
     get hasWrite() {
         for (const registration of this.registrations) {
             if (!this.context.auth.canAccessRegistration(registration, this.organization, PermissionLevel.Write)) {
-                return false
+                return false;
             }
         }
-        return true
+        return true;
     }
 
     getMoveAction(): TableAction<PlatformMember>[] {
         return [
             new MenuTableAction({
-                name: "Verplaatsen naar",
+                name: 'Verplaatsen naar',
                 priority: 1,
                 groupIndex: 5,
                 needsSelection: true,
@@ -82,51 +82,51 @@ export class RegistrationActionBuilder {
                 enabled: this.hasWrite,
                 childActions: () => [
                     new MenuTableAction({
-                        name: "Wachtlijsten",
+                        name: 'Wachtlijsten',
                         groupIndex: 0,
                         enabled: this.organization.period.waitingLists.length > 0,
                         childActions: () => [
-                            ...this.organization.period.waitingLists.map(g => {
+                            ...this.organization.period.waitingLists.map((g) => {
                                 return new InMemoryTableAction({
                                     name: g.settings.name,
                                     needsSelection: true,
                                     allowAutoSelectAll: false,
                                     handler: async () => {
-                                        await this.moveRegistrations(g)
-                                    }
-                                })
-                            })
-                        ]
+                                        await this.moveRegistrations(g);
+                                    },
+                                });
+                            }),
+                        ],
                     }),
-                    ...this.getActionsForCategory(this.organization.period.adminCategoryTree, (group) => this.moveRegistrations(group)).map(r => {
-                        r.description = this.organization.period.period.name
-                        return r
-                    })
-                ]
-            })
-        ]
+                    ...this.getActionsForCategory(this.organization.period.adminCategoryTree, group => this.moveRegistrations(group)).map((r) => {
+                        r.description = this.organization.period.period.name;
+                        return r;
+                    }),
+                ],
+            }),
+        ];
     }
 
     getEditAction(): TableAction<PlatformMember>[] {
         return [
             new InMemoryTableAction({
-                name: "Bewerk inschrijving",
+                name: 'Inschrijving bewerken',
                 priority: 1,
                 groupIndex: 1,
                 needsSelection: true,
                 allowAutoSelectAll: false,
                 enabled: this.hasWrite,
                 handler: async () => {
-                    await this.editRegistrations()
+                    await this.editRegistrations();
                 },
-                icon: 'edit'
-            })
-        ]
+                icon: 'edit',
+            }),
+        ];
     }
 
     getUnsubscribeAction(): TableAction<PlatformMember>[] {
         return [new InMemoryTableAction({
-            name: "Uitschrijven",
+            name: 'Uitschrijven',
             destructive: true,
             priority: 0,
             groupIndex: 7,
@@ -134,14 +134,14 @@ export class RegistrationActionBuilder {
             allowAutoSelectAll: false,
             enabled: this.hasWrite,
             handler: async () => {
-                await this.deleteRegistration()
-            }
+                await this.deleteRegistration();
+            },
         })];
     }
 
-    getActionsForCategory(category: GroupCategoryTree, action: (group: Group) => void|Promise<void>): TableAction<PlatformMember>[] {
+    getActionsForCategory(category: GroupCategoryTree, action: (group: Group) => void | Promise<void>): TableAction<PlatformMember>[] {
         return [
-            ...category.categories.map(c => {
+            ...category.categories.map((c) => {
                 return new MenuTableAction({
                     name: c.settings.name,
                     groupIndex: 2,
@@ -149,18 +149,18 @@ export class RegistrationActionBuilder {
                     allowAutoSelectAll: false,
                     enabled: c.groups.length > 0 || c.categories.length > 0,
                     childActions: () => this.getActionsForCategory(c, action),
-                })
+                });
             }),
-            ...category.groups.map(g => {
+            ...category.groups.map((g) => {
                 return new InMemoryTableAction({
                     name: g.settings.name,
                     needsSelection: true,
                     allowAutoSelectAll: false,
                     handler: async () => {
-                        await action(g)
-                    }
-                })
-            })
+                        await action(g);
+                    },
+                });
+            }),
         ];
     }
 
@@ -168,7 +168,7 @@ export class RegistrationActionBuilder {
         if (this.registrations.length !== 1) {
             return [];
         }
-        const organizationId = this.registrations[0].organizationId
+        const organizationId = this.registrations[0].organizationId;
         if (this.context.organization?.id === organizationId) {
             return [];
         }
@@ -177,21 +177,21 @@ export class RegistrationActionBuilder {
             return [];
         }
 
-        const memberId = this.registrations[0].memberId
-        const registration =  this.registrations[0]
-        const member = this.members.find(m => m.id === memberId)
+        const memberId = this.registrations[0].memberId;
+        const registration = this.registrations[0];
+        const member = this.members.find(m => m.id === memberId);
         if (!member) {
-            return []
+            return [];
         }
-        const organization = member.organizations.find(o => o.id === registration.organizationId)
+        const organization = member.organizations.find(o => o.id === registration.organizationId);
         if (!organization) {
-            return []
+            return [];
         }
 
         // Add quick switch action
         return [
             new InMemoryTableAction({
-                name: "Open beheerderportaal",
+                name: 'Open beheerderportaal',
                 description: organization.name,
                 priority: 0,
                 groupIndex: 5,
@@ -200,11 +200,11 @@ export class RegistrationActionBuilder {
                 icon: 'external',
                 enabled: this.hasWrite,
                 handler: async () => {
-                    const href = '/beheerders/' + organization.uri
-                    window.open(href, '_blank')
-                }
-            })
-        ]
+                    const href = '/beheerders/' + organization.uri;
+                    window.open(href, '_blank');
+                },
+            }),
+        ];
     }
 
     getActions(): TableAction<PlatformMember>[] {
@@ -212,21 +212,21 @@ export class RegistrationActionBuilder {
             ...this.getMoveAction(),
             ...this.getAdminActions(),
             ...this.getEditAction(),
-            ...this.getUnsubscribeAction()
-        ]
+            ...this.getUnsubscribeAction(),
+        ];
     }
 
     async deleteRegistration() {
         const deleteRegistrations: RegistrationWithMember[] = [];
 
         for (const registration of this.registrations) {
-            const member = this.members.find(m => m.id === registration.memberId)
+            const member = this.members.find(m => m.id === registration.memberId);
             if (!member) {
-                console.warn("Member not found for registration in RegistrationActionBuilder.moveRegistrations", registration)
-                continue
+                console.warn('Member not found for registration in RegistrationActionBuilder.moveRegistrations', registration);
+                continue;
             }
 
-            deleteRegistrations.push(RegistrationWithMember.from(registration, member.patchedMember.tiny))
+            deleteRegistrations.push(RegistrationWithMember.from(registration, member.patchedMember.tiny));
         }
 
         return await chooseOrganizationMembersForGroup({
@@ -240,32 +240,31 @@ export class RegistrationActionBuilder {
                 present: this.present,
                 show: this.present,
                 pop: () => Promise.resolve(),
-                dismiss: () => Promise.resolve()
-            }
-        })
+                dismiss: () => Promise.resolve(),
+            },
+        });
     }
-
 
     async moveRegistrations(group: Group) {
         const items: RegisterItem[] = [];
-        const checkout = new RegisterCheckout()
-        checkout.setDefaultOrganization(this.organization)
+        const checkout = new RegisterCheckout();
+        checkout.setDefaultOrganization(this.organization);
 
         for (const registration of this.registrations) {
-            const member = this.members.find(m => m.id === registration.memberId)
+            const member = this.members.find(m => m.id === registration.memberId);
             if (!member) {
-                console.warn("Member not found for registration in RegistrationActionBuilder.moveRegistrations", registration)
-                continue
+                console.warn('Member not found for registration in RegistrationActionBuilder.moveRegistrations', registration);
+                continue;
             }
 
-            member.family.checkout = checkout
-            member.family.pendingRegisterItems = []
+            member.family.checkout = checkout;
+            member.family.pendingRegisterItems = [];
 
-            const item = registration.group.id === group.id ? RegisterItem.fromRegistration(registration, member, this.organization) : RegisterItem.defaultFor(member, group, this.organization)
-            item.replaceRegistrations = [registration]
+            const item = registration.group.id === group.id ? RegisterItem.fromRegistration(registration, member, this.organization) : RegisterItem.defaultFor(member, group, this.organization);
+            item.replaceRegistrations = [registration];
             items.push(item);
         }
-        
+
         if (items.length === 1) {
             return await checkoutRegisterItem({
                 item: items[0],
@@ -275,16 +274,16 @@ export class RegistrationActionBuilder {
                     present: this.present,
                     show: this.present,
                     pop: () => Promise.resolve(),
-                    dismiss: () => Promise.resolve()
+                    dismiss: () => Promise.resolve(),
                 },
                 displayOptions: {
                     action: 'present',
-                    modalDisplayStyle: 'popup'
+                    modalDisplayStyle: 'popup',
                 },
 
                 // Immediately checkout instead of only adding it to the cart
-                startCheckoutFlow: true
-            })
+                startCheckoutFlow: true,
+            });
         }
 
         return await chooseOrganizationMembersForGroup({
@@ -296,28 +295,28 @@ export class RegistrationActionBuilder {
                 present: this.present,
                 show: this.present,
                 pop: () => Promise.resolve(),
-                dismiss: () => Promise.resolve()
-            }
-        })
+                dismiss: () => Promise.resolve(),
+            },
+        });
     }
 
     async editRegistrations() {
         const items: RegisterItem[] = [];
-        const groupOrganization = this.organization
-        const checkout = new RegisterCheckout()
-        checkout.setDefaultOrganization(groupOrganization)
+        const groupOrganization = this.organization;
+        const checkout = new RegisterCheckout();
+        checkout.setDefaultOrganization(groupOrganization);
 
         for (const registration of this.registrations) {
-            const member = this.members.find(m => m.id === registration.memberId)
+            const member = this.members.find(m => m.id === registration.memberId);
             if (!member) {
-                console.warn("Member not found for registration in RegistrationActionBuilder.editRegistrations", registration)
-                continue
+                console.warn('Member not found for registration in RegistrationActionBuilder.editRegistrations', registration);
+                continue;
             }
-            member.family.checkout = checkout
-            member.family.pendingRegisterItems = []
+            member.family.checkout = checkout;
+            member.family.pendingRegisterItems = [];
 
-            const item = RegisterItem.fromRegistration(registration, member, groupOrganization) 
-            item.replaceRegistrations = [registration]
+            const item = RegisterItem.fromRegistration(registration, member, groupOrganization);
+            item.replaceRegistrations = [registration];
             items.push(item);
         }
 
@@ -330,16 +329,16 @@ export class RegistrationActionBuilder {
                     present: this.present,
                     show: this.present,
                     pop: () => Promise.resolve(),
-                    dismiss: () => Promise.resolve()
+                    dismiss: () => Promise.resolve(),
                 },
                 displayOptions: {
                     action: 'present',
-                    modalDisplayStyle: 'popup'
+                    modalDisplayStyle: 'popup',
                 },
 
                 // Immediately checkout instead of only adding it to the cart
-                startCheckoutFlow: true
-            })
+                startCheckoutFlow: true,
+            });
         }
 
         return await chooseOrganizationMembersForGroup({
@@ -351,8 +350,8 @@ export class RegistrationActionBuilder {
                 present: this.present,
                 show: this.present,
                 pop: () => Promise.resolve(),
-                dismiss: () => Promise.resolve()
-            }
-        })
+                dismiss: () => Promise.resolve(),
+            },
+        });
     }
 }
