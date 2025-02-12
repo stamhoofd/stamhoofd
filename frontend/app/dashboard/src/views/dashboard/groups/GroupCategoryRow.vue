f889cca726ed003c67961ff0094c0f38e02c04b5
Simon Backx <simon@stamhoofd.be>
2025-02-12 18:16:31 +0100

    Fixed broken import

--- frontend/app/dashboard/src/views/dashboard/groups/GroupCategoryRow.vue
+++ frontend/app/dashboard/src/views/dashboard/groups/GroupCategoryRow.vue
@@ -18,7 +18,7 @@
 <script lang="ts" setup>
 import { AutoEncoderPatchType, PatchableArray, PatchableArrayAutoEncoder } from '@simonbackx/simple-encoding';
 import { ComponentWithProperties, usePresent } from '@simonbackx/vue-app-navigation';
-import { CenteredMessage, ContextMenu, ContextMenuItem, useAuth, useEmitPatch, usePatchMoveUpDownSingleIds } from '@stamhoofd/components';
+import { CenteredMessage, ContextMenu, ContextMenuItem, useAuth, useEmitPatch, usePatchMoveUpDownIds } from '@stamhoofd/components';
 import { GroupCategory, Organization, OrganizationRegistrationPeriod, OrganizationRegistrationPeriodSettings } from '@stamhoofd/structures';
 import { computed } from 'vue';
 import EditCategoryGroupsView from './EditCategoryGroupsView.vue';
@@ -39,7 +39,7 @@ const grandParentCategory = computed(() => parentCategory.value?.getParentCatego
 const subCategories = computed(() => parentCategory.value.categoryIds.map(id => props.period.settings.categories.find(c => c.id === id)!).filter(c => c && c.id !== props.category.id));
 const childCategories = computed(() => props.category.categoryIds.map(id => props.period.settings.categories.find(c => c.id === id)!).filter(c => c));
 const childGroups = computed(() => props.category.groupIds.map(id => props.period.groups.find(g => g.id === id)!).filter(g => g));
-const { up, down } = usePatchMoveUpDownSingleIds(props.category.id, computed(() => parentCategory.value.categoryIds), (categoryIds) => {
+const { up, down } = usePatchMoveUpDownIds(props.category.id, computed(() => parentCategory.value.categoryIds), (categoryIds) => {
     const patchCategory = GroupCategory.patch({ id: parentCategory.value.id, categoryIds });
     const arr = new PatchableArray() as PatchableArrayAutoEncoder<GroupCategory>;
     arr.addPatch(patchCategory);
