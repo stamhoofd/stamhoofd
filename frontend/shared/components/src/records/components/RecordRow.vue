cd358f80e4b50509377f24fc8df8c6888c5982b4
Simon Backx <simon@stamhoofd.be>
2025-02-12 19:56:45 +0100

    Added filter description to record row

--- frontend/shared/components/src/records/components/RecordRow.vue
+++ frontend/shared/components/src/records/components/RecordRow.vue
@@ -19,13 +19,16 @@
             {{ description }}
         </p>
 
+        <p v-if="record.filter && !record.filter.isAlwaysEnabledAndRequired" class="style-description-small">
+            <span>{{ propertyFilterToString(record.filter, filterBuilder) }}</span>
+        </p>
+
         <template #right>
             <span class="button icon drag gray" @click.stop @contextmenu.stop />
             <span class="icon arrow-right-small gray" />
         </template>
     </STListItem>
 </template>
-
 
 <script setup lang="ts">
 import { AutoEncoderPatchType, PatchableArray, PatchableArrayAutoEncoder } from '@simonbackx/simple-encoding';
@@ -34,133 +37,137 @@ import { computed } from 'vue';
 import { usePatchMoveUpDownSingle } from '../../hooks';
 import { ContextMenu, ContextMenuItem } from '../../overlays/ContextMenu';
 import { RecordEditorSettings } from '../RecordEditorSettings';
+import { propertyFilterToString } from '../../filters/UIFilter';
 
 const props = withDefaults(
     defineProps<{
-        record: RecordSettings,
-        category: RecordCategory
-        parentCategory?: RecordCategory | null
-        rootCategories: RecordCategory[]
-        settings: RecordEditorSettings<any>
+        record: RecordSettings;
+        category: RecordCategory;
+        parentCategory?: RecordCategory | null;
+        rootCategories: RecordCategory[];
+        settings: RecordEditorSettings<any>;
     }>(),
     {
-        parentCategory: null
-    }
+        parentCategory: null,
+    },
 );
 
 const emit = defineEmits<{
     // Patch patches the root categories (allows full control in case of moving records around categories)
-    patch: [patch: PatchableArrayAutoEncoder<RecordCategory>],
-    edit: [edit: RecordSettings]
+    patch: [patch: PatchableArrayAutoEncoder<RecordCategory>];
+    edit: [edit: RecordSettings];
 }>();
-const {up, down} = usePatchMoveUpDownSingle(props.record.id, props.category.records, addPatch)
+const { up, down } = usePatchMoveUpDownSingle(props.record.id, props.category.records, addPatch);
+const filterBuilder = computed(() => props.settings.filterBuilder(props.rootCategories));
 
 const description = computed(() => {
     if (props.record.type === RecordType.Checkbox) {
         if (props.record.askComments) {
-            return "Aankruisvakje met tekstvak indien aangevinkt"
+            return 'Aankruisvakje met tekstvak indien aangevinkt';
         }
         if (props.record.required) {
-            return 'Verplicht aankruisvakje'
+            return 'Verplicht aankruisvakje';
         }
-        return 'Aankruisvakje'
+        return 'Aankruisvakje';
     }
     if (props.record.type === RecordType.ChooseOne) {
         if (!props.record.required) {
-            return 'Kies optioneel één uit ' + props.record.choices.length + ' opties'
+            return 'Kies optioneel één uit ' + props.record.choices.length + ' opties';
         }
-        return 'Kies één uit ' + props.record.choices.length + ' opties'
+        return 'Kies één uit ' + props.record.choices.length + ' opties';
     }
     if (props.record.type === RecordType.MultipleChoice) {
         if (props.record.required) {
-            return 'Kies minstens één uit ' + props.record.choices.length + ' opties'
+            return 'Kies minstens één uit ' + props.record.choices.length + ' opties';
         }
-        return 'Kies optioneel meerdere keuzes uit ' + props.record.choices.length + ' opties'
+        return 'Kies optioneel meerdere keuzes uit ' + props.record.choices.length + ' opties';
     }
     if (props.record.type === RecordType.Email) {
         if (!props.record.required) {
-            return 'Optioneel e-mailadres'
+            return 'Optioneel e-mailadres';
         }
-        return 'E-mailadres'
+        return 'E-mailadres';
     }
     if (props.record.type === RecordType.Address) {
         if (!props.record.required) {
-            return 'Optioneel adres'
+            return 'Optioneel adres';
         }
-        return 'Adres'
+        return 'Adres';
     }
     if (props.record.type === RecordType.Phone) {
         if (!props.record.required) {
-            return 'Optioneel telefoonnummer'
+            return 'Optioneel telefoonnummer';
         }
-        return 'Telefoonnummer'
+        return 'Telefoonnummer';
     }
     if (props.record.type === RecordType.Date) {
         if (!props.record.required) {
-            return 'Optionele datum'
+            return 'Optionele datum';
         }
-        return 'Datum'
+        return 'Datum';
     }
 
     if (props.record.type === RecordType.Textarea) {
         if (!props.record.required) {
-            return 'Optioneel (groot) tekstveld'
+            return 'Optioneel (groot) tekstveld';
         }
-        return 'Groot tekstveld'
+        return 'Groot tekstveld';
     }
 
     if (!props.record.required) {
-        return 'Optioneel tekstveld'
+        return 'Optioneel tekstveld';
     }
     return 'Tekstveld';
-})
+});
 
 function editRecord() {
-    emit('edit', props.record)
+    emit('edit', props.record);
 }
 
 // Patch a root category
 function addRootPatch(patch: AutoEncoderPatchType<RecordCategory>) {
-    const array: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray()
-    array.addPatch(patch)
-    emit("patch", array)
+    const array: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray();
+    array.addPatch(patch);
+    emit('patch', array);
 }
 
 // Sends patch meant the root categories
 function addPatch(patch: PatchableArrayAutoEncoder<RecordSettings>) {
     // Is category a root category or not?
-    const rootCategory = props.rootCategories.find(c => c.id === props.category.id)
+    const rootCategory = props.rootCategories.find(c => c.id === props.category.id);
     if (rootCategory) {
         const categoryPatch = RecordCategory.patch({
             id: rootCategory.id,
-            records: patch
-        })
-        addRootPatch(categoryPatch)
-    } else {
+            records: patch,
+        });
+        addRootPatch(categoryPatch);
+    }
+    else {
         // Find the root category that contains this category
-        const rootCategory = props.rootCategories.find(c => c.childCategories.find(r => r.id === props.category.id))
+        const rootCategory = props.rootCategories.find(c => c.childCategories.find(r => r.id === props.category.id));
         if (rootCategory) {
             const childCategoryPatch = RecordCategory.patch({
                 id: props.category.id,
-                records: patch
-            })
+                records: patch,
+            });
 
-            const arr: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray()
-            arr.addPatch(childCategoryPatch)
+            const arr: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray();
+            arr.addPatch(childCategoryPatch);
 
             const categoryPatch = RecordCategory.patch({
-                id: rootCategory.id, 
-                childCategories: arr
-            })
-            addRootPatch(categoryPatch)
-        } else {
-            console.warn('Could not find root category to patch for this record')
+                id: rootCategory.id,
+                childCategories: arr,
+            });
+            addRootPatch(categoryPatch);
+        }
+        else {
+            console.warn('Could not find root category to patch for this record');
         }
     }
 }
 
 function moveTo(category: RecordCategory) {
-    const me = props.record
+    const me = props.record;
 
     // Delete record from current category
     const deleteArray: PatchableArrayAutoEncoder<RecordSettings> = new PatchableArray();
@@ -169,15 +176,15 @@ function moveTo(category: RecordCategory) {
 
     // Add it to the right category
     const putArray: PatchableArrayAutoEncoder<RecordSettings> = new PatchableArray();
-    putArray.addPut(me)
+    putArray.addPut(me);
 
     const rootCategory = props.rootCategories.find(r => r.id === category.id);
     if (rootCategory) {
         const categoryPatch = RecordCategory.patch({
             id: rootCategory.id,
-            records: putArray
-        })
-        addRootPatch(categoryPatch)
+            records: putArray,
+        });
+        addRootPatch(categoryPatch);
         return;
     }
 
@@ -187,73 +194,74 @@ function moveTo(category: RecordCategory) {
         return;
     }
 
-    const childCategoryPatch: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray()
+    const childCategoryPatch: PatchableArrayAutoEncoder<RecordCategory> = new PatchableArray();
     childCategoryPatch.addPatch(RecordCategory.patch({
         id: category.id,
-        records: putArray
-    }))
+        records: putArray,
+    }));
 
     const categoryPatch = RecordCategory.patch({
         id: parentRootCategory.id,
-        childCategories: childCategoryPatch
-    })
-    addRootPatch(categoryPatch)
+        childCategories: childCategoryPatch,
+    });
+    addRootPatch(categoryPatch);
 }
 
 function showContextMenu(event: MouseEvent) {
     const menu = new ContextMenu([
         [
             new ContextMenuItem({
-                name: "Verplaats omhoog",
-                icon: "arrow-up",
+                name: 'Verplaats omhoog',
+                icon: 'arrow-up',
                 action: () => {
-                    up()
+                    up();
                     return true;
-                }
+                },
             }),
             new ContextMenuItem({
-                name: "Verplaats omlaag",
-                icon: "arrow-down",
+                name: 'Verplaats omlaag',
+                icon: 'arrow-down',
                 action: () => {
-                    down()
+                    down();
                     return true;
-                }
+                },
             }),
             new ContextMenuItem({
-                name: "Verplaats naar",
+                name: 'Verplaats naar',
                 childMenu: new ContextMenu([
-                    props.rootCategories.map(category => {
+                    props.rootCategories.map((category) => {
                         return new ContextMenuItem({
                             name: category.name,
                             action: () => {
-                                moveTo(category)
+                                moveTo(category);
                             },
-                            childMenu: category.childCategories.length > 0 ? new ContextMenu([
-                                [
-                                    new ContextMenuItem({
-                                        name: 'Algemene vraag',
-                                        action: () => {
-                                            moveTo(category)
-                                        },
-                                    })
-                                ],
-                                category.childCategories.map(childCategory => {
-                                    return new ContextMenuItem({
-                                        name: childCategory.name,
-                                        action: () => {
-                                            moveTo(childCategory)
-                                        },
-                                    })
-                                })
-                            ]) : undefined
-                        })
-                    })
-                ])
+                            childMenu: category.childCategories.length > 0
+                                ? new ContextMenu([
+                                    [
+                                        new ContextMenuItem({
+                                            name: 'Algemene vraag',
+                                            action: () => {
+                                                moveTo(category);
+                                            },
+                                        }),
+                                    ],
+                                    category.childCategories.map((childCategory) => {
+                                        return new ContextMenuItem({
+                                            name: childCategory.name,
+                                            action: () => {
+                                                moveTo(childCategory);
+                                            },
+                                        });
+                                    }),
+                                ])
+                                : undefined,
+                        });
+                    }),
+                ]),
             }),
-        ]
-    ])
-    menu.show({ clickEvent: event }).catch(console.error)
+        ],
+    ]);
+    menu.show({ clickEvent: event }).catch(console.error);
 }
-
 
 </script>
