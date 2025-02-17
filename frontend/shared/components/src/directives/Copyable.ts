675bd676d9e5eaf952d1815661a4b02afe1eedeb
Simon Backx <simon@stamhoofd.be>
2025-02-17 08:58:55 +0100

    Fixed reactive values in v-copyable

--- frontend/shared/components/src/directives/Copyable.ts
+++ frontend/shared/components/src/directives/Copyable.ts
@@ -1,75 +1,75 @@
-import { ComponentWithProperties } from "@simonbackx/vue-app-navigation";
+import { ComponentWithProperties } from '@simonbackx/vue-app-navigation';
 
-import { ModalStackEventBus } from "../overlays/ModalStackEventBus";
-import Tooltip from "../overlays/Tooltip.vue";
-import { ObjectDirective } from "vue";
+import { ModalStackEventBus } from '../overlays/ModalStackEventBus';
+import Tooltip from '../overlays/Tooltip.vue';
+import { ObjectDirective } from 'vue';
 
 const helper = {
     copyElementFallback(event, vnode) {
         event.target.contentEditable = true;
 
         document.execCommand('selectAll', false);
-        document.execCommand('copy')
+        document.execCommand('copy');
 
         event.target.contentEditable = false;
 
-
         const w = window as any;
         if (w.getSelection) {
-            if (w.getSelection().empty) {  // Chrome
+            if (w.getSelection().empty) { // Chrome
                 w.getSelection().empty();
-            } else if (w.getSelection().removeAllRanges) {  // Firefox
+            }
+            else if (w.getSelection().removeAllRanges) { // Firefox
                 w.getSelection().removeAllRanges();
             }
         }
         this.copiedPopup(event, vnode);
 
-        (document.activeElement as HTMLElement)?.blur()
+        (document.activeElement as HTMLElement)?.blur();
     },
-    
+
     copiedPopup(event) {
-        const el = event.currentTarget || event.target
+        const el = event.currentTarget || event.target;
         const rect = el.getBoundingClientRect();
 
         const displayedComponent = new ComponentWithProperties(Tooltip, {
-            text: "Gekopieerd naar klembord",
-            icon: "",
+            text: 'Gekopieerd naar klembord',
+            icon: '',
             x: rect.left,
-            y: rect.top + el.offsetHeight + 5
+            y: rect.top + el.offsetHeight + 5,
         });
 
-        ModalStackEventBus.sendEvent("present", {
+        ModalStackEventBus.sendEvent('present', {
             components: [
-                displayedComponent
+                displayedComponent,
             ],
-            modalDisplayStyle: "overlay",
-        }).catch(console.error)
+            modalDisplayStyle: 'overlay',
+        }).catch(console.error);
 
         setTimeout(() => {
-            (displayedComponent.componentInstance() as any)?.hide?.()
+            (displayedComponent.componentInstance() as any)?.hide?.();
         }, 1000);
 
         if (el.$tooltipDisplayedComponent) {
             try {
-                (el.$tooltipDisplayedComponent.componentInstance() as any)?.hide?.()
+                (el.$tooltipDisplayedComponent.componentInstance() as any)?.hide?.();
                 el.$tooltipDisplayedComponent = null;
-            } catch (e) {
+            }
+            catch (e) {
                 // ignore
             }
         }
         el.$tooltipDisplayedComponent = displayedComponent;
 
-
         // Add style
-        el.classList.add("copied");
+        el.classList.add('copied');
         setTimeout(() => {
-            el.classList.remove("copied");
+            el.classList.remove('copied');
         }, 500);
     },
 
     copyElement(event, bindingValue: any, vnode: any) {
         if (window.getSelection() !== null && window.getSelection()!.toString().length > 0) {
-            return
+            return;
         }
         if (navigator.clipboard) {
             // Select all
@@ -80,23 +80,28 @@ const helper = {
                 console.error(e);
                 this.copyElementFallback(event, vnode);
             });
-        } else {
-            console.warn("No navigator.clipboard support");
+        }
+        else {
+            console.warn('No navigator.clipboard support');
             this.copyElementFallback(event, vnode);
-        }        
+        }
     },
 };
 
-const CopyableDirective: ObjectDirective<HTMLElement & {$tooltipDisplayedComponent: null|ComponentWithProperties}, string>  =  {
+const CopyableDirective: ObjectDirective<HTMLElement & { $tooltipDisplayedComponent: null | ComponentWithProperties }, string> = {
     beforeMount(el, binding, vnode) {
         // Add a hover listener
         el.addEventListener(
-            "click",
+            'click',
             (event) => {
-                helper.copyElement(event, binding.value, vnode)
+                helper.copyElement(event, (el as any)._copyableValue ?? binding.value, vnode);
             },
-            { passive: true }
+            { passive: true },
         );
+    },
+
+    updated(el, binding, vnode) {
+        (el as any)._copyableValue = binding.value;
     },
 };
 
