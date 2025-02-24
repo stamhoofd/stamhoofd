3e5cd7d12a8890e232302133608cbbc06ccfcbfb
Simon Backx <simon@stamhoofd.be>
2025-02-24 17:32:18 +0100

    Added jest-console-group-reporter reporter

--- backend/app/api/jest.config.cjs
+++ backend/app/api/jest.config.cjs
@@ -1,3 +1,5 @@
+const useGithubActions = !!process.env.GITHUB_ACTIONS;
+
 module.exports = {
     roots: ['<rootDir>/dist'],
     testEnvironment: 'node',
@@ -5,6 +7,21 @@ module.exports = {
         'jest-extended/all',
         './dist/tests/jest.setup.js',
     ],
+    reporters: [['jest-console-group-reporter', {
+        consoleLevels: ['error', 'warn'],
+        filters: [],
+        groups: [],
+        onlyFailingTestSuites: true,
+        afterEachTest: {
+            enable: false,
+        },
+        afterAllTests: {
+            reportType: 'detailed',
+            enable: true,
+            filePaths: true,
+        },
+        useGitHubActions: useGithubActions,
+    }]],
     globalSetup: './dist/tests/jest.global.setup.js',
     // verbose: true,
 };
