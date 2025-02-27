d6fc4485cddec4288916806f358b65cf47c99252
Simon Backx <simon@stamhoofd.be>
2025-02-27 13:48:08 +0100

    Added boot test

---
+++ test/boot/boot-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Go to the backend API folder (note that it should be built already)
+cd backend/app/api;
+
+# Start the process in the background
+node dist/index.js &
+PID=$!  # Capture the PID of the background process
+
+# Wait a few seconds
+sleep 5
+
+# Send SIGTERM to the process
+kill -SIGTERM $PID
+
+# Wait for the process to exit
+wait $PID
+EXIT_CODE=$?
+
+# Check the exit code
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "Process exited successfully with code 0."
+else
+    echo "Process exited with code $EXIT_CODE."
+fi
+
+exit $EXIT_CODE
