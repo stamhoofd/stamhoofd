91be7f4862c1ab2b0d625fa733ceba6ad29647f0
Simon Backx <simon@stamhoofd.be>
2025-03-05 11:12:23 +0100

    Improved responsibilities sorting

--- frontend/shared/components/src/members/components/view/ViewMemberResponsibilitiesBox.vue
+++ frontend/shared/components/src/members/components/view/ViewMemberResponsibilitiesBox.vue
@@ -8,8 +8,6 @@
             </div>
         </h2>
 
-
-
         <button v-if="sortedResponsibilities.length === 0" class="info-box selectable with-button" type="button" @click="editResponsibilities">
             {{ props.member.patchedMember.details.firstName }} heeft geen functies
 
@@ -32,45 +30,59 @@ import { useMemberActions } from '../../classes/MemberActionBuilder';
 import ViewMemberResponsibilityRow from './ViewMemberResponsibilityRow.vue';
 
 defineOptions({
-    inheritAttrs: false
-})
+    inheritAttrs: false,
+});
 
 const props = defineProps<{
-    member: PlatformMember
-}>()
+    member: PlatformMember;
+}>();
 
-const platform = usePlatform()
-const organization = useOrganization()
-const auth = useAuth()
-const hasResponsibilities = computed(() => ((platform.value.config.responsibilities.length > 0 || (organization.value && organization.value.privateMeta?.responsibilities?.length)) && props.member.patchedMember.details.defaultAge >= 16) || responsibilities.value.length)
-const responsibilities = computed(() => props.member.getResponsibilities(organization.value))
-const buildActions = useMemberActions()
+const platform = usePlatform();
+const organization = useOrganization();
+const auth = useAuth();
+const hasResponsibilities = computed(() => ((platform.value.config.responsibilities.length > 0 || (organization.value && organization.value.privateMeta?.responsibilities?.length)) && props.member.patchedMember.details.defaultAge >= 16) || responsibilities.value.length);
+const responsibilities = computed(() => props.member.getResponsibilities({ organization: organization.value ?? undefined }));
+const buildActions = useMemberActions();
+
+const nationalResponsibilities = computed(() => {
+    return platform.value.config.responsibilities.filter(r => !r.organizationBased);
+});
+
+const organizationResponsibilities = computed(() => {
+    return platform.value.config.responsibilities.filter(r => !r.organizationBased);
+});
 
 function getResponsibilityIndex(responsibility: MemberResponsibilityRecord) {
-    const platformIndex = platform.value.config.responsibilities.findIndex(r => r.id === responsibility.responsibilityId);
-    if (platformIndex !== -1) {
-        return platformIndex;
+    let index = nationalResponsibilities.value.findIndex(r => r.id === responsibility.responsibilityId);
+    if (index !== -1) {
+        return index;
     }
 
-    if (organization.value && organization.value.privateMeta) {
-        const organizationIndex = organization.value?.privateMeta.responsibilities.findIndex(r => r.id === responsibility.responsibilityId)
+    index = organizationResponsibilities.value.findIndex(r => r.id === responsibility.responsibilityId);
+    if (index !== -1) {
+        return index + nationalResponsibilities.value.length;
+    }
+
+    const org = responsibility.organizationId ? props.member.organizations.find(o => o.id === responsibility.organizationId) : null;
+    if (org && org.privateMeta) {
+        const organizationIndex = org.privateMeta.responsibilities.findIndex(r => r.id === responsibility.responsibilityId);
         if (organizationIndex !== -1) {
             return organizationIndex + platform.value.config.responsibilities.length;
         }
-        return organization.value?.privateMeta.responsibilities.length + platform.value.config.responsibilities.length;
+        return org.privateMeta.responsibilities.length + platform.value.config.responsibilities.length;
     }
 
-    return platform.value.config.responsibilities.length
+    return platform.value.config.responsibilities.length;
 }
 
 const sortedResponsibilities = computed(() => {
     // Sort based on index in platform.value.config.responsibilities, followed by index in organization.value.privateMeta.responsibilities
     return responsibilities.value.slice().sort((a, b) => {
-        return getResponsibilityIndex(a) - getResponsibilityIndex(b)
-    })
-})
+        return getResponsibilityIndex(a) - getResponsibilityIndex(b);
+    });
+});
 
 async function editResponsibilities() {
-    await buildActions().editResponsibilities(props.member)
+    await buildActions().editResponsibilities(props.member);
 }
 </script>
