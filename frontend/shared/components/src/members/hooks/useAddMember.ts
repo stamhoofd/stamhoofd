8ca43193ef6e80cdc7dc1584ec4e047b0a6a04b5
Simon Backx <simon@stamhoofd.be>
2025-03-10 13:33:49 +0100

    Corrected hook inside useAddMember

--- frontend/shared/components/src/members/hooks/useAddMember.ts
+++ frontend/shared/components/src/members/hooks/useAddMember.ts
@@ -2,17 +2,17 @@ import { ComponentWithProperties, NavigationController } from '@simonbackx/vue-a
 import { PlatformFamily, PlatformMember } from '@stamhoofd/structures';
 import { markRaw, reactive } from 'vue';
 import { EditMemberGeneralBox, MemberStepView } from '..';
-import { DisplayOptions, NavigationActions, runDisplayOptions, useNavigationActions } from '../../types/NavigationActions';
 import { useAppContext } from '../../context';
+import { DisplayOptions, NavigationActions, runDisplayOptions, useNavigationActions } from '../../types/NavigationActions';
 
 export function useAddMember() {
     const navigate = useNavigationActions();
+    const app = useAppContext();
 
     return async (family: PlatformFamily, options: { displayOptions: DisplayOptions; finishHandler: (member: PlatformMember, navigate: NavigationActions) => Promise<void> | void }) => {
         // We clone the family, so we can cancel the new member that was added to the family
         const clonedFamily = family.clone();
         const member = reactive(clonedFamily.newMember() as any) as PlatformMember;
-        const app = useAppContext();
 
         const component = new ComponentWithProperties(NavigationController, {
             root: new ComponentWithProperties(MemberStepView, {
@@ -25,13 +25,13 @@ export function useAddMember() {
 
                     // Find the member
                     // The reference to the member could have changed - we need to make sure we have a reference to a member in the original family
-                    const realMember = family.members.find(m => m.id == clonedFamily.members[clonedFamily.members.length - 1].id);
+                    const realMember = family.members.find(m => m.id === clonedFamily.members[clonedFamily.members.length - 1].id);
                     if (!realMember) {
                         await navigate.dismiss({ force: true });
                         return;
                     }
 
-                    if (app === 'registration' && realMember.filterRegistrations({ currentPeriod: true }).length) {
+                    if (app === 'registration' && realMember.filterRegistrations({ currentPeriod: true }).length > 0) {
                         // This (new) member already has registrations. We don't need to show the choose group view.
                         await navigate.dismiss({ force: true });
                         return;
