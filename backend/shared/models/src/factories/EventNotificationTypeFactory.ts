f13476fcf2757c3d04b7c569498d6473ff0eb199
Simon Backx <simon@stamhoofd.be>
2025-03-13 16:26:19 +0100

    Added more tests

--- backend/shared/models/src/factories/EventNotificationTypeFactory.ts
+++ backend/shared/models/src/factories/EventNotificationTypeFactory.ts
@@ -1,10 +1,11 @@
 import { Factory } from '@simonbackx/simple-database';
-import { EventNotificationType } from '@stamhoofd/structures';
+import { EventNotificationType, RecordCategory } from '@stamhoofd/structures';
 
 import { Platform } from '../models/Platform';
 
 class Options {
     title?: string;
+    recordCategories?: RecordCategory[];
 }
 
 export class EventNotificationTypeFactory extends Factory<Options, EventNotificationType> {
@@ -12,6 +13,8 @@ export class EventNotificationTypeFactory extends Factory<Options, EventNotifica
         const eventType = EventNotificationType.create({
             title: this.options.title ?? ('Melding ' + (new Date().getTime() + Math.floor(Math.random() * 999999))),
         });
+
+        eventType.recordCategories = this.options.recordCategories ?? [];
 
         // Add to platform
         const platform = await Platform.getForEditing();
