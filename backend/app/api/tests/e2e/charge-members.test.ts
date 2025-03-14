5b1e75ea9dd1ea58a80594f7b26cc24156872fa8
Bjarne <bjarne.backx@gmail.com>
2025-03-14 16:17:48 +0100

    Tested should not charge members of other organization

--- backend/app/api/tests/e2e/charge-members.test.ts
+++ backend/app/api/tests/e2e/charge-members.test.ts
@@ -11,9 +11,11 @@ describe('E2E.ChargeMembers', () => {
     const memberBalanceEndpoint = new GetMemberBalanceEndpoint();
     let period: RegistrationPeriod;
     let organization: Organization;
+    let otherOrganization: Organization;
     let financialDirector: User;
     let financialDirectorToken: Token;
     let financialDirectorRole: PermissionRoleDetailed;
+    let financialDirectorRoleOfOtherOrganization: PermissionRoleDetailed;
 
     const postCharge = async (filter: StamhoofdFilter, organization: Organization, body: ChargeMembersRequest, token: Token) => {
         const request = Request.buildJson('POST', `/v${Version}/admin/charge-members`, organization.getApiHost(), body);
@@ -47,8 +49,15 @@ describe('E2E.ChargeMembers', () => {
             accessRights: [AccessRight.OrganizationFinanceDirector],
         });
 
+        financialDirectorRoleOfOtherOrganization = PermissionRoleDetailed.create({
+            name: 'financial director other organization',
+            accessRights: [AccessRight.OrganizationFinanceDirector],
+        });
+
         organization = await new OrganizationFactory({ period, roles: [financialDirectorRole] })
             .create();
+
+        otherOrganization = await new OrganizationFactory({ period, roles: [financialDirectorRoleOfOtherOrganization] }).create();
 
         financialDirector = await new UserFactory({
             organization,
@@ -85,7 +94,6 @@ describe('E2E.ChargeMembers', () => {
         };
 
         const body = ChargeMembersRequest.create({
-            organizationId: organization.id,
             description: 'test description',
             price: 3,
             amount: 4,
@@ -150,7 +158,6 @@ describe('E2E.ChargeMembers', () => {
         };
 
         const body = ChargeMembersRequest.create({
-            organizationId: organization.id,
             description: 'test description',
             price: 3,
             amount: 4,
@@ -170,7 +177,7 @@ describe('E2E.ChargeMembers', () => {
             expect(balanceItem1.price).toEqual(12);
             expect(balanceItem1.amount).toEqual(body.amount);
             expect(balanceItem1.description).toEqual(body.description);
-            expect(balanceItem1.organizationId).toEqual(body.organizationId);
+            expect(balanceItem1.organizationId).toEqual(organization.id);
             // const dueAt = balanceItem1.dueAt!;
             expect(balanceItem1.dueAt).toEqual(body.dueAt);
             expect(balanceItem1.createdAt).toEqual(body.createdAt);
@@ -178,6 +185,62 @@ describe('E2E.ChargeMembers', () => {
 
         testBalanceResponse(await getBalance(member1.id, organization, financialDirectorToken));
         testBalanceResponse(await getBalance(member2.id, organization, financialDirectorToken));
+    });
+
+    test('Should not charge members of other organization', async () => {
+        // arrange
+        const member1 = await new MemberFactory({ }).create();
+
+        const member2 = await new MemberFactory({ }).create();
+
+        await new RegistrationFactory({ member: member1, organization: otherOrganization }).create();
+        await new RegistrationFactory({ member: member2, organization: otherOrganization }).create();
+
+        const filter: StamhoofdFilter = {
+            id: {
+                $in: [member1.id, member2.id],
+            },
+        };
+
+        const otherFinancialDirector = await new UserFactory({
+            organization: otherOrganization,
+            permissions: Permissions.create({
+                level: PermissionLevel.None,
+                roles: [
+                    financialDirectorRoleOfOtherOrganization,
+                ],
+                resources: new Map([[PermissionsResourceType.Groups, new Map([[
+                    '',
+                    ResourcePermissions.create({
+                        level: PermissionLevel.Write,
+                    }),
+                ]])]]),
+            }),
+        })
+            .create();
+
+        const otherFinancialDirectorToken = await Token.createToken(otherFinancialDirector);
+
+        const body = ChargeMembersRequest.create({
+            description: 'test description',
+            price: 3,
+            amount: 4,
+            dueAt: new Date(2023, 0, 10),
+            createdAt: new Date(2023, 0, 4),
+        });
+
+        // act and assert
+        const result = await postCharge(filter, organization, body, financialDirectorToken);
+        expect(result).toBeDefined();
+        expect(result.body).toBeUndefined();
+
+        const testBalanceResponse = (response: Response<BalanceItemWithPayments[]>) => {
+            expect(response).toBeDefined();
+            expect(response.body.length).toBe(0);
+        };
+
+        testBalanceResponse(await getBalance(member1.id, otherOrganization, otherFinancialDirectorToken));
+        testBalanceResponse(await getBalance(member2.id, otherOrganization, otherFinancialDirectorToken));
     });
 
     test('Should fail if invalid request', async () => {
@@ -198,7 +261,6 @@ describe('E2E.ChargeMembers', () => {
         const testCases: [body: ChargeMembersRequest, expectedErrorMessage: string][] = [
             // empty description
             [ChargeMembersRequest.create({
-                organizationId: organization.id,
                 description: ' ',
                 price: 3,
                 amount: 4,
@@ -208,7 +270,6 @@ describe('E2E.ChargeMembers', () => {
 
             // price 0
             [ChargeMembersRequest.create({
-                organizationId: organization.id,
                 description: 'test description',
                 price: 0,
                 amount: 4,
@@ -218,7 +279,6 @@ describe('E2E.ChargeMembers', () => {
 
             // amount 0
             [ChargeMembersRequest.create({
-                organizationId: organization.id,
                 description: 'test description',
                 price: 3,
                 amount: 0,
