8b26a13c1b7b9f0317f65ff6cd07c85371031efc
Simon Backx <git@simonbackx.com>
2025-03-17 17:29:12 +0100

    Added vitest frontend tests (#268)

---
+++ frontend/tests/vitest.setup.ts
@@ -0,0 +1,9 @@
+import { TestUtils } from '@stamhoofd/test-utils';
+import { beforeAll, beforeEach, afterEach } from 'vitest';
+
+// In vitest there is no alternative for setupfiles after env, so the env has not been setup yet...
+(window as any).beforeAll = beforeAll;
+(window as any).beforeEach = beforeEach;
+(window as any).afterEach = afterEach;
+
+TestUtils.setup();
