52014df2b57a26b83cceba31b74e79b79ecebeac
Bjarne <bjarne.backx@gmail.com>
2025-03-18 17:48:43 +0100

    Added TimeInput tests for different time zones

--- frontend/shared/components/src/inputs/TimeInput.test.ts
+++ frontend/shared/components/src/inputs/TimeInput.test.ts
@@ -16,7 +16,7 @@ describe('TimeInput', async () => {
         Formatter.timezone = timezone;
     };
 
-    test('Model does not update without user input', async () => {
+    test('Model should not update without user input', async () => {
         setFormatterTimeZone('Asia/Shanghai');
 
         const date = new Date(2023, 0, 1, 12, 0, 0);
@@ -33,29 +33,66 @@ describe('TimeInput', async () => {
         expect(wrapper.vm.modelValue).toBe(date);
     });
 
-    test('Model does update after user input', async () => {
-        // +08:00
-        setFormatterTimeZone('Asia/Shanghai');
-
-        // 12:00 in UTC -> 20:00 in Shanghai
+    describe('Model should output correct Date after user input', async () => {
         const date = new Date(2023, 0, 1, 12, 0, 0);
 
-        const wrapper = mount(TimeInput, {
-            props: {
-                'title': 'Phone Number',
-                'modelValue': date,
-                'onUpdate:modelValue': async (e) => {
-                    console.error('update model value: ', e);
-                    await wrapper.setProps({ modelValue: e });
-                },
+        const testCases: { timezone: string; input: string; output: Date }[] = [
+
+            { // +11:00
+                timezone: 'Asia/Sakhalin',
+                input: '20:30',
+                // 20:30 in Sakhalin -> 09:30 in UTC
+                output: new Date(2023, 0, 1, 9, 30, 0),
             },
-        });
+            { // +12:00 (SDT), +13:00 (DST)
+                timezone: 'Antarctica/South_Pole',
+                input: '05:00',
+                // 05:00 on South_Pole -> 16:00 in UTC
+                // the date will be shown as 2023-01-02
+                output: new Date(2023, 0, 1, 16, 0, 0),
+            },
+            { // +08:00
+                timezone: 'Asia/Shanghai',
+                input: '20:30',
+                // 20:30 in Shangai -> 12:30 in UTC
+                output: new Date(2023, 0, 1, 12, 30, 0),
+            },
+            {
+                // +01:00 (SDT)
+                timezone: 'Europe/Brussels',
+                input: '20:15',
+                // 19:15 in Brussels -> 19:15 in UTC
+                output: new Date(2023, 0, 1, 19, 15, 0),
+            },
+            {
+                // -11:00
+                timezone: 'Pacific/Pago_Pago',
+                input: '20:30',
+                // 20:30 in Pago_Pago -> 7:30 (+ 1 day) in UTC
+                output: new Date(2023, 0, 2, 7, 30, 0),
+            },
+        ];
 
-        const input = wrapper.find('input');
+        for (const { timezone, input, output } of testCases) {
+            test(`organization time zone ${timezone}`, async () => {
+                setFormatterTimeZone(timezone);
 
-        // 20:30 in Shangai -> 12:30 in UTC
-        await input.setValue('20:30');
+                const wrapper = mount(TimeInput, {
+                    props: {
+                        'title': 'Phone Number',
+                        'modelValue': new Date(date),
+                        'onUpdate:modelValue': async (e) => {
+                            await wrapper.setProps({ modelValue: e });
+                        },
+                    },
+                });
 
-        expect(wrapper.props('modelValue').getTime()).toEqual(new Date(2023, 0, 1, 12, 30, 0).getTime());
+                const inputWrapper = wrapper.find('input');
+
+                await inputWrapper.setValue(input);
+
+                expect(wrapper.props('modelValue').getTime()).toEqual(output.getTime());
+            });
+        }
     });
 });
