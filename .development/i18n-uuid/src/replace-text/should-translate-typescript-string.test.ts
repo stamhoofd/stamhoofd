ba4d2cb234f1b840eb5dbbe53ff1e4e225b9bed6
Bjarne <bjarne.backx@gmail.com>
2025-04-15 11:39:15 +0200

    Added support for deep keys

--- .development/i18n-uuid/src/replace-text/should-translate-typescript-string.test.ts
+++ .development/i18n-uuid/src/replace-text/should-translate-typescript-string.test.ts
@@ -16,4 +16,27 @@ describe('shouldTranslateTypescriptString', () => {
             expect(result).toBe(false);
         }
     })
+
+    test('key not in blacklist should return true', () => {
+        const allParts = `    private static throwIfInvalidBody(body: Body) {
+        if (!body.description?.trim()?.length) {
+            throw new SimpleError({
+                code: 'invalid_field',
+                message: 'Invalid description',
+                human: `;
+
+        const result = shouldTranslateTypescriptString(allParts);
+        expect(result).toBe(true);
+    })
+
+    test('key in keyCombinationBlacklist should return false', () => {
+        const allParts = `    private static throwIfInvalidBody(body: Body) {
+        if (!body.description?.trim()?.length) {
+            throw new SimpleError({
+                code: 'invalid_field',
+                message:`;
+
+        const result = shouldTranslateTypescriptString(allParts);
+        expect(result).toBe(false);
+    })
 })
--- .development/i18n-uuid/src/replace-text/should-translate-typescript-string.ts
+++ .development/i18n-uuid/src/replace-text/should-translate-typescript-string.ts
@@ -1,18 +1,51 @@
 import { getTextType, TextType } from "./get-text-type";
 
 const blackLists = new Map<TextType, Set<string>>([
-    [TextType.Key, new Set(['icon', 'method', 'path', 'modalDisplayStyle', 'id', 'Id', 'service', 'type', 'Type', 'Connection', 'Keys', 'keys', 'environment', 'sign', 'sort', 'direction', 'field', 'code', 'key', 'token', 'mode', 'url', 'server', 'status'])],
+    [TextType.Key, new Set(['icon', 'method', 'path', 'modalDisplayStyle', 'id', 'Id', 'service', 'type', 'Type', 'Connection', 'Keys', 'keys', 'environment', 'sign', 'sort', 'direction', 'field', 'code', 'key', 'token', 'mode', 'url', 'server', 'status', 'locale'])],
     [TextType.FunctionArgument, new Set(['$t', 'process.on', 'console.error', 'console.log', 'console.info', 'console.warn', 'import', 'resolve', 'where', 'whereNot', 'Migration.runAll', 'AuditLogReplacement.key', 'loadAllEndpoints', '.set', '.includes', ' if', 'registerCron', '.column', 'require', 'setZone', 'startsWith', 'parseParameters', 'isRunning','SQLAlias', 'schedule', '.or', '.from', '.wildcard', '.table', 'fromRows', 'andWhere', 'QueueHandler.debounce', '.checkDuplicate'])],
-    [TextType.Variable, new Set(['process.env.TZ', 'Keys', 'keys', 'id', 'Id', 'uploadExt', 'mode', 'url', 'server', 'status'])]
+    [TextType.Variable, new Set(['process.env.TZ', 'Keys', 'keys', 'id', 'Id', 'uploadExt', 'mode', 'url', 'server', 'status', 'locale'])]
+]);
+
+const keyCombinationBlacklist = new Map<TextType, Map<string, Set<string>>>([
+[TextType.FunctionArgument, new Map([
+    ['new SimpleError', new Set(['message'])]
+])],
+[TextType.Variable, new Map([
+    ['errBody', new Set(['message'])]
+])]
 ])
 
-export function shouldTranslateTypescriptString(allParts: {value: string}[]): boolean {
+export function shouldTranslateTypescriptString(allParts: {value: string}[] | string): boolean {
+    return shouldTranslateTypescriptStringHelper(allParts);
+}
+
+function shouldTranslateTypescriptStringHelper(allParts: {value: string}[] | string, initialKeyName?: string): boolean {
     const textType = getTextType(allParts);
 
     if(textType) {
         const {type, name} = textType;
+
         if(name === null) {
             return false;
+        }
+
+        if(initialKeyName) {
+            if(type === TextType.Key) {
+                return shouldTranslateTypescriptStringHelper(name, initialKeyName);
+            }
+
+            const blackList = keyCombinationBlacklist.get(type);
+            if(blackList) {
+                for(const [key, value] of blackList.entries()) {
+                    if(name.endsWith(key)) {
+                        for(const item of value.values()) {
+                            if(initialKeyName.endsWith(item)) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         const blackList = blackLists.get(type);
@@ -23,6 +56,12 @@ export function shouldTranslateTypescriptString(allParts: {value: string}[]): bo
                     return false;
                 }
             }
+        }
+
+        if(type === TextType.Key) {
+            // todo: find beginning?
+
+            return shouldTranslateTypescriptStringHelper(name, name.trimEnd());
         }
     }
 
