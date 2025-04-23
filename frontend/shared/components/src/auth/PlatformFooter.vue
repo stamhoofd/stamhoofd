92d537b8dd99fe7272558efc6e468ea441df5fdf
Simon Backx <simon@stamhoofd.be>
2025-04-23 13:28:19 +0200

    Added language switcher to footer

--- frontend/shared/components/src/auth/PlatformFooter.vue
+++ frontend/shared/components/src/auth/PlatformFooter.vue
@@ -2,13 +2,23 @@
     <div v-if="platform.config.footerText.html" class="legal-footer">
         <div>
             <aside class="style-wysiwyg gray subtle-links" v-html="platform.config.footerText.html" />
-            <div />
+            <div>
+                <button v-if="hasLanguages" type="button" class="button text" @click="switchLanguage">
+                    <span class="icon language" />
+                    <span>{{ LanguageHelper.getNativeName(I18nController.shared.language) }}</span>
+                    <span class="icon arrow-down-small" />
+                </button>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { usePlatform } from '../hooks';
-const platform = usePlatform();
+import { LanguageHelper } from '@stamhoofd/structures';
+import { I18nController } from '@stamhoofd/frontend-i18n';
+import { useSwitchLanguage } from '../views/hooks/useSwitchLanguage';
 
+const platform = usePlatform();
+const { switchLanguage, hasLanguages } = useSwitchLanguage();
 </script>
---
+++ frontend/shared/components/src/views/hooks/useSwitchLanguage.ts
@@ -0,0 +1,33 @@
+import { I18nController } from '@stamhoofd/frontend-i18n';
+import { LanguageHelper } from '@stamhoofd/structures';
+import { ContextMenu, ContextMenuItem } from '../../overlays/ContextMenu';
+
+export function useSwitchLanguage() {
+    async function switchLanguage(event: MouseEvent) {
+        const menu = new ContextMenu([
+            I18nController.shared.availableLanguages.map((language) => {
+                return new ContextMenuItem({
+                    name: LanguageHelper.getNativeName(language),
+                    selected: language === I18nController.shared.language,
+                    action: async () => {
+                        await I18nController.shared.switchToLocale({
+                            language,
+                        });
+
+                        // Reload full page
+                        window.location.reload();
+                    },
+                });
+            }),
+        ]);
+
+        await menu.show({
+            clickEvent: event,
+        });
+    }
+
+    return {
+        switchLanguage,
+        hasLanguages: I18nController.shared.availableLanguages.length > 1
+    };
+}
