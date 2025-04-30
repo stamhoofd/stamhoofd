43182725f718c24f9b0a5abd59eb3c857eee5911
Bjarne <bjarne.backx@gmail.com>
2025-04-30 15:37:25 +0200

    Added disabled by default

--- frontend/shared/components/src/members/MembersTableView.vue
+++ frontend/shared/components/src/members/MembersTableView.vue
@@ -314,6 +314,7 @@ const allColumns: Column<ObjectType, any>[] = [
             minimumWidth: 120,
             recommendedWidth: 140,
             allowSorting: false,
+            enabled: false,
         })
         : null,
     new Column<ObjectType, string[]>({
