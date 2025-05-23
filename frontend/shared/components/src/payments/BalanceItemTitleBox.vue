11b54b55cbaa6efe0ff0bc76218a691d366b08fe
Simon Backx <simon@stamhoofd.be>
2025-05-23 15:40:23 +0200

    Hid prices from cancelled balance items

--- frontend/shared/components/src/payments/BalanceItemTitleBox.vue
+++ frontend/shared/components/src/payments/BalanceItemTitleBox.vue
@@ -1,6 +1,6 @@
 <template>
     <template v-if="paymentStatus === null">
-        <p v-if="item.dueAt && item.priceOpen >= 0" class="style-title-prefix-list" :class="{error: item.isOverDue}">
+        <p v-if="item.dueAt && item.price >= 0" class="style-title-prefix-list" :class="{error: item.isOverDue}">
             <span>{{ $t('04392571-f6a2-470d-b2a1-90004ac27dbc', {date: formatDate(item.dueAt)}) }}</span>
             <span v-if="item.isOverDue" class="icon error small" />
         </p>
@@ -12,7 +12,7 @@
             <span>{{ $t('0c39a71f-be73-4404-8af0-cd9f238d2060') }}</span>
             <span class="icon undo small" />
         </p>
-        <p v-else-if="item.priceOpen < 0" class="style-title-prefix-list">
+        <p v-else-if="item.price >= 0 && item.priceOpen < 0" class="style-title-prefix-list">
             <span v-if="isPayable">{{ $t('bdf22906-037e-4221-8d3e-113bc62da28e') }}</span>
             <span v-else>{{ $t('c59769e0-b0fa-42f3-b713-82a2d7237a9c') }}</span>
             <span class="icon undo small" />
@@ -26,7 +26,7 @@
     <p v-if="item.itemDescription" class="style-description-small pre-wrap" v-text="item.itemDescription" />
 
     <template v-if="showPrices">
-        <template v-if="paymentStatus === null">
+        <template v-if="paymentStatus === null && item.status !== BalanceItemStatus.Canceled">
             <p v-if="item.price === item.amount * item.unitPrice" class="style-description-small">
                 {{ formatFloat(item.amount) }} x {{ formatPrice(item.unitPrice) }}
             </p>
