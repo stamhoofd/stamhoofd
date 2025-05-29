1c6b5ba630ee79287f4c7fcb7e1d5a02cb5c85a8
Bjarne <bjarne.backx@gmail.com>
2025-05-29 18:32:12 +0200

    Added blacklist for isLocationCustomDomain

--- frontend/shared/components/src/helpers/isLocationCustomDomain.ts
+++ frontend/shared/components/src/helpers/isLocationCustomDomain.ts
@@ -1,12 +1,24 @@
 import { Webshop } from '@stamhoofd/structures';
 
 export function isLocationCustomDomain(webshop: Webshop) {
+    if (STAMHOOFD.environment === 'development') {
+        return true;
+    }
+
     if (!webshop.hasCustomDomain) {
-        if (STAMHOOFD.environment === 'development') {
-            return true;
-        }
         return false;
     }
+
     const configuredDomain = webshop.domain;
+    if (configuredDomain === null) {
+        return false;
+    }
+
+    const blackList = new Set([...Object.values(STAMHOOFD.domains.webshop ?? {}), STAMHOOFD.domains.legacyWebshop, STAMHOOFD.domains.dashboard, ...Object.values(STAMHOOFD.domains.registration ?? {})]);
+
+    if (blackList.has(configuredDomain)) {
+        return false;
+    }
+
     return location.hostname === configuredDomain;
 }
