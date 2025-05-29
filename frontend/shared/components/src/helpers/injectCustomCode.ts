e31a8181edded472d9f69c2bb4c3cff6c65192e4
Bjarne <bjarne.backx@gmail.com>
2025-05-29 15:49:11 +0200

    Refactored inject custom code

--- frontend/shared/components/src/helpers/injectCustomCode.ts
+++ frontend/shared/components/src/helpers/injectCustomCode.ts
@@ -8,34 +8,5 @@ export function injectCustomCode(webshop: Webshop) {
         return;
     }
 
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(customCode, 'text/html');
-
-    for (const child of [...doc.body.children]) {
-        if (isScriptElement(child)) {
-            const script = document.createElement('script');
-            [...child.attributes].forEach((attr) => {
-                script.setAttribute(attr.name, attr.value);
-            });
-
-            script.text = child.innerHTML;
-            document.body.appendChild(script);
-            continue;
-        }
-
-        if (isStyleElement(child)) {
-            document.head.appendChild(child);
-            continue;
-        }
-
-        document.body.appendChild(child);
-    }
-}
-
-function isStyleElement(element: Element): element is HTMLScriptElement {
-    return element?.tagName === 'STYLE';
-}
-
-function isScriptElement(element: Element): element is HTMLScriptElement {
-    return element.tagName === 'SCRIPT';
+    document.head.appendChild(document.createRange().createContextualFragment(customCode));
 }
