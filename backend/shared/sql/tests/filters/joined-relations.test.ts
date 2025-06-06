17ad5e341343c6c607c09113b78451ff3b66c406
Simon Backx <simon@stamhoofd.be>
2025-06-06 08:30:42 +0200

    Added join only once test

--- backend/shared/sql/tests/filters/joined-relations.test.ts
+++ backend/shared/sql/tests/filters/joined-relations.test.ts
@@ -48,6 +48,37 @@ describe('Joined relations', () => {
         });
     });
 
+    it('Joins only once when used twice', async () => {
+        const filters = {
+            ...baseSQLFilterCompilers,
+            name: createColumnFilter({ expression: SQL.column('name'), type: SQLValueType.String, nullable: false }),
+            organization: createJoinedRelationFilter(organizationJoin, {
+                ...baseSQLFilterCompilers,
+                id: createColumnFilter({ expression: SQL.column('id'), type: SQLValueType.String, nullable: false }),
+            }),
+        };
+
+        await testSelect({
+            filter: [
+                {
+                    organization: {
+                        id: '123',
+                    },
+                },
+                {
+                    organization: {
+                        id: '124',
+                    },
+                },
+            ],
+            filters,
+            query: {
+                query: 'SELECT `test_table`.* FROM `test_table` LEFT JOIN `organizations` ON `organizations`.`id` = `test_table`.`organizationId` WHERE `organizations`.`id` = ? AND `organizations`.`id` = ?',
+                params: ['123', '124'],
+            },
+        });
+    });
+
     it('Does not left join when always true', async () => {
         const filters = {
             ...baseSQLFilterCompilers,
