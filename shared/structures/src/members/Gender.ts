5bf753ed38a72a2c06b22ab94411babf425937b5
Bjarne <bjarne.backx@gmail.com>
2025-06-13 17:46:01 +0200

    Added translation keys for gender names

--- shared/structures/src/members/Gender.ts
+++ shared/structures/src/members/Gender.ts
@@ -7,10 +7,10 @@ export enum Gender {
 export function getGenderName(gender: Gender) {
     switch (gender) {
         case Gender.Male:
-            return 'Man';
+            return $t(`f972abd4-de1e-484b-b7da-ad4c75d37808`);
         case Gender.Female:
-            return 'Vrouw';
-        case Gender.Other:
-            return 'Andere';
+            return $t(`e21f499d-1078-4044-be5d-6693d2636699`);
+        default:
+            return $t(`60f13ba4-c6c9-4388-9add-43a996bf6bee`);
     }
 }
