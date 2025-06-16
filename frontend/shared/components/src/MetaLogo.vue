4e9bc7311d2c7b40208f919227c03ee340f58a17
Simon Backx <simon@stamhoofd.be>
2025-06-16 13:40:11 +0200

    Added webshop favicons
    fixes STA-667

--- frontend/shared/components/src/MetaLogo.vue
+++ frontend/shared/components/src/MetaLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="meta-logo" :class="{expand: metaData.expandLogo}">
-        <ImageComponent v-if="logo" :image="logo" :image-dark="logoDark"/>
+        <ImageComponent v-if="logo" :image="logo" :image-dark="logoDark" />
         <template v-else>
             <span class="organization-logo-text">{{ name }}</span>
         </template>
@@ -10,7 +10,7 @@
 <script setup lang="ts">
 import { ImageComponent, useDeviceWidth } from '@stamhoofd/components';
 import { OrganizationMetaData, WebshopMetaData } from '@stamhoofd/structures';
-import { computed } from 'vue';
+import { computed, onMounted } from 'vue';
 
 const props = defineProps<{
     metaData: OrganizationMetaData | WebshopMetaData;
@@ -21,6 +21,22 @@ const width = useDeviceWidth();
 
 const logo = computed(() => width.value > 800 ? (props.metaData.horizontalLogo ?? props.metaData.squareLogo) : (props.metaData.squareLogo ?? props.metaData.horizontalLogo));
 const logoDark = computed(() => width.value > 800 ? (props.metaData.horizontalLogoDark ?? props.metaData.squareLogoDark) : (props.metaData.squareLogoDark ?? props.metaData.horizontalLogoDark));
+
+onMounted(() => {
+    // Inject favicon if no favicon is present
+    if (!document.querySelector("link[rel='icon']")) {
+        const resolution = props.metaData.squareLogo?.getResolutionForSize(256, 256);
+
+        if (resolution && resolution.width === resolution.height) {
+            const path = resolution.file.getPublicPath();
+            const link = document.createElement('link');
+            link.rel = 'icon';
+            link.href = path;
+            link.type = path.endsWith('.png') ? 'image/png' : (path.endsWith('.svg') ? 'image/svg+xml' : 'image/jpeg');
+            document.head.appendChild(link);
+        }
+    }
+});
 
 </script>
 
