d89e913baeeccc987d009dace65e70f660b15dec
Simon Backx <simon@stamhoofd.be>
2025-07-02 17:14:12 +0200

    Fixed openai rate limits and implementation

--- .development/i18n-uuid/src/auto-translate/translators/OpenAiTranslator.ts
+++ .development/i18n-uuid/src/auto-translate/translators/OpenAiTranslator.ts
@@ -8,8 +8,8 @@ import { TranslationManager } from "../TranslationManager";
 import { Translator } from "./Translator";
 
 export class OpenAiTranslator extends Translator {
-    protected readonly maxBatchLength = 3000;
-    protected readonly queue = new PromiseQueue<Batch<any>>(3, 500);
+    protected readonly maxBatchLength = 15000;
+    protected readonly queue = new PromiseQueue<Batch<any>>(10, 60 * 1000 / 45); // 500 requests per minute
     protected readonly openai: OpenAI;
 
     constructor(manager: TranslationManager, options: AutoTranslateOptions) {
@@ -18,13 +18,11 @@ export class OpenAiTranslator extends Translator {
         this.openai = new OpenAI({
             apiKey: globals.OPENAI_API_KEY,
         });
-
-        throw new Error('Not implemented')
     }
 
     protected async generateResponse(prompt: string): Promise<string> {
         const result = await this.openai.chat.completions.create({
-            model: "gpt-4o-mini",
+            model: "gpt-4.1-mini-2025-04-14",
             store: true,
             messages: [{ role: "user", content: prompt }],
         });
@@ -32,5 +30,19 @@ export class OpenAiTranslator extends Translator {
         console.log(chalk.blue(JSON.stringify(result)));
 
         return result.choices[0].message.content ?? "";
+    }
+
+    protected override transformParsedJson<T>(parsedJson: T): T {
+        if(Array.isArray(parsedJson)) {
+            return parsedJson.map(value => {
+                if(typeof value === 'string' && value.trim().length === 0) {
+                    return null;
+                }
+
+                return value;
+            }) as T;
+        }
+
+        return parsedJson;
     }
 }
