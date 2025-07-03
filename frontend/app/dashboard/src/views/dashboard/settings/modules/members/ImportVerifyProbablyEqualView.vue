4dd2e18417da6f72813461c5659ac77faec25afe
Bjarne <bjarne.backx@gmail.com>
2025-07-03 14:36:22 +0200

    Refactored ImportVerifyProbablyEqualView with SaveView

--- frontend/app/dashboard/src/views/dashboard/settings/modules/members/ImportVerifyProbablyEqualView.vue
+++ frontend/app/dashboard/src/views/dashboard/settings/modules/members/ImportVerifyProbablyEqualView.vue
@@ -1,62 +1,50 @@
 <template>
-    <div class="st-view">
-        <STNavigationBar title="Lijken op elkaar" :dismiss="canDismiss" :pop="canPop" />
+    <SaveView :title="$t('Lijken op elkaar')" :save-text="$t('Volgende')" @save="goNext">
+        <h1>
+            Vink de rijen aan als het om dezelfde personen gaat
+        </h1>
+        <p>We zijn niet 100% zeker dat deze leden uit Stamhoofd en jouw bestand op dezelfde persoon duiden (bv. door een typfout of een vergissing in de geboortedatum). Kan je dit manueel verifiëren? De linkse gegevens uit jouw bestand zullen de rechtse (= uit Stamhoofd) overschrijven als je ze aanvinkt.</p>
 
-        <main>
-            <h1>
-                Vink de rijen aan als het om dezelfde personen gaat
-            </h1>
-            <p>We zijn niet 100% zeker dat deze leden uit Stamhoofd en jouw bestand op dezelfde persoon duiden (bv. door een typfout of een vergissing in de geboortedatum). Kan je dit manueel verifiëren? De linkse gegevens uit jouw bestand zullen de rechtse (= uit Stamhoofd) overschrijven als je ze aanvinkt.</p>
+        <button v-if="!isCheckedAll" class="button text" type="button" @click="checkAll">
+            Alles aanvinken
+        </button>
+        <button v-else class="button text" type="button" @click="uncheckAll">
+            Alles uitvinken
+        </button>
 
-            <button v-if="!isCheckedAll" class="button text" type="button" @click="checkAll">
-                Alles aanvinken
-            </button>
-            <button v-else class="button text" type="button" @click="uncheckAll">
-                Alles uitvinken
-            </button>
+        <STList>
+            <STListItem v-for="(match, index) in members" :key="index" element-name="label" :selectable="true">
+                <div>
+                    <h2 class="style-title-list">
+                        {{ match.name }}
+                    </h2>
+                    <p class="style-description-small">
+                        {{ match.birthDayFormatted || "/" }}
+                    </p>
+                </div>
 
-            <STList>
-                <STListItem v-for="(match, index) in members" :key="index" element-name="label" :selectable="true">
+                <template #right>
                     <div>
                         <h2 class="style-title-list">
-                            {{ match.name }}
+                            {{ match.existingMember.member.details.name }}
                         </h2>
                         <p class="style-description-small">
-                            {{ match.birthDayFormatted || "/" }}
+                            {{ match.existingMember.member.details.birthDayFormatted || "/" }}
                         </p>
                     </div>
+                </template>
 
-                    <template #right>
-                        <div>
-                            <h2 class="style-title-list">
-                                {{ match.existingMember.member.details.name }}
-                            </h2>
-                            <p class="style-description-small">
-                                {{ match.existingMember.member.details.birthDayFormatted || "/" }}
-                            </p>
-                        </div>
-                    </template>
-
-                    <template #left>
-                        <Checkbox :model-value="match.isEqual" @update:model-value="setEqual(match, $event)" />
-                    </template>
-                </STListItem>
-            </STList>
-        </main>
-
-        <STToolbar>
-            <template #right>
-                <button class="button primary" type="button" @click="goNext">
-                    Verder
-                </button>
-            </template>
-        </STToolbar>
-    </div>
+                <template #left>
+                    <Checkbox :model-value="match.isEqual" @update:model-value="setEqual(match, $event)" />
+                </template>
+            </STListItem>
+        </STList>
+    </SaveView>
 </template>
 
 <script lang="ts" setup>
-import { ComponentWithProperties, PushOptions, useCanDismiss, useCanPop, useShow } from '@simonbackx/vue-app-navigation';
-import { Checkbox, STList, STListItem, STNavigationBar, STToolbar } from '@stamhoofd/components';
+import { ComponentWithProperties, PushOptions, useShow } from '@simonbackx/vue-app-navigation';
+import { Checkbox, STList, STListItem } from '@stamhoofd/components';
 import { PlatformMember } from '@stamhoofd/structures';
 import { computed } from 'vue';
 import { ExistingMemberResult } from '../../../../../classes/import/ExistingMemberResult';
@@ -66,8 +54,6 @@ const props = defineProps<{
     onVerified: (show: (options: PushOptions | ComponentWithProperties) => Promise<void>) => void;
 }>();
 
-const canDismiss = useCanDismiss();
-const canPop = useCanPop();
 const show = useShow();
 
 const isCheckedAll = computed(() => {
