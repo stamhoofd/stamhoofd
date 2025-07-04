712386608fbdeccb3fe6392de2a6e86289857cb6
Benjamin De Letter <bdeletter@gmail.com>
2025-07-04 14:11:19 +0200

    Updated migration to use default character set en collate

--- backend/shared/models/src/migrations/1751545537-create-uitpas-tokens.sql
+++ backend/shared/models/src/migrations/1751545537-create-uitpas-tokens.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `uitpas_tokens` (
-   `id` varchar(36) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+   `id` varchar(36) NOT NULL,
    `clientId` varchar(255) NOT NULL,
    `clientSecret` varchar(255) NOT NULL,
    `createdAt` date NOT NULL,
