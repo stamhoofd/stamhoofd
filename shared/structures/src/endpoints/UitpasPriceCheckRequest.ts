87913d4db74b1183dd3d7a0ac96411ac66f0efa2
Benjamin De Letter <bdeletter@gmail.com>
2025-07-04 17:11:56 +0200

    added uitpas number validation endpoint

---
+++ shared/structures/src/endpoints/UitpasPriceCheckRequest.ts
@@ -0,0 +1,17 @@
+import { AutoEncoder, StringDecoder, field, IntegerDecoder } from '@simonbackx/simple-encoding';
+
+export class UitpasPriceCheckRequest extends AutoEncoder {
+    @field({ decoder: IntegerDecoder })
+    price: number;
+
+    @field({ decoder: StringDecoder, nullable: true })
+    uitpasEventId: string;
+
+    @field({ decoder: StringDecoder })
+    uitpasNumber: string;
+}
+
+export class UitpasPriceCheckResponse extends AutoEncoder {
+    @field({ decoder: IntegerDecoder })
+    price: number;
+}
