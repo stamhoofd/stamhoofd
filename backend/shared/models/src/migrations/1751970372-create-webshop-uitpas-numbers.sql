b331cb28c9e6fd605fde48ac1cda32a918bb03c3
Benjamin De Letter <bdeletter@gmail.com>
2025-07-08 15:23:06 +0200

    changed articleId to productId

--- backend/shared/models/src/migrations/1751970372-create-webshop-uitpas-numbers.sql
+++ backend/shared/models/src/migrations/1751970372-create-webshop-uitpas-numbers.sql
@@ -2,7 +2,7 @@ CREATE TABLE `webshop_uitpas_numbers` (
    `id` varchar(36) NOT NULL,
    `uitpasNumber` varchar(36) NOT NULL,
    `webshopId` varchar(36) NOT NULL,
-   `articleId` varchar(36) NOT NULL,
+   `productId` varchar(36) NOT NULL,
    `orderId` varchar(36) NOT NULL,
    PRIMARY KEY (id),
    CONSTRAINT `fk_webshop` FOREIGN KEY (`webshopId`) REFERENCES `webshops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
