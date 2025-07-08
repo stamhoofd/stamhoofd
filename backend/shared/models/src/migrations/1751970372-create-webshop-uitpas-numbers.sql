ece1ff224283e5d590c722ff295f257b74d11f6a
Benjamin De Letter <bdeletter@gmail.com>
2025-07-08 15:23:36 +0200

    added extra foreign key constraint on orderId

--- backend/shared/models/src/migrations/1751970372-create-webshop-uitpas-numbers.sql
+++ backend/shared/models/src/migrations/1751970372-create-webshop-uitpas-numbers.sql
@@ -6,4 +6,5 @@ CREATE TABLE `webshop_uitpas_numbers` (
    `orderId` varchar(36) NOT NULL,
    PRIMARY KEY (id),
    CONSTRAINT `fk_webshop` FOREIGN KEY (`webshopId`) REFERENCES `webshops` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+   CONSTRAINT `fk_order` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 );
