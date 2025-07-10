639b460981d28add706351f04474f9f8db1420bd
Benjamin De Letter <bdeletter@gmail.com>
2025-07-10 15:16:28 +0200

    Shortcut to add UiTPAS social tariff + message notifying non-automatically registering (#295)
    * added shortcut button for UiTPAS social tariff (only displayed when no tariff already marked as UiTPAS)
    
    * Added message when selecting UiTPAS social tariff
    
    * added computed property uitpasBaseProductPriceAvailable for readability

--- frontend/app/dashboard/src/views/dashboard/webshop/edit/products/EditProductView.vue
+++ frontend/app/dashboard/src/views/dashboard/webshop/edit/products/EditProductView.vue
@@ -51,7 +51,7 @@
         <hr><h2 class="style-with-button">
             <div>{{ $t('2b96c0b9-6c20-4962-8e99-ff898d893a0d') }}</div>
             <div>
-                <button class="button text only-icon-smartphone" type="button" @click="addProductPrice">
+                <button class="button text only-icon-smartphone" type="button" @click="addProductPrice()">
                     <span class="icon add" />
                     <span>{{ $t('52bff8d2-52af-4d3f-b092-96bcfa4c0d03') }}</span>
                 </button>
@@ -83,6 +83,16 @@
         </template>
 
         <hr><STList>
+            <STListItem v-if="!patchedProduct.prices.some(p => p.uitpasBaseProductPriceId !== null) && uitpasBaseProductPriceAvailable" :selectable="true" element-name="button" @click="addProductPrice(true)">
+                <template #left>
+                    <span class="icon add gray" />
+                </template>
+
+                <h3 class="style-title-list">
+                    {{ $t('Apart UiTPAS kansentarief') }}
+                </h3>
+            </STListItem>
+
             <STListItem v-if="seatingPlan" :selectable="true" element-name="button" @click="chooseSeatingPlan">
                 <template #left>
                     <span class="icon seat gray" />
@@ -755,8 +765,30 @@ function addField() {
     ).catch(console.error);
 }
 
-function addProductPrice() {
+const productPricesAvailableForUitpasBaseProductPrice = computed(() => {
+    return patchedProduct.value.prices.filter(p => (p.uitpasBaseProductPriceId === null));
+});
+
+const uitpasBaseProductPriceAvailable = computed(() => {
+    return productPricesAvailableForUitpasBaseProductPrice.value.length !== 0;
+});
+
+async function addProductPrice(enableUitpasSocialTariff: boolean = false) {
     const price = ProductPrice.create({});
+    if (enableUitpasSocialTariff) {
+        if (!uitpasBaseProductPriceAvailable.value) {
+            // should not be possible
+            Toast.error($t('Er zijn geen prijzen beschikbaar om een apart UiTPAS kansentarief aan toe te voegen.')).show();
+            return;
+        }
+        const isConfirmed = await CenteredMessage.confirm($t('De toegepaste kortingen worden niet automatisch geregistreerd en doorgestuurd naar UiTPAS. Je ontvangt geen terugbetalingen van UiTPAS.'), $t('Ok'), undefined, $t('Annuleren'), false);
+        if (!isConfirmed) {
+            return;
+        }
+        price.uitpasBaseProductPriceId = productPricesAvailableForUitpasBaseProductPrice.value[0].id;
+        price.name = 'UiTPAS kansentarief';
+    }
+
     const p = Product.patch({ id: props.product.id });
     p.prices.addPut(price);
 
