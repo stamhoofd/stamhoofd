da4870cd9e9bfba368eda58b03f11382452bcaee
Simon Backx <simon@stamhoofd.be>
2025-07-15 17:29:51 +0200

    Fixed missing back buttons in NoPermissionsView

--- frontend/app/dashboard/src/views/login/NoPermissionsView.vue
+++ frontend/app/dashboard/src/views/login/NoPermissionsView.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="st-view">
+        <STNavigationBar :large="true" />
+
         <main class="center">
             <h1>{{ $t('a7e6596c-9614-426a-a9ef-dc6e32ec2864') }}</h1>
             <p>{{ $t('3abb6fe0-18cc-492b-9221-0a7825ecc9a2') }} {{ organization.name }}{{ $t('cf581821-fd16-416c-bdd2-be5315810373') }}</p>
