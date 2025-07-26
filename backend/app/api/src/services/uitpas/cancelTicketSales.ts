430eb6524b09c23fed7651da78f6bf36de885644
Benjamin De Letter <bdeletter@gmail.com>
2025-07-26 23:37:38 +0200

    added register and cancel ticket sale with UiTPAS

---
+++ backend/app/api/src/services/uitpas/cancelTicketSales.ts
@@ -0,0 +1,41 @@
+import { SimpleError } from '@simonbackx/simple-errors';
+
+async function cancelTicketSale(access_token: string, ticketSaleId: string) {
+    // https://docs.publiq.be/docs/uitpas/uitpas-api/reference/operations/delete-a-ticket-sale
+    const url = 'https://api-test.uitpas.be/ticket-sales/' + ticketSaleId;
+    const myHeaders = new Headers();
+    myHeaders.append('Authorization', 'Bearer ' + access_token);
+    myHeaders.append('Accept', 'application/json');
+    const requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders,
+    };
+    const response = await fetch(url, requestOptions).catch(() => {
+        // Handle network errors
+        throw new SimpleError({
+            code: 'uitpas_unreachable_registering_ticket_sales',
+            message: `Network issue when registering UiTPAS ticket sales`,
+            human: $t(
+                `We konden UiTPAS niet bereiken om de ticket verkoop te registreren bij UiTPAS. Probeer het later opnieuw.`,
+            ),
+        });
+    });
+    if (!response.ok) {
+        throw new SimpleError({
+            code: 'unsuccessful_response_registering_ticket_sales',
+            message: `Unsuccessful response when registering UiTPAS ticket sales`,
+            human: $t(`Er is een fout opgetreden bij het verbinden met UiTPAS. Probeer het later opnieuw.`),
+        });
+    }
+    return ticketSaleId;
+}
+
+/**
+ * Will never throw an error, but will return array of successfully canceled ticket sale ids
+ */
+export async function cancelTicketSales(access_token: string, ticketSaleIds: string[]) {
+    // https://docs.publiq.be/docs/uitpas/uitpas-api/reference/operations/delete-a-ticket-sale
+    const promises = ticketSaleIds.map(ticketSaleId => cancelTicketSale(access_token, ticketSaleId));
+    const results = await Promise.allSettled(promises);
+    return results.filter(result => result.status === 'fulfilled').map(result => result.value);
+}
