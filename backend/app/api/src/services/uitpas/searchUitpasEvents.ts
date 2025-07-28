e25cf866c85d6a8777bb5faa277d8c5d9df143e9
Benjamin De Letter <bdeletter@gmail.com>
2025-07-28 18:27:08 +0200

    implemented search uitpas event and view

---
+++ backend/app/api/src/services/uitpas/searchUitpasEvents.ts
@@ -0,0 +1,141 @@
+import { UitpasEventResponse, UitpasEventsResponse } from '@stamhoofd/structures';
+import { SimpleError } from '@simonbackx/simple-errors';
+
+type EventsResponse = {
+    totalItems: number;
+    itemsPerPage: number;
+    member: Array<{
+        '@id': string;
+        'name': {
+            nl: string;
+        };
+        'location': {
+            name: object;
+        };
+        'startDate'?: string;
+        'endDate'?: string;
+    }>;
+};
+
+function assertIsEventsResponse(json: unknown): asserts json is EventsResponse {
+    if (
+        typeof json !== 'object'
+        || json === null
+        || !('totalItems' in json)
+        || typeof json.totalItems !== 'number'
+        || !('itemsPerPage' in json)
+        || typeof json.itemsPerPage !== 'number'
+        || !('member' in json)
+        || !Array.isArray(json.member)
+        || json.member.some(member => (
+            typeof member !== 'object'
+            || member === null
+            || !('@id' in member)
+            || typeof member['@id'] !== 'string'
+            || !('name' in member)
+            || typeof member.name !== 'object'
+            || member.name === null
+            || !('nl' in member.name)
+            || typeof member.name.nl !== 'string'
+            || !('location' in member)
+            || typeof member.location !== 'object'
+            || member.location === null
+            || !('name' in member.location)
+            || typeof member.location.name !== 'object'
+            || member.location.name === null
+        ))
+    ) {
+        console.error('Invalid events response', json);
+        throw new SimpleError({
+            code: 'invalid_events_response',
+            message: `Invalid events response`,
+            human: $t(`Er is een fout opgetreden bij het ophalen van de UiTPAS-evenementen.`),
+        });
+    }
+}
+
+export async function searchUitpasEvents(clientId: string, uitpasOrganizerId: string, textQuery?: string): Promise<UitpasEventsResponse> {
+    // uses no credentials (only client id of the organization)
+    // https://docs.publiq.be/docs/uitpas/events/searching
+    if (!clientId) {
+        throw new SimpleError({
+            code: 'no_client_id_for_uitpas_events',
+            message: `No client ID configured for Uitpas events`,
+            human: $t(`Er is geen UiTPAS client ID geconfigureerd voor deze organisatie.`),
+        });
+    }
+    const baseUrl = 'https://search-test.uitdatabank.be/events';
+    const params = new URLSearchParams();
+    params.append('clientId', clientId);
+    params.append('organizerId', uitpasOrganizerId);
+    params.append('embed', 'true');
+    params.append('uitpas', 'true');
+    params.append('start', '0');
+    params.append('limit', '200');
+    if (textQuery) {
+        params.append('text', textQuery);
+    }
+    const url = `${baseUrl}?${params.toString()}`;
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+    };
+    const response = await fetch(url, requestOptions).catch(() => {
+        // Handle network errors
+        throw new SimpleError({
+            code: 'uitpas_unreachable_searching_events',
+            message: `Network issue when searching for UiTPAS events`,
+            human: $t(
+                `We konden UiTPAS niet bereiken om UiTPAS-evenementen op te zoeken. Probeer het later opnieuw.`,
+            ),
+        });
+    });
+    if (!response.ok) {
+        console.error('Unsuccessful response when searching for UiTPAS events', response);
+        throw new SimpleError({
+            code: 'unsuccessful_response_searching_uitpas_events',
+            message: `Unsuccessful response when searching for UiTPAS events`,
+            human: $t(`Er is een fout opgetreden bij het verbinden met UiTPAS. Probeer het later opnieuw.`),
+        });
+    }
+    const json = await response.json().catch(() => {
+        // Handle JSON parsing errors
+        throw new SimpleError({
+            code: 'invalid_json_searching_uitpas_events',
+            message: `Invalid json when searching for UiTPAS events`,
+            human: $t(
+                `Er is een fout opgetreden bij het communiceren met UiTPAS. Probeer het later opnieuw.`,
+            ),
+        });
+    });
+
+    assertIsEventsResponse(json);
+    const eventsResponse = new UitpasEventsResponse();
+    eventsResponse.totalItems = json.totalItems;
+    eventsResponse.itemsPerPage = json.itemsPerPage;
+    eventsResponse.member = json.member.map((member) => {
+        const event = new UitpasEventResponse();
+        event.url = member['@id'];
+        event.name = member.name.nl;
+        const locationName = member.location.name as Record<string, string>;
+        const entrs = Object.entries(locationName);
+        const hasNl = entrs.find(([key]) => key === 'nl');
+        if (hasNl) {
+            event.location = locationName.nl;
+        }
+        else {
+            const lang = entrs[0];
+            event.location = lang ? lang[1] : '';
+        }
+        if (member.startDate) {
+            event.startDate = new Date(member.startDate);
+        }
+        if (member.endDate) {
+            event.endDate = new Date(member.endDate);
+        }
+        return event;
+    });
+    return eventsResponse;
+}
---
+++ frontend/shared/components/src/organizations/components/SearchUitpasEventView.vue
@@ -0,0 +1,173 @@
+<template>
+    <div class="st-view search-uitpas-event-view">
+        <STNavigationBar :title="t" />
+
+        <main>
+            <h1>{{ t }}</h1>
+            <p class="style-description-small">
+                {{ $t('Door een evenement uit de UiTdatabank te koppelen kan {platform} een verkoop met kansentarief registreren bij jouw UiTPAS-regio. Je krijgt dan automatisch een (gedeeltelijke) terugbetaling en jouw regio bepaalt het kansentarief.', { platform: capitalizeFirstLetter(STAMHOOFD.platformName) }) }}
+            </p>
+            <p v-if="showNoteAboutNonOfficialFlow" class="style-description-small">
+                <I18nComponent :t="$t('Wil je de UiTPAS-nummers niet doorsturen en zelf het tarief instellen, <button>klik dan hier</button>.')">
+                    <template #button="{content}">
+                        <button class="inline-link" type="button" @click="doSelectEvent(null)">
+                            {{ content }}
+                        </button>
+                    </template>
+                </I18nComponent>
+            </p>
+            <STErrorsDefault :error-box="errors.errorBox" />
+
+            <form class="search-box input-icon-container icon search gray" @submit.prevent>
+                <input ref="input" v-model="text" :autofocus="true" class="input" name="search" inputmode="search" type="search" enterkeyhint="search" autocorrect="off" autocomplete="off" :spellcheck="false" autocapitalize="off" :placeholder="$t('Zoek op naam, locatie, beschrijving, ...')">
+            </form>
+
+            <Spinner v-if="loadingResults || (loadingAfterEventSelect && !selectedEventUrl)" class="gray center" />
+            <STList v-else>
+                <STListItem v-for="event in results" :key="event.url" :selectable="true" @click="doSelectEvent(event)">
+                    <template #left>
+                        <DateBox :date="event.startDate ?? event.endDate ?? undefined" />
+                    </template>
+                    <h2>{{ event.name }}</h2>
+                    <p class="style-description-small">
+                        <span class="style-description-small">{{ event.location }}</span>
+                    </p>
+                    <template #right>
+                        <Spinner v-if="loadingAfterEventSelect && (event.url === selectedEventUrl)" class="gray center" />
+                        <span v-else class="icon arrow-right-small gray" />
+                    </template>
+                </STListItem>
+            </STList>
+            <p v-if="!loadingResults && results.length === 0 && text" class="info-box">
+                {{ $t('Geen evenementen gevonden.') }}
+            </p>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Decoder } from '@simonbackx/simple-encoding';
+import { Request } from '@simonbackx/simple-networking';
+import { ErrorBox, NavigationActions, Spinner, Toast, useContext, useErrors, useNavigationActions, DateBox } from '@stamhoofd/components';
+import { UitpasEventResponse, UitpasEventsResponse } from '@stamhoofd/structures';
+import { useRequestOwner } from '@stamhoofd/networking';
+import { throttle } from '@stamhoofd/utility';
+import { Ref, ref, watch } from 'vue';
+import { I18nComponent } from '@stamhoofd/frontend-i18n';
+
+const props = withDefaults(
+    defineProps<{
+        title?: string;
+        selectEvent: (UitpasEventResponse: UitpasEventResponse | null, navigation: NavigationActions) => Promise<void> | void;
+        showNoteAboutNonOfficialFlow?: boolean;
+    }>(), {
+        title: $t('UiTPAS-evenement zoeken'),
+        showNoteAboutNonOfficialFlow: true,
+    },
+);
+
+const context = useContext();
+const navigationActions = useNavigationActions();
+const loadingResults = ref(false);
+const text = ref('');
+const results = ref([]) as Ref<UitpasEventResponse[]>;
+const owner = useRequestOwner();
+const errors = useErrors();
+const t = props.title;
+
+let lastText = '';
+let counter = 0;
+
+const loadingAfterEventSelect = ref(false);
+const selectedEventUrl = ref('');
+
+const updateResults = async () => {
+    const t = text.value;
+    const cachedCount = counter;
+
+    if (t.length === 0) {
+        results.value = [];
+        loadingResults.value = false;
+        return;
+    }
+    loadingResults.value = true;
+
+    try {
+        const response = await context.value.authenticatedServer.request({
+            method: 'GET',
+            path: '/organization/search-uitpas-events',
+            query: { text: t },
+            decoder: UitpasEventsResponse as Decoder<UitpasEventsResponse>,
+            owner,
+        });
+
+        if (cachedCount !== counter) {
+            // A new request have started already
+            return;
+        }
+        results.value = response.data.member;
+    }
+    catch (e) {
+        if (cachedCount !== counter) {
+            // A new request have started already
+            return;
+        }
+        if (!Request.isAbortError(e)) {
+            console.error(e);
+            Toast.fromError(e).show();
+        }
+        results.value = [];
+    }
+
+    if (cachedCount !== counter) {
+        // A new request have started already
+        return;
+    }
+    loadingResults.value = false;
+};
+
+const throttleUpdateResults = throttle(updateResults, 500);
+
+const startUpdateResults = async () => {
+    const value = text.value;
+    if (value === lastText) {
+        return;
+    }
+    lastText = value;
+    loadingResults.value = true;
+    counter += 1;
+
+    Request.cancelAll(owner);
+
+    if (value.length === 0) {
+        results.value = [];
+        loadingResults.value = false;
+        return;
+    }
+    throttleUpdateResults();
+};
+
+watch(text, startUpdateResults);
+
+async function doSelectEvent(event: UitpasEventResponse | null) {
+    try {
+        errors.errorBox = null;
+        selectedEventUrl.value = event?.url ?? '';
+        loadingAfterEventSelect.value = true;
+        await props.selectEvent(event, navigationActions);
+        loadingAfterEventSelect.value = false;
+    }
+    catch (e) {
+        errors.errorBox = new ErrorBox(e);
+    }
+}
+
+</script>
+
+<style lang="scss">
+.search-uitpas-event-view {
+    .search-box {
+        margin: 15px 0;
+    }
+}
+</style>
