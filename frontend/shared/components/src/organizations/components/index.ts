c26aa4e2af8cf5319b8052db117948d91bf07d83
Benjamin De Letter <bdeletter@gmail.com>
2025-07-28 18:27:08 +0200

    implemented search uitpas event and view

--- frontend/shared/components/src/organizations/components/index.ts
+++ frontend/shared/components/src/organizations/components/index.ts
@@ -2,4 +2,5 @@ export { default as CompanyInputBox } from './CompanyInputBox.vue';
 export { default as EditCompanyView } from './EditCompanyView.vue';
 export { default as OrganizationSelect } from './OrganizationSelect.vue';
 export { default as SearchUitpasOrganizerView } from './SearchUitpasOrganizerView.vue';
+export { default as SearchUitpasEventView } from './SearchUitpasEventView.vue';
 export { default as SetUitpasClientCredentialsView } from './SetUitpasClientCredentialsView.vue';
