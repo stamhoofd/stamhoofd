cae98e406671a0607f4ecc674d07e65775f553fc
Bjarne <bjarne.backx@gmail.com>
2025-07-29 17:12:49 +0200

    Added create groups, periods and registration periods

--- backend/shared/models/src/factories/RegistrationPeriodFactory.ts
+++ backend/shared/models/src/factories/RegistrationPeriodFactory.ts
@@ -1,20 +1,21 @@
 import { Factory } from '@simonbackx/simple-database';
 import { RegistrationPeriodSettings } from '@stamhoofd/structures';
 
-import { RegistrationPeriod } from '../models';
+import { Organization, RegistrationPeriod } from '../models';
 
 class Options {
     startDate?: Date;
     endDate?: Date;
     previousPeriodId?: string;
     locked?: boolean;
+    organization?: Organization;
 }
 
 export class RegistrationPeriodFactory extends Factory<Options, RegistrationPeriod> {
     async create(): Promise<RegistrationPeriod> {
         const period = new RegistrationPeriod();
 
-        period.organizationId = null;
+        period.organizationId = this.options.organization ? this.options.organization.id : null;
         period.startDate = this.options.startDate ?? new Date(2024, 0, 1, 0, 0, 0, 0);
         period.endDate = this.options.endDate ?? new Date(2024, 11, 31, 59, 59, 59, 999);
         if (this.options.previousPeriodId) {
