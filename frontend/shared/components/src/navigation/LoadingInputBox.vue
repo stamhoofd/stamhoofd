dbf893243669164725ad2875c3babb21aaa8aa13
Benjamin De Letter <bdeletter@gmail.com>
2025-07-30 12:19:26 +0200

    removed unused ref

--- frontend/shared/components/src/navigation/LoadingInputBox.vue
+++ frontend/shared/components/src/navigation/LoadingInputBox.vue
@@ -2,7 +2,6 @@
     <div>
         <div v-if="loading || delayLoading" class="input-wrapper">
             <input
-                ref="ignore"
                 class="input with-spinner"
                 type="text"
                 autocomplete="off"
