5abb02fb9a6a33369bebabe42fc8b987a5acf9bc
Bjarne <bjarne.backx@gmail.com>
2025-08-13 16:19:45 +0200

    Improved ui text

--- frontend/shared/components/src/groups/EditGroupView.vue
+++ frontend/shared/components/src/groups/EditGroupView.vue
@@ -299,7 +299,7 @@
 
                 <button v-if="preventGroupIds.length === 0" type="button" class="button text only-icon-smartphone" @click="addPreventGroupIds">
                     <span class="icon add" />
-                    <span>{{ $t('Verhinder andere inschrijving') }}</span>
+                    <span>{{ $t('Verhinder inschrijven als ingeschreven bij') }}</span>
                 </button>
             </div>
 
@@ -493,7 +493,7 @@
             </JumpToContainer>
 
             <JumpToContainer v-if="patchedGroup.type === GroupType.Membership" class="container" :visible="forceShowPreventGroupIds || !!preventGroupIds.length">
-                <GroupIdsInput v-model="preventGroupIds" :default-period-id="patchedGroup.periodId" :title="$t('Verhinder andere inschrijvingen')" />
+                <GroupIdsInput v-model="preventGroupIds" :default-period-id="patchedGroup.periodId" :title="$t('Verhinder inschrijven als ingeschreven bij')" />
             </JumpToContainer>
 
             <template v-if="$feature('member-trials')">
