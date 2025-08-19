c88443125b0c6df07c569c587909cc091b93d75a
Simon Backx <simon@stamhoofd.be>
2025-08-19 15:48:33 +0200

    Enabled language switching in development

--- shared/build-development-env/src/presets/platform.ts
+++ shared/build-development-env/src/presets/platform.ts
@@ -2,5 +2,8 @@ export function build(): any {
     return {
         userMode: 'platform',
         fixedCountry: 'BE',
+        locales: {
+            BE: ['nl', 'fr', 'en'],
+        },
     };
 }
