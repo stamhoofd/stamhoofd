8e7183356ecd30f95ff8312448cdcbd6d4baa331
Bjarne <bjarne.backx@gmail.com>
2025-08-19 21:32:43 +0200

    Added period id checks to factories

--- backend/shared/models/src/factories/OrganizationRegistrationPeriodFactory.ts
+++ backend/shared/models/src/factories/OrganizationRegistrationPeriodFactory.ts
@@ -1,5 +1,6 @@
 import { Factory } from '@simonbackx/simple-database';
 
+import { SimpleError } from '@simonbackx/simple-errors';
 import { OrganizationRegistrationPeriod } from '../models';
 import { Organization } from '../models/Organization';
 import { RegistrationPeriod } from '../models/RegistrationPeriod';
@@ -14,7 +15,15 @@ export class OrganizationRegistrationPeriodFactory extends Factory<Options, Orga
         const organizationRegistrationPeriod = new OrganizationRegistrationPeriod();
 
         organizationRegistrationPeriod.organizationId = this.options.organization.id;
-        // todo: migrate-platform-period-id
+
+        if (STAMHOOFD.userMode === 'organization' && this.options.period.organizationId !== this.options.organization.id) {
+            throw new SimpleError({
+                code: 'invalid_period',
+                message: 'Period has different organization id then the organization',
+                statusCode: 400,
+            });
+        }
+
         organizationRegistrationPeriod.periodId = this.options.period.id;
 
         await organizationRegistrationPeriod.save();
