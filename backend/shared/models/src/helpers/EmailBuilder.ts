7ebc4d364b83fdf13c3419bef4036e71bc5398c5
Simon Backx <simon@stamhoofd.be>
2025-08-29 15:13:54 +0200

    Fixed eager regex

--- backend/shared/models/src/helpers/EmailBuilder.ts
+++ backend/shared/models/src/helpers/EmailBuilder.ts
@@ -487,7 +487,7 @@ export function stripSensitiveRecipientReplacements(recipient: Recipient | Email
         return Replacement.create({
             ...r,
             // Strip <span class="style-inline-code">(.*)</span> and replace content with XXXX-XXXX-XXXX-XXXX
-            html: r.html ? r.html.replace(/<span class="style-inline-code">.*<\/span>/g, '<span class="style-inline-code">•••• (' + $t('wel aanwezig in bericht') + ')</span>') : r.html,
+            html: r.html ? r.html.replace(/<span class="style-inline-code">.*?<\/span>/g, '<span class="style-inline-code">•••• (' + $t('wel aanwezig in bericht') + ')</span>') : r.html,
         });
     });
 }
