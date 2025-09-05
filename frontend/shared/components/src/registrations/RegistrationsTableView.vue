a9fb938ca912b8836218530ed944a2e8c0c2542b
Simon Backx <simon@stamhoofd.be>
2025-09-05 12:45:44 +0200

    Added note

--- frontend/shared/components/src/registrations/RegistrationsTableView.vue
+++ frontend/shared/components/src/registrations/RegistrationsTableView.vue
@@ -1,6 +1,9 @@
 <template>
     <LoadingViewTransition>
         <ModernTableView v-if="!loading" ref="modernTableView" :table-object-fetcher="tableObjectFetcher" :filter-builders="filterBuilders" :title="title" :column-configuration-id="configurationId" :default-filter="defaultFilter" :actions="actions" :all-columns="allColumns" :estimated-rows="estimatedRows" :Route="Route" :default-sort-column="defaultSortColumn" :default-sort-direction="defaultSortDirection">
+            <p v-if="app === 'admin' && !group" class="style-description-block">
+                {{ $t('Een lid kan dubbel staan in de lijst als het meerdere inschrijvingen heeft.') }}
+            </p>
             <template #empty>
                 {{ $t('6b029bcd-5605-42c5-a5db-38f40ab70214') }}
             </template>
