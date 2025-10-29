816242208a4dc7dae56ff03c9e362b593a1b0283
Simon Backx <simon@stamhoofd.be>
2025-10-29 12:28:19 +0100

    Updated default period time

--- backend/shared/models/src/migrations/1719568079-default-period.sql
+++ backend/shared/models/src/migrations/1719568079-default-period.sql
@@ -1,2 +1,2 @@
 INSERT INTO `registration_periods` (`id`, `startDate`, `endDate`, `locked`, `organizationId`, `settings`, `createdAt`, `updatedAt`) VALUES
-('9785ddf1-bb80-4e50-a7ae-6a3a1bc6c3ff', '2024-09-01 09:33:00', '2025-08-31 09:33:00', 0, NULL, '{\"value\": {}, \"version\": 263}', '2024-06-28 09:33:42', '2024-06-28 09:33:42');
+('9785ddf1-bb80-4e50-a7ae-6a3a1bc6c3ff', '2025-08-31 22:00:00', '2026-08-31 21:59:59', 0, NULL, '{\"value\": {}, \"version\": 263}', '2025-08-31 22:00:00', '2026-08-31 22:00:00');
