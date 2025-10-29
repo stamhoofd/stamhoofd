b85d760ff13dfa362679af69a274ab430f0e2b48
Simon Backx <simon@stamhoofd.be>
2025-10-29 12:11:55 +0100

    Added jambo development preset

---
+++ env.jambo.js
@@ -0,0 +1,24 @@
+module.exports = {
+    presets: [
+        // Run on default ports
+        "ports-primary", 
+
+        // Run local DNS server
+        "services/coredns", 
+
+        // Start caddy server
+        "services/caddy", 
+
+        // Start stripe webhook server
+        "services/stripe", 
+
+        // Connect with maildev smtp server
+        "services/maildev", 
+
+        // Check frontend types
+        "services/frontend-type-checking", 
+
+        // Default Keeo configuration
+        "jambo",
+    ],
+}
---
+++ shared/build-development-env/src/presets/jambo.ts
@@ -0,0 +1,42 @@
+import { Service } from '../Service';
+
+export function build(service: Service): any {
+    const config = {
+        presets: ['platform'],
+        domains: {
+            dashboard: 'jambo.dashboard.stamhoofd',
+            marketing: {
+                '': 'jamboree2027.be',
+            },
+            webshop: {
+                '': 'jambo.shop.stamhoofd',
+            },
+            api: 'jambo.api.stamhoofd',
+            rendererApi: 'jambo.renderer.stamhoofd',
+
+            defaultTransactionalEmail: {
+                '': 'stamhoofd.be',
+                'NL': 'stamhoofd.nl',
+            },
+
+            defaultBroadcastEmail: {
+                '': 'stamhoofd.email',
+            },
+            webshopCname: 'jambo.shop.stamhoofd',
+        },
+        translationNamespace: 'jamboree',
+        platformName: 'jamboree',
+        fixedCountry: 'BE',
+        DB_DATABASE: 'stamhoofd-jamboree',
+        locales: {
+            BE: ['nl', 'fr'],
+        },
+        singleOrganization: '1d444815-3213-4a16-9846-91a41f2de9a4',
+
+        // Enable member numbers
+        MEMBER_NUMBER_ALGORITHM: 'Incremental',
+        MEMBER_NUMBER_ALGORITHM_LENGTH: 10,
+    };
+
+    return config;
+}
