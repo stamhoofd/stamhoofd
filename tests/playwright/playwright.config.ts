888c57a64e30a8c0a52cced57448b7f20a8b7b2a
Bjarne <bjarne.backx@gmail.com>
2025-11-12 10:47:39 +0100

    Fixed test urls

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    // retries: process.env.CI ? 2 : 0,
-    retries: 2,
+    retries: process.env.CI ? 2 : 0,
+    // retries: 2,
     /* Opt out of parallel tests on CI. */
-    // workers: process.env.CI ? 1 : undefined,
-    workers: 1,
+    workers: process.env.CI ? 1 : undefined,
+    // workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
