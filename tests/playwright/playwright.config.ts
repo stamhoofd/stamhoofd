32a4ab28d1c951c18cf9d7c74a07954c330638f0
Bjarne <bjarne.backx@gmail.com>
2025-12-01 20:41:51 +0100

    Try higher timeout

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    retries: 2,
+    retries: 1,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
