e04b82331839b0bfdaa7c50463ffba579579d7de
Bjarne <bjarne.backx@gmail.com>
2025-12-01 14:29:27 +0100

    Added upload artifacts

--- tests/playwright/src/setup/helpers/getCurrentDir.ts
+++
@@ -1,6 +0,0 @@
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-export function getCurrentDir(importMeta: ImportMeta) {
-    return dirname(fileURLToPath(importMeta.url));
-}
