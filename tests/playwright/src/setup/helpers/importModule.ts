c3da3ade2f7a014bf06ac3c4e662de25619fabed
Bjarne <bjarne.backx@gmail.com>
2025-12-01 14:29:27 +0100

    Added upload artifacts

--- tests/playwright/src/setup/helpers/importModule.ts
+++ tests/playwright/src/setup/helpers/importModule.ts
@@ -2,7 +2,6 @@ import { createRequire } from "node:module";
 import { resolve } from "node:path";
 
 export function importModule(id: string) {
-    // todo: this may fail in CI
     const monoRepoRoot = resolve(__dirname, "../../../../../node_modules");
     const require = createRequire(monoRepoRoot);
     return require(id);
