bb153922eeaae585a5ed918394dde5916c7168c4
Bjarne <bjarne.backx@gmail.com>
2025-12-01 21:36:19 +0100

    Try only run login

--- tests/playwright/src/tests/login.spec.ts
+++ tests/playwright/src/tests/login.spec.ts
@@ -1,84 +1,84 @@
-// // test should always be imported first
-// import { test } from "../setup/fixtures";
+// test should always be imported first
+import { test } from "../setup/fixtures";
 
-// import { expect } from "@playwright/test";
-// import { Organization, OrganizationFactory, Token, User, UserFactory } from "@stamhoofd/models";
-// import { PermissionLevel, Permissions } from "@stamhoofd/structures";
-// import { TestUtils } from "@stamhoofd/test-utils";
+import { expect } from "@playwright/test";
+import { Organization, OrganizationFactory, Token, User, UserFactory } from "@stamhoofd/models";
+import { PermissionLevel, Permissions } from "@stamhoofd/structures";
+import { TestUtils } from "@stamhoofd/test-utils";
             
-// // login
-// test.describe("userMode organization", () => {
-//     console.log('inside: login - userMode organization')
-//     let organization: Organization;
-//     let user: User;
+// login
+test.describe("userMode organization", () => {
+    console.log('inside: login - userMode organization')
+    let organization: Organization;
+    let user: User;
 
-//     const organizationName = "Test Organization";
-//     const email = "john.doe@gmail.com";
-//     const password = "testAbc123456";
+    const organizationName = "Test Organization";
+    const email = "john.doe@gmail.com";
+    const password = "testAbc123456";
 
-//     test.beforeAll(
-//         async () => {
-//             console.log('inside: login - userMode organization - beforeAll start')
-//             TestUtils.setPermanentEnvironment("userMode", "organization");
+    test.beforeAll(
+        async () => {
+            console.log('inside: login - userMode organization - beforeAll start')
+            TestUtils.setPermanentEnvironment("userMode", "organization");
 
-//             organization = await new OrganizationFactory({
-//                 name: organizationName,
-//             }).create();
+            organization = await new OrganizationFactory({
+                name: organizationName,
+            }).create();
 
-//             user = await new UserFactory({
-//                 firstName: "John",
-//                 lastName: "Doe",
-//                 email,
-//                 password,
-//                 organization,
-//                 permissions: Permissions.create({
-//                     level: PermissionLevel.Full,
-//                 }),
-//             }).create();
+            user = await new UserFactory({
+                firstName: "John",
+                lastName: "Doe",
+                email,
+                password,
+                organization,
+                permissions: Permissions.create({
+                    level: PermissionLevel.Full,
+                }),
+            }).create();
 
-//             await Token.createToken(user);
-//             console.log('inside: login - userMode organization - beforeAll finished')
-//         },
-//     );
+            await Token.createToken(user);
+            console.log('inside: login - userMode organization - beforeAll finished')
+        },
+    );
 
-//     test("happy flow", async ({ page, dashboard }) => {
-//         console.log('inside: login - test userMode organization - start')
-//         await dashboard.goto();
+    test("happy flow", async ({ page, dashboard }) => {
+        console.log('inside: login - test userMode organization - start')
+        await dashboard.goto();
 
-//         // click search input and fill in organization name
-//         const searchInput = page.getByTestId("organization-search-input");
-//         await searchInput.click();
-//         await searchInput.fill(organizationName);
+        // click search input and fill in organization name
+        const searchInput = page.getByTestId("organization-search-input");
+        await searchInput.click();
+        await searchInput.fill(organizationName);
 
-//         // click organization
-//         await page
-//             .getByTestId("organization-button")
-//             .filter({ hasText: organizationName })
-//             .click();
+        // click organization
+        await page
+            .getByTestId("organization-button")
+            .filter({ hasText: organizationName })
+            .click();
 
-//         // fill in email
-//         const emailInput = page.getByTestId("email-input");
-//         await emailInput.click();
-//         await emailInput.fill(email);
+        // fill in email
+        const emailInput = page.getByTestId("email-input");
+        await emailInput.click();
+        await emailInput.fill(email);
 
-//         // fill in password
-//         const passwordInput = page.getByTestId("password-input");
-//         await passwordInput.click();
-//         await passwordInput.fill(password);
+        // fill in password
+        const passwordInput = page.getByTestId("password-input");
+        await passwordInput.click();
+        await passwordInput.fill(password);
 
-//         // login
-//         await page.getByTestId("login-button").click();
+        // login
+        await page.getByTestId("login-button").click();
 
-//         // wait for data-testid element to appear (h1 with name of organization)
-//         await page.getByTestId("organization-name").waitFor();
+        // wait for data-testid element to appear (h1 with name of organization)
+        await page.getByTestId("organization-name").waitFor();
 
-//         // check if page contains name of organization
-//         await expect(page.getByTestId("organization-name")).toContainText(
-//             organizationName,
-//         );
-//         console.log('inside: login - test userMode organization - done')
-//     });
-// });
+        // check if page contains name of organization
+        await expect(page.getByTestId("organization-name")).toContainText(
+            organizationName,
+        );
+        console.log('inside: login - test userMode organization - done')
+    });
+});
 
 // test.describe("userMode platform", () => {
 //     console.log('inside: userMode platform')
