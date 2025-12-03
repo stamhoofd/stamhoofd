81b2a3888e9edf665da535d3b3cc1272322705d5
Simon Backx <simon@stamhoofd.be>
2025-12-03 10:52:11 +0100

    New headless mode

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
             name: "chromium",
             use: {
                 ...devices["Desktop Chrome"],
+                channel: 'chromium',
             },
         },
     ],
