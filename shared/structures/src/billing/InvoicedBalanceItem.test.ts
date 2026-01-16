c210d4fe12836d1901c8c227f6444d97419f5690
Simon Backx <simon@stamhoofd.be>
2026-01-16 16:16:05 +0100

    Adjusted InvoicedBalanceItem structure

---
+++ shared/structures/src/billing/InvoicedBalanceItem.test.ts
@@ -0,0 +1,228 @@
+import { BalanceItem, VATExcemptReason } from '../BalanceItem.js';
+import { InvoicedBalanceItem } from './InvoicedBalanceItem.js';
+
+describe('InvoicedBalanceItem', () => {
+    describe('createFor', () => {
+        test('Create an invoice item for a balance item including VAT with quantity of 1', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 5_00_00; // 5 euro, including VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 21;
+            balanceItem.VATIncluded = true;
+            balanceItem.VATExcempt = null;
+
+            expect(balanceItem.priceWithVAT).toBe(5_00_00);
+            expect(balanceItem.priceWithoutVAT).toBe(4_13_22); // 5 / 1.21 + rounding
+
+            // Create an invoiced balance item for the full quantity
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 5_00_00);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(5_00_00);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+            expect(invoicedItem.VATPercentage).toBe(21);
+            expect(invoicedItem.VATIncluded).toBe(true);
+            expect(invoicedItem.VATExcempt).toBeNull();
+            expect(invoicedItem.totalWithoutVAT).toBe(4_13_00);
+        });
+
+        test('Create an invoice item for a balance item including VAT with quantity of 3', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 5_00_00; // 5 euro, including VAT
+            balanceItem.quantity = 3;
+            balanceItem.VATPercentage = 21;
+            balanceItem.VATIncluded = true;
+            balanceItem.VATExcempt = null;
+
+            expect(balanceItem.priceWithVAT).toBe(15_00_00);
+            expect(balanceItem.priceWithoutVAT).toBe(12_39_67); // 15 / 1.21 + rounding
+
+            // Create an invoiced balance item for one item
+            {
+                const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 5_00_00);
+
+                expect(invoicedItem.balanceInvoicedAmount).toBe(5_00_00);
+                expect(invoicedItem.quantity).toBe(1_00_00);
+                expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+                expect(invoicedItem.VATPercentage).toBe(21);
+                expect(invoicedItem.VATIncluded).toBe(true);
+                expect(invoicedItem.VATExcempt).toBeNull();
+                expect(invoicedItem.totalWithoutVAT).toBe(4_13_00);
+            }
+
+            // Create an invoiced balance item for two items
+            {
+                const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 10_00_00);
+
+                expect(invoicedItem.balanceInvoicedAmount).toBe(10_00_00);
+                expect(invoicedItem.quantity).toBe(2_00_00);
+                expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+                expect(invoicedItem.VATPercentage).toBe(21);
+                expect(invoicedItem.VATIncluded).toBe(true);
+                expect(invoicedItem.VATExcempt).toBeNull();
+                expect(invoicedItem.totalWithoutVAT).toBe(8_26_00);
+            }
+
+            // Create an invoiced balance item for three items
+            {
+                const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 15_00_00);
+
+                expect(invoicedItem.balanceInvoicedAmount).toBe(15_00_00);
+                expect(invoicedItem.quantity).toBe(3_00_00);
+                expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+                expect(invoicedItem.VATPercentage).toBe(21);
+                expect(invoicedItem.VATIncluded).toBe(true);
+                expect(invoicedItem.VATExcempt).toBeNull();
+                expect(invoicedItem.totalWithoutVAT).toBe(12_39_00);
+            }
+
+            // Create an invoiced balance item for half an item
+            {
+                const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 2_50_00);
+
+                expect(invoicedItem.balanceInvoicedAmount).toBe(2_50_00);
+                expect(invoicedItem.quantity).toBe(50_00); // 50% of an item
+                expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+                expect(invoicedItem.VATPercentage).toBe(21);
+                expect(invoicedItem.VATIncluded).toBe(true);
+                expect(invoicedItem.VATExcempt).toBeNull();
+                expect(invoicedItem.totalWithoutVAT).toBe(2_07_00);
+            }
+
+            // Create an invoiced balance item for 1/3 of an item
+            {
+                const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 1_66_66);
+
+                expect(invoicedItem.balanceInvoicedAmount).toBe(1_66_66);
+                expect(invoicedItem.quantity).toBe(33_33); // 33.33% of an item
+                expect(invoicedItem.unitPrice).toBe(4_13_00); // should be rounded to 1 cent
+                expect(invoicedItem.VATPercentage).toBe(21);
+                expect(invoicedItem.VATIncluded).toBe(true);
+                expect(invoicedItem.VATExcempt).toBeNull();
+                expect(invoicedItem.totalWithoutVAT).toBe(1_38_00);
+            }
+        });
+
+        test('Create an invoice item for a balance item excluding VAT with quantity of 1', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 20_00; // 0,20 euro, excluding VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 21;
+            balanceItem.VATIncluded = false;
+            balanceItem.VATExcempt = null;
+
+            expect(balanceItem.priceWithVAT).toBe(24_20); // 0,242 euro
+            expect(balanceItem.priceWithoutVAT).toBe(20_00);
+
+            // Create an invoiced balance item for the full quantity
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 24_20);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(24_20);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(20_00);
+            expect(invoicedItem.VATPercentage).toBe(21);
+            expect(invoicedItem.VATIncluded).toBe(false);
+            expect(invoicedItem.VATExcempt).toBeNull();
+            expect(invoicedItem.totalWithoutVAT).toBe(20_00);
+        });
+
+        test('Create an invoice item for a balance item excluding 0% VAT with quantity of 1', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 20_00; // 0,20 euro, excluding VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 0;
+            balanceItem.VATIncluded = false;
+            balanceItem.VATExcempt = null;
+
+            expect(balanceItem.priceWithVAT).toBe(20_00);
+            expect(balanceItem.priceWithoutVAT).toBe(20_00);
+
+            // Create an invoiced balance item for the full quantity
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 20_00);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(20_00);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(20_00);
+            expect(invoicedItem.VATPercentage).toBe(0);
+            expect(invoicedItem.VATIncluded).toBe(false);
+            expect(invoicedItem.VATExcempt).toBeNull();
+            expect(invoicedItem.totalWithoutVAT).toBe(20_00);
+        });
+
+        test('Create an invoice item for a balance item including 0% VAT with quantity of 1', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 20_00; // 0,20 euro, excluding VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 0;
+            balanceItem.VATIncluded = true;
+            balanceItem.VATExcempt = null;
+
+            expect(balanceItem.priceWithVAT).toBe(20_00);
+            expect(balanceItem.priceWithoutVAT).toBe(20_00);
+
+            // Create an invoiced balance item for the full quantity
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 20_00);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(20_00);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(20_00);
+            expect(invoicedItem.VATPercentage).toBe(0);
+            expect(invoicedItem.VATIncluded).toBe(true);
+            expect(invoicedItem.VATExcempt).toBeNull();
+            expect(invoicedItem.totalWithoutVAT).toBe(20_00);
+        });
+
+        test('Create an invoice item for a balance item excluding VAT with quantity of 1 that is excempt VAT', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 20_00; // 0,20 euro, excluding VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 21;
+            balanceItem.VATIncluded = false;
+            balanceItem.VATExcempt = VATExcemptReason.IntraCommunity;
+
+            expect(balanceItem.priceWithVAT).toBe(20_00);
+            expect(balanceItem.priceWithoutVAT).toBe(20_00);
+
+            // Create an invoiced balance item for the full quantity
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 20_00);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(20_00);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(20_00);
+            expect(invoicedItem.VATPercentage).toBe(21);
+            expect(invoicedItem.VATIncluded).toBe(false);
+            expect(invoicedItem.VATExcempt).toBe(VATExcemptReason.IntraCommunity);
+            expect(invoicedItem.totalWithoutVAT).toBe(20_00);
+        });
+
+        test('Create an invoice item for a balance item including VAT with quantity of 1 that is excempt VAT', () => {
+            const balanceItem = new BalanceItem();
+            balanceItem.description = 'Test Item';
+            balanceItem.unitPrice = 24_20; // 0,242 euro, including VAT
+            balanceItem.quantity = 1;
+            balanceItem.VATPercentage = 21;
+            balanceItem.VATIncluded = true;
+            balanceItem.VATExcempt = VATExcemptReason.IntraCommunity;
+
+            expect(balanceItem.priceWithVAT).toBe(20_00);
+            expect(balanceItem.priceWithoutVAT).toBe(20_00);
+
+            // Create an invoiced balance item for the full quantity (0,20 is payable or invoiceable, the system should treat that as one item)
+            const invoicedItem = InvoicedBalanceItem.createFor(balanceItem, 20_00);
+
+            expect(invoicedItem.balanceInvoicedAmount).toBe(20_00);
+            expect(invoicedItem.quantity).toBe(1_00_00);
+            expect(invoicedItem.unitPrice).toBe(20_00);
+            expect(invoicedItem.VATPercentage).toBe(21);
+            expect(invoicedItem.VATIncluded).toBe(true);
+            expect(invoicedItem.VATExcempt).toBe(VATExcemptReason.IntraCommunity);
+            expect(invoicedItem.totalWithoutVAT).toBe(20_00);
+        });
+    });
+});
--- shared/structures/src/billing/InvoicedBalanceItem.ts
+++ shared/structures/src/billing/InvoicedBalanceItem.ts
@@ -1,6 +1,7 @@
 import { AutoEncoder, BooleanDecoder, EnumDecoder, field, IntegerDecoder, StringDecoder } from '@simonbackx/simple-encoding';
 import { v4 as uuidv4 } from 'uuid';
-import { VATExcemptReason } from '../BalanceItem.js';
+import { BalanceItem, VATExcemptReason } from '../BalanceItem.js';
+import { SimpleError } from '@simonbackx/simple-errors';
 
 export class InvoicedBalanceItem extends AutoEncoder {
     @field({ decoder: StringDecoder, defaultValue: () => uuidv4() })
@@ -12,9 +13,33 @@ export class InvoicedBalanceItem extends AutoEncoder {
     @field({ decoder: StringDecoder })
     description = '';
 
+    /**
+     * Price of the balance that was actually invoiced. Always includes VAT.
+     *
+     * This only explains the due balance that was invoiced so we don't invoice items multiple times. It does not correspond to the visible unit price or price on the invoice.
+     *
+     * - E.g. if balance item price is 5 euro including VAT (=balanceInvoicedAmount), then unit price would be 4,13 euro. You'll see this causes a rounding issue if the amount is 2, because then the price including VAT of the invoice would be 9,99 instead of 10.
+     * - E.g. if balance item price is 0,242 euro including VAT (=balanceInvoicedAmount), then unit price would be 0,20 euro (no rounding issue)
+     * - E.g. if balance item price is 0,243 euro including VAT (=balanceInvoicedAmount), then unit price would also be 0,20 euro (rounding issue)
+     *
+     * Any rounding issues are fixed on the invoice level.
+     */
     @field({ decoder: IntegerDecoder })
-    amount = 1;
+    balanceInvoicedAmount = 1;
 
+    /**
+     * quantity per ten thousand.
+     * We need to support 4 decimals in case of partial payments.
+     * E.g. 3.33 euro of 10 euro was paid and is invoiced, then the amount would be 0.3333, stored as 3333
+     * Mostly for legal reasons, to be able to say wat was actually bought without faking an amount
+     */
+    @field({ decoder: IntegerDecoder })
+    quantity = 1_00_00;
+
+    /**
+     * Price per piece
+     * Always excludes VAT
+     */
     @field({ decoder: IntegerDecoder })
     unitPrice = 0;
 
@@ -26,11 +51,10 @@ export class InvoicedBalanceItem extends AutoEncoder {
     VATPercentage: number = 0;
 
     /**
-     * This is duplicated to allow altering the vat percentage of the corresponding balance item later, so
-     * mistakes can be corrected and credited.
+     * Note: does not apply to unitPrice, only to the balance item at the time of invoicing
      */
     @field({ decoder: BooleanDecoder })
-    VATIncluded = true;
+    VATIncluded = false;
 
     /**
      * This is duplicated to allow altering the vat percentage of the corresponding balance item later, so
@@ -40,8 +64,55 @@ export class InvoicedBalanceItem extends AutoEncoder {
     VATExcempt: VATExcemptReason | null = null;
 
     /**
-     * Cached value so we can correct how much has been paid of an invoice exactly.
+     * This should be rounded to 2 decimals to match PEPPOL requirements
+     * = LineExtensionAmount
      */
-    @field({ decoder: IntegerDecoder })
-    pricePaid = 0;
+    get totalWithoutVAT() {
+        return Math.round(this.unitPrice * this.quantity / 1_00_00_00) * 100;
+    }
+
+    /**
+     * Creates a default configuration for a balance item that needs to be invoiced. The result can still be adjusted to correct rounding issues, because
+     * a balanace item cannot always be perfectly represented on an invoice (on an invoice rounding happens on invoice level, while balance items can have any price that is not really rounded at any time unless when paid).
+     *
+     * amount = paid (or to be paid) price, including VAT if there is VAT
+     */
+    static createFor(balanceItem: BalanceItem, amount: number) {
+        if (balanceItem.VATPercentage === null) {
+            throw new SimpleError({
+                message: 'Cannot create InvoicedBalanceItem for balance item without VAT percentage',
+                human: $t('Het is niet mogelijk om een factuur aan te maken omdat een BTW-tarief ontbreekt'),
+                code: 'balance_item_without_vat_percentage',
+            });
+        }
+        const item = new InvoicedBalanceItem();
+        item.name = balanceItem.itemTitle;
+        item.description = balanceItem.itemDescription ?? '';
+        item.balanceInvoicedAmount = amount;
+        item.VATPercentage = balanceItem.VATPercentage;
+        item.VATExcempt = balanceItem.VATExcempt;
+        item.VATIncluded = balanceItem.VATIncluded;
+
+        // Calculate amount and unit price
+        const balanceItemUnitPriceWithVAT = Math.round(balanceItem.priceWithVAT / balanceItem.quantity);
+        const quantity = Math.round(item.balanceInvoicedAmount * 1_00_00 / balanceItemUnitPriceWithVAT); // Amount per ten thousand
+        if (quantity <= 0) {
+            throw new SimpleError({
+                message: 'Invoiced amount is too low to create an invoiced balance item',
+                human: $t('Het is niet mogelijk om een factuur aan te maken omdat het gefactureerde bedrag te laag is'),
+                code: 'invoiced_amount_too_low',
+            });
+        }
+        item.quantity = quantity;
+        item.unitPrice = amount * 1_00_00 / quantity;
+
+        // Remove VAT from unit price if needed
+        if (!balanceItem.VATExcempt) {
+            item.unitPrice = item.unitPrice * 100 / (100 + balanceItem.VATPercentage);
+        }
+        // Round unitPrice to 1 cent (= PEPPOL requirement)
+        item.unitPrice = Math.round(item.unitPrice / 100) * 100;
+
+        return item;
+    }
 }
