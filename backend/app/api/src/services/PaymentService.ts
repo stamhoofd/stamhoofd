5a3ab4312b15e49fa53f98814cf17d1487960264
Simon Backx <simon@stamhoofd.be>
2026-01-16 16:20:21 +0100

    Updated PaymentService wip round method

--- backend/app/api/src/services/PaymentService.ts
+++ backend/app/api/src/services/PaymentService.ts
@@ -2,11 +2,11 @@ import createMollieClient, { PaymentStatus as MolliePaymentStatus } from '@molli
 import { BalanceItem, BalanceItemPayment, MolliePayment, MollieToken, Organization, PayconiqPayment, Payment } from '@stamhoofd/models';
 import { QueueHandler } from '@stamhoofd/queues';
 import { AuditLogSource, BalanceItemRelation, BalanceItemStatus, BalanceItemType, PaymentMethod, PaymentProvider, PaymentStatus } from '@stamhoofd/structures';
-import { BuckarooHelper } from '../helpers/BuckarooHelper';
-import { StripeHelper } from '../helpers/StripeHelper';
-import { AuditLogService } from './AuditLogService';
-import { BalanceItemPaymentService } from './BalanceItemPaymentService';
-import { BalanceItemService } from './BalanceItemService';
+import { BuckarooHelper } from '../helpers/BuckarooHelper.js';
+import { StripeHelper } from '../helpers/StripeHelper.js';
+import { AuditLogService } from './AuditLogService.js';
+import { BalanceItemPaymentService } from './BalanceItemPaymentService.js';
+import { BalanceItemService } from './BalanceItemService.js';
 
 export const PaymentService = {
     async handlePaymentStatusUpdate(payment: Payment, organization: Organization, status: PaymentStatus) {
@@ -336,46 +336,28 @@ export const PaymentService = {
      * Say the total amount to pay is 15,238 because (e.g. because of VAT). In that case,
      * we'll need to round the payment to 1 cent. That can cause issues in the financial statements because
      * the total amount of balances does not match the total amount received/paid.
-     * 
+     *
      * To fix that, we create an extra balance item with the difference. So the rounding always matches.
+     *
+     * TODO: update this method to generate a virtual invoice and use the price of the invoice instead of the rounded payment price, so we don't get differences in calculation
      */
     async round(payment: Payment) {
-        if (!payment.organizationId) {
-            throw new Error('Cannot round payments without organizationId')
-        }
-        
         const amount = payment.price;
-        const rounded = Payment.roundPrice(payment.price)
+        const rounded = Payment.roundPrice(payment.price);
         const difference = rounded - amount;
 
         if (difference === 0) {
             return;
         }
 
-        if (difference > 100) {
-            throw new Error('Unexpected rounding difference.')
+        if (difference > 100 || difference < -100) {
+            throw new Error('Unexpected rounding difference of ' + difference + ' for payment ' + payment.id);
         }
 
-        const balanceItem = new BalanceItem();
-        balanceItem.type = BalanceItemType.Rounding;
-        balanceItem.userId = payment.payingUserId;
-        balanceItem.payingOrganizationId = payment.payingOrganizationId
-        balanceItem.unitPrice = difference;
-        balanceItem.pricePaid = 0;
-        balanceItem.organizationId = payment.organizationId;
-        balanceItem.status = BalanceItemStatus.Hidden;
-        await balanceItem.save();
-
-        // Create balance item payment
-        const balanceItemPayment = new BalanceItemPayment();
-        balanceItemPayment.organizationId = payment.organizationId;
-        balanceItemPayment.balanceItemId = balanceItem.id;
-        balanceItemPayment.price = difference;
-        balanceItemPayment.paymentId = payment.id;
-        await balanceItemPayment.save();
+        // payment.roundingAmount = difference;
 
         // Change payment total price
-        payment.price += difference
+        payment.price += difference;
         await payment.save();
-    }
+    },
 };
