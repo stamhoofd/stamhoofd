be6b433a5439199b5be03bcb367b446daaec0e6e
Bjarne <bjarne.backx@gmail.com>
2026-01-24 16:35:29 +0100

    Reset playwright config

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 0 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : 5,
+    workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -25,7 +25,7 @@ export default defineConfig({
         trace: "on-first-retry",
 
         // todo
-        headless: process.env.CI ? true : true,
+        headless: process.env.CI ? true : false,
         // ignoreHTTPSErrors: true,
         launchOptions: {
             args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-gpu"],
