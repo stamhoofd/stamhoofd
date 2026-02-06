627d767f9b419614be55131413b176096ad4b4a8
Simon Backx <simon@stamhoofd.be>
2026-02-06 16:54:18 +0100

    Linted CenteredMEssage

--- frontend/shared/components/src/overlays/CenteredMessage.ts
+++ frontend/shared/components/src/overlays/CenteredMessage.ts
@@ -29,70 +29,71 @@ export class CenteredMessageButton {
 }
 
 export class CenteredMessage {
-    protected static listeners: Map<any, CenteredMessageListener> = new Map()
+    protected static listeners: Map<any, CenteredMessageListener> = new Map();
 
-    type = "none"
-    title = ""
-    description = ""
+    type = 'none';
+    title = '';
+    description = '';
 
+    buttons: CenteredMessageButton[] = [];
 
-    buttons: CenteredMessageButton[] = []
+    doHide: (() => void) | null = null;
 
-    doHide: (() => void) | null = null
-
-    constructor(title: string, description = "", type = "none") {
-        this.title = title
-        this.description = description
-        this.type = type
+    constructor(title: string, description = '', type = 'none') {
+        this.title = title;
+        this.description = description;
+        this.type = type;
     }
 
     static fromError(errors: unknown) {
-        let simpleErrors: SimpleErrors
+        let simpleErrors: SimpleErrors;
         if (isSimpleError(errors)) {
-            simpleErrors = new SimpleErrors(errors)
-        } else if (isSimpleErrors(errors)) {
-            simpleErrors = errors
-        } else {
+            simpleErrors = new SimpleErrors(errors);
+        }
+        else if (isSimpleErrors(errors)) {
+            simpleErrors = errors;
+        }
+        else {
             simpleErrors = new SimpleErrors(new SimpleError({
-                code: "unknown_error",
-                message: (errors as any).message
-            }))
+                code: 'unknown_error',
+                message: (errors as any).message,
+            }));
         }
 
-        if (simpleErrors.hasCode("network_error") || simpleErrors.hasCode("network_timeout")) {
-            return new CenteredMessage($t(`18e54ffc-b12d-4175-9128-46446334942f`), $t(`ea7a24eb-79de-40ca-8ebf-1ca4a600587e`), "error")
+        if (simpleErrors.hasCode('network_error') || simpleErrors.hasCode('network_timeout')) {
+            return new CenteredMessage($t(`18e54ffc-b12d-4175-9128-46446334942f`), $t(`ea7a24eb-79de-40ca-8ebf-1ca4a600587e`), 'error');
         }
 
-        return new CenteredMessage(simpleErrors.getHuman(), "", "error")
+        return new CenteredMessage(simpleErrors.getHuman(), '', 'error');
     }
 
     static addListener(owner: any, listener: CenteredMessageListener) {
-        this.listeners.set(owner, listener)
+        this.listeners.set(owner, listener);
     }
 
     static removeListener(owner: any) {
-        this.listeners.delete(owner)
+        this.listeners.delete(owner);
     }
 
     static callListeners(toast: CenteredMessage) {
         for (const listener of this.listeners.values()) {
-            listener(toast)
+            listener(toast);
         }
     }
 
-    addCloseButton(text = $t(`bef7a2f9-129a-4e1c-b8d2-9003ff0a1f8b`), action?: (() => Promise<any>) | undefined) {
-        this.buttons.push(new CenteredMessageButton(text, { type: "secundary", action }))
-        return this
+    addCloseButton(text = $t(`bef7a2f9-129a-4e1c-b8d2-9003ff0a1f8b`), action?: (() => Promise<any>)) {
+        this.buttons.push(new CenteredMessageButton(text, { type: 'secundary', action }));
+        return this;
     }
 
     addButton(button: CenteredMessageButton) {
-        this.buttons.push(button)
-        return this
+        this.buttons.push(button);
+        return this;
     }
 
     show() {
-        CenteredMessage.callListeners(this)
-        return this
+        CenteredMessage.callListeners(this);
+        return this;
     }
 
     hide() {
