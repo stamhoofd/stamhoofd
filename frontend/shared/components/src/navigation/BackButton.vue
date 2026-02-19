71bc22a53fe6693de9976e8535f8cebca2e66f14
Simon Backx <simon@stamhoofd.be>
2026-02-19 19:04:16 +0100

    Added icon based back button in iOS

--- frontend/shared/components/src/navigation/BackButton.vue
+++ frontend/shared/components/src/navigation/BackButton.vue
@@ -1,7 +1,9 @@
 <template>
-    <button class="button navigation" type="button">
+    <button v-if="$isIOS || $isAndroid" class="button icon navigation arrow-back" type="button" />
+
+    <button v-else class="button navigation" type="button">
         <span class="icon arrow-back" />
-        <span class="hide-android">
+        <span>
             <slot>{{ $t('1b311bbf-8cc3-4b91-a019-f80a062d8d4d') }}</slot>
         </span>
     </button>
