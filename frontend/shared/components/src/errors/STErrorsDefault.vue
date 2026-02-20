0308f2ae0ff89134aa720ec937659b2e62817def
Simon Backx <simon@stamhoofd.be>
2026-02-20 13:44:24 +0100

    Refactored STErrorsDefault to composition api

--- frontend/shared/components/src/errors/STErrorsDefault.vue
+++ frontend/shared/components/src/errors/STErrorsDefault.vue
@@ -1,61 +1,47 @@
 <template>
-    <div ref="errors">
+    <div ref="errorElement">
         <template v-for="(error, index) in errors" :key="index">
             <STErrorBox :error="error" />
         </template>
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { SimpleError } from '@simonbackx/simple-errors';
-import { Component, Prop, VueComponent, Watch } from '@simonbackx/vue-app-navigation/classes';
+import { nextTick, ref, useTemplateRef, watch } from 'vue';
 
 import { ErrorBox } from './ErrorBox';
 import STErrorBox from './STErrorBox.vue';
-import { Ref, unref } from 'vue';
-import { Request } from '@simonbackx/simple-networking';
 
-@Component({
-    components: {
-        STErrorBox,
-    },
-})
-export default class STErrorsDefault extends VueComponent {
-    @Prop()
-    errorBox!: ErrorBox | Ref<ErrorBox> | null;
+const props = withDefaults(
+    defineProps<{
+        errorBox?: ErrorBox | null;
+    }>(),
+    {
+        errorBox: null,
+    });
 
-    errors: SimpleError[] = [];
+const errors = ref<SimpleError[]>([]);
+const errorsElement = useTemplateRef('errorElement');
 
-    mounted() {
-        this.onNewErrors(unref(this.errorBox));
+watch(() => props.errorBox, (val: ErrorBox | ErrorBox[] | null) => {
+    if (!val) {
+        errors.value = [];
+        return;
     }
-
-    getErrorMessage(error: SimpleError) {
-        if (Request.isNetworkError(error as any)) {
-            return $t(`94bdc2a4-9ebb-42d2-a4e9-d674eb9aafef`);
-        }
-        return error.getHuman();
-    }
-
-    @Watch('errorBox')
-    onNewErrors(val: ErrorBox | Ref<ErrorBox> | null) {
-        if (!val) {
-            this.errors = [];
+    // Wait for next tick, to prevent a useless rerender of errors that will get removed by other inputs
+    nextTick(() => {
+        const el = errorsElement.value as HTMLElement;
+        if (!el) {
             return;
         }
-        // Wait for next tick, to prevent a useless rerender of errors that will get removed by other inputs
-        this.$nextTick(() => {
-            const errors = unref(val).remaining;
-            this.errors = errors.errors;
 
-            ErrorBox.scrollTo(this.errors, this.$refs.errors as HTMLElement);
-        });
-    }
-}
+        const remaining = props.errorBox?.remaining;
+        if (!remaining) {
+            return;
+        }
+        errors.value = remaining.errors;
+        ErrorBox.scrollTo(errors.value, el);
+    }).catch(console.error);
+}, { immediate: true });
 </script>
-
-<style lang="scss">
-    .error {
-        color: red;
-    }
-</style>
