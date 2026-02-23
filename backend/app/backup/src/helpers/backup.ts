22bb42b08d2f74d56dab643d1b1c2ec18e769109
Simon Backx <simon@stamhoofd.be>
2026-02-23 08:20:10 +0100

    Increased size

--- backend/app/backup/src/helpers/backup.ts
+++ backend/app/backup/src/helpers/backup.ts
@@ -18,7 +18,7 @@ const execPromise = util.promisify(exec);
 // a full history of 40 days - and leaves us enough margin in case more
 // logs are created in a day
 const MAX_BINARY_LOGS = 200;
-const MAX_BACKUPS = 30; // in days
+const MAX_BACKUPS = 60; // in days
 const BACKUP_PREFIX = 'backup-';
 const BINARY_LOG_PREFIX = 'binlog.';
 
