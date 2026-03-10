f9b6bdd708a41b07eace1cbe334638150295e1e4
Simon Backx <simon@stamhoofd.be>
2026-03-10 07:16:14 +0100

    Made legacyWebshop optional

--- backend/app/api/src/endpoints/global/webshops/GetWebshopFromDomainEndpoint.ts
+++ backend/app/api/src/endpoints/global/webshops/GetWebshopFromDomainEndpoint.ts
@@ -86,7 +86,7 @@ export class GetWebshopFromDomainEndpoint extends Endpoint<Params, Query, Body, 
         }
         
         // If ends with the legacy webshop domain
-        if (request.query.domain.endsWith("." + STAMHOOFD.domains.legacyWebshop)) {
+        if (STAMHOOFD.domains.legacyWebshop && request.query.domain.endsWith("." + STAMHOOFD.domains.legacyWebshop)) {
             const strippped = request.query.domain.substr(0, request.query.domain.length - ("." + STAMHOOFD.domains.legacyWebshop).length )
             if (strippped.includes(".")) {
                 throw new SimpleError({
--- environment.d.ts
+++ environment.d.ts
@@ -32,7 +32,7 @@ declare global {
         marketing: LocalizedDomain,         // main landing page
         webshopPrefix: string,              // Webshop prefix that is used on the marketing domain, e.g. 'shop', for stamhoofd.be/shop/my-webshop
         webshop: LocalizedDomain,           // E.g. shop.stamhoofd.be
-        legacyWebshop: string,              // E.g. stamhoofd.shop for *.stamhoofd.shop
+        legacyWebshop?: string,              // In the past, webshops were hosted on a subdomain. This is deprecated, but the links should still work. E.g. stamhoofd.shop for *.stamhoofd.shop
         api: string,                        // requires wildcard prefix DNS
         demoApi: string,                    // requires wildcard prefix DNS
         admin: string,
