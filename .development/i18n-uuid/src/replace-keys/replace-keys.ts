65cc9920174bda315bc455a0e5b4ddddec500124
Simon Backx <simon@stamhoofd.be>
2026-03-11 12:14:47 +0100

    Forced compressing when replacing keys

--- .development/i18n-uuid/src/replace-keys/replace-keys.ts
+++ .development/i18n-uuid/src/replace-keys/replace-keys.ts
@@ -1,7 +1,9 @@
 import { addMissingKeys } from "./add-missing-keys";
+import { compressUuids } from "./compress-uuids";
 import { replaceKeysWithUuid } from "./replace-keys-with-uuid";
 
 export function replaceKeys() {
     replaceKeysWithUuid();
     addMissingKeys();
+    compressUuids()
 }
