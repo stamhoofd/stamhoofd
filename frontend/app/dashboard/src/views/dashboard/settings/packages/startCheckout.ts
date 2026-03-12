31dbbd814658012737b42cafa72467ed13afd8cc
Simon Backx <simon@stamhoofd.be>
2026-03-12 11:18:52 +0100

    Added wip checkout steps for packages

---
+++ frontend/app/dashboard/src/views/dashboard/settings/packages/startCheckout.ts
@@ -0,0 +1,17 @@
+import { ViewStepsManager } from '@stamhoofd/components/steps/ViewStepsManager';
+import { SessionContext } from '@stamhoofd/networking';
+import { PackageCheckout } from '@stamhoofd/structures';
+import { DisplayOptions, NavigationActions } from '@stamhoofd/components/types/NavigationActions';
+import { InvoiceStep } from './steps/InvoiceStep';
+
+export async function startCheckout({ checkout, context, displayOptions }: { checkout: PackageCheckout; context: SessionContext; displayOptions: DisplayOptions }, navigate: NavigationActions) {
+    const steps = [
+        new InvoiceStep(checkout),
+    ];
+
+    const stepManager = new ViewStepsManager(steps, async (navigate: NavigationActions) => {
+        // todo
+    }, displayOptions);
+
+    await stepManager.saveHandler(null, navigate);
+}
---
+++ frontend/app/dashboard/src/views/dashboard/settings/packages/steps/InvoiceStep.ts
@@ -0,0 +1,27 @@
+import { ComponentWithProperties } from '@simonbackx/vue-app-navigation';
+import { type ViewStep } from '@stamhoofd/components/steps/ViewStep';
+import { type ViewStepsManager } from '@stamhoofd/components/steps/ViewStepsManager';
+import { NavigationActions } from '@stamhoofd/components/types/NavigationActions';
+import { PackageCheckout } from '@stamhoofd/structures';
+import PaymentCustomerView from '@stamhoofd/components/members/checkout/PaymentCustomerView.vue';
+
+export class InvoiceStep implements ViewStep {
+    checkout: PackageCheckout;
+
+    constructor(checkout: PackageCheckout) {
+        this.checkout = checkout;
+    }
+
+    isEnabled(_manager: ViewStepsManager) {
+        return true;
+    }
+
+    getComponent(manager: ViewStepsManager): ComponentWithProperties {
+        return new ComponentWithProperties(PaymentCustomerView, {
+            checkout: this.checkout,
+            saveHandler: async (navigate: NavigationActions) => {
+                await manager.saveHandler(this, navigate);
+            },
+        });
+    }
+}
