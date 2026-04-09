cec293d629f8efaec8b35d5fcc43afadb9516621
Bjarne <bjarne.backx@gmail.com>
2026-04-09 11:30:50 +0200

    Added check if table exists

--- backend/shared/models/src/migrations/1720080976-convert-charset-leads.ts
+++ backend/shared/models/src/migrations/1720080976-convert-charset-leads.ts
@@ -14,15 +14,41 @@ export default new Migration(async () => {
         return Promise.resolve();
     }
 
-    const sqlStatement: string = [
-        'set foreign_key_checks=0;',
-        'ALTER TABLE `leads` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;',
-        'ALTER TABLE `leads` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;',
-        'set foreign_key_checks=1;'
-    ].join('');
+    const hasTable = await tableExists('leads');
+    console.log('Has leads table: ' + hasTable);
 
-    console.log('Start updating charset of leads table.');
-    await Database.statement(sqlStatement);
+    if (hasTable) {
+        const sqlStatement: string = [
+            'set foreign_key_checks=0;',
+            'ALTER TABLE `leads` CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;',
+            'ALTER TABLE `leads` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;',
+            'set foreign_key_checks=1;'
+        ].join('');
+
+        console.log('Start updating charset of leads table.');
+        await Database.statement(sqlStatement);
+    }
 
     return Promise.resolve();
 });
+
+
+async function tableExists(tableName: string): Promise<boolean> {
+    const [rows] = await Database.select(
+        `
+        SELECT COUNT(*) as count
+        FROM information_schema.tables
+        WHERE table_schema = DATABASE()
+        AND table_name = ?
+        `,
+        [tableName]
+    );
+
+    const count = rows[0]['']['count'];
+
+    if (typeof count !== 'number') {
+        throw new Error('count is not a number');
+    }
+
+    return count > 0;
+}
