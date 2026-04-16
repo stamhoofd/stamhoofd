a3fb8a9adb8b635fb3a07f014c31832c41548efa
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-04-16 18:49:25 +0200

    🐛 Fixed cron update cached balances (#370)
    fixes STA-1043

--- backend/app/api/src/crons/update-cached-balances.ts
+++ backend/app/api/src/crons/update-cached-balances.ts
@@ -1,5 +1,6 @@
-import { CachedBalance } from '@stamhoofd/models';
 import { registerCron } from '@stamhoofd/crons';
+import { CachedBalance } from '@stamhoofd/models';
+import { ReceivableBalanceType } from '@stamhoofd/structures';
 
 registerCron('updateCachedBalances', updateCachedBalances);
 
@@ -11,28 +12,53 @@ async function updateCachedBalances() {
 
     const balances = await CachedBalance.select().where(
         CachedBalance.whereNeedsUpdate(),
-    ).limit(100).fetch();
+    )
+    /**
+     * Cached balances of member should be updated before the cached balances of the user
+     * because the cached balances of the user are dependent on the cached balances of the member.
+     */
+    .orderBy('objectType', 'ASC')
+    // minimize the number of queries when updating
+    .orderBy('organizationId', 'ASC')
+    .limit(100).fetch();
 
-    // Group by object type and by organization id
-    const grouped = new Map<string, CachedBalance[]>();
+    // Group by object type, next by organization id
+    const grouped = new Map<ReceivableBalanceType, Map<string, CachedBalance[]>>();
 
     for (const balance of balances) {
-        const key = balance.objectType + '-' + balance.organizationId;
-        const arr = grouped.get(key);
+        const objectType = balance.objectType;
+        let organizationMap = grouped.get(objectType);
+        if (!organizationMap) {
+            organizationMap = new Map<string, CachedBalance[]>();
+            grouped.set(objectType, organizationMap);
+        }
 
-        if (!arr) {
-            grouped.set(key, [balance]);
+        const organizationId = balance.organizationId;
+        const balances = organizationMap.get(organizationId);
+
+        if (!balances) {
+            organizationMap.set(organizationId, [balance]);
             continue;
         }
 
-        arr.push(balance);
+        balances.push(balance);
     }
 
-    for (const [_, balances] of grouped) {
-        const balance = balances[0];
+    // update member balances first
+    const objectTypeMemberFirst = Array.from(grouped.entries());
+    objectTypeMemberFirst.sort((a, b) => {
+        const aIsMember = a[0] === ReceivableBalanceType.member;
+        const bIsMember = b[0] === ReceivableBalanceType.member;
+        if (aIsMember && !bIsMember) return -1;
+        if (!aIsMember && bIsMember) return 1;
+        return 0; 
+    });
 
-        const ids = balances.map(b => b.objectId);
-        console.log('Updating', ids.length, balance.objectType, 'for', balance.organizationId);
-        await CachedBalance.updateForObjects(balance.organizationId, ids, balance.objectType);
+    for (const [objectType, organizationMap] of objectTypeMemberFirst) {
+        for (const [organizationId, balances] of organizationMap) {
+            const ids = balances.map(b => b.objectId);
+            console.log('Updating', ids.length, objectType, 'for', organizationId);
+            await CachedBalance.updateForObjects(organizationId, ids, objectType);
+        }
     }
 }
