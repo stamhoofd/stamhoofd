4f21b6a76a56c67b7f626e5af1cf9b627b446c42
Simon Backx <simon@stamhoofd.be>
2026-06-05 01:46:57 +0200

    Increased playwright performance

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     retries: process.env.CI ? 1 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'line',
 
