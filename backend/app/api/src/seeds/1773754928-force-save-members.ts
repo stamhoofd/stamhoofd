05481e46031fb10c64173c01700558cad2488535
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-06-05 17:24:33 +0200

    Improved force-save-members speed

--- backend/app/api/src/seeds/1773754928-force-save-members.ts
+++ backend/app/api/src/seeds/1773754928-force-save-members.ts
@@ -1,15 +1,49 @@
 import { Migration } from '@simonbackx/simple-database';
 import { Member, Registration } from '@stamhoofd/models';
+import { LoggingTools } from '../helpers/LoggingTools.js';
+import { SeedTools } from '../helpers/SeedTools.js';
 
 export default new Migration(async () => {
     process.stdout.write('\n');
+
+    await migrateMembers();
+    await migrateRegistrations();
+});
+
+async function migrateMembers() {
+    console.log('Start updating members.');
+
+    const batchProcessor = SeedTools.createBatchProcessor({
+        batchSize: 100,
+        action: async (member: Member) => {
+            member.forceSaveProperty('details');
+            await member.save();
+        },
+    });
+
+    const memberProgressLogger = await LoggingTools.createProgressLoggerFromQuery(Member.select());
+    batchProcessor.setProgressLogger(memberProgressLogger);
+
     for await (const member of Member.select().all()) {
-        member.forceSaveProperty('details');
-        await member.save();
+        await batchProcessor.execute(member);
     }
+}
+
+async function migrateRegistrations() {
+    console.log('Start updating registrations.');
+
+    const batchProcessor = SeedTools.createBatchProcessor({
+        batchSize: 100,
+        action: async (registration: Registration) => {
+            registration.forceSaveProperty('groupPrice');
+            await registration.save();
+        },
+    });
+
+    const registrationProgressLogger = await LoggingTools.createProgressLoggerFromQuery(Registration.select());
+    batchProcessor.setProgressLogger(registrationProgressLogger);
 
     for await (const registration of Registration.select().all()) {
-        registration.forceSaveProperty('groupPrice');
-        await registration.save();
+        await batchProcessor.execute(registration);
     }
-});
+}
