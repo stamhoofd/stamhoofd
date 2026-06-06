6845e47eb133bdc4b18c2d9ab8df3dde162bc13a
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-06-06 20:42:14 +0200

    Added migrate update orders in batch

--- backend/app/api/src/seeds/1729253172-update-orders.ts
+++ backend/app/api/src/seeds/1729253172-update-orders.ts
@@ -1,6 +1,7 @@
 import { Migration } from '@simonbackx/simple-database';
 import { Order } from '@stamhoofd/models';
 import { LoggingTools } from '@stamhoofd/utility';
+import { SeedTools } from '../helpers/SeedTools.js';
 
 export default new Migration(async () => {
     if (STAMHOOFD.environment === 'test') {
@@ -10,14 +11,21 @@ export default new Migration(async () => {
 
     console.log('Start saving orders.');
 
-    const batchSize = 100;
+    const batchProcessor = SeedTools.createBatchProcessor({
+        batchSize: 100,
+        action: async (order: Order) => {
+            await order.save();
+        },
+    });
 
     const ordersProgressLogger = await LoggingTools.createProgressLoggerFromQuery(Order.select(), { tag: 'Update-orders' });
+    batchProcessor.setProgressLogger(ordersProgressLogger);
 
-    for await (const order of Order.select().limit(batchSize).all()) {
-        await order.save();
-        ordersProgressLogger.update();
+    for await (const order of Order.select().limit(150).all()) {
+        await batchProcessor.execute(order);
     }
+
+    await batchProcessor.finish();
 
     console.log('Finished saving ' + ordersProgressLogger.total + ' orders.');
 });
