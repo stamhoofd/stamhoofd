bdae4eb4ce76b98c9f6876c56b455b1810468c78
Simon Backx <simon@stamhoofd.be>
2026-06-07 12:02:15 +0200

    Added transactions to user permissions seed

--- backend/app/api/src/seeds/1715028563-user-permissions.ts
+++ backend/app/api/src/seeds/1715028563-user-permissions.ts
@@ -1,7 +1,6 @@
 import { Migration } from '@simonbackx/simple-database';
 import { User } from '@stamhoofd/models';
 import { UserPermissions } from '@stamhoofd/structures';
-import { LoggingTools } from '@stamhoofd/utility';
 import { SeedTools } from '../helpers/SeedTools.js';
 
 export default new Migration(async () => {
@@ -11,10 +10,12 @@ export default new Migration(async () => {
     }
     process.stdout.write('\n');
 
-    const getQuery = () => User.select().whereNot('organizationPermissions', null).whereNot('organizationId', null);
+    let realUpdate = 0;
 
-    const batchProcessor = SeedTools.createBatchProcessor({
+    const result = await SeedTools.loop({
         batchSize: 100,
+        query: User.select().whereNot('organizationPermissions', null).whereNot('organizationId', null),
+        useTransactionPerBatch: true,
         action: async (admin: User) => {
             if (!admin.organizationPermissions || !admin.organizationId) {
                 return;
@@ -23,21 +24,16 @@ export default new Migration(async () => {
             p.organizationPermissions.set(admin.organizationId, admin.organizationPermissions);
             admin.permissions = UserPermissions.limitedAdd(admin.permissions, p, admin.organizationId);
             admin.organizationPermissions = null;
-            await admin.save({
+            if (await admin.save({
                 skipMarkSaved: true,
                 skipSendEvents: true,
-            });
+            })) {
+                realUpdate++;
+            }
         },
     });
 
-    const progressLogger = await LoggingTools.createProgressLoggerFromQuery(getQuery());
-    batchProcessor.setProgressLogger(progressLogger);
-
-    for await (const admin of getQuery().limit(100).all()) {
-        await batchProcessor.execute(admin);
-    }
-
-    await batchProcessor.finish();
+    console.log('Updated permissions of ' + realUpdate + ' users, looped ' + result.total + ' users');
 
     // Do something here
     return Promise.resolve();
