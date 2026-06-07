17f8eda9af389b44cdf4cc1e1c88619c64a2430d
Simon Backx <simon@stamhoofd.be>
2026-06-07 14:56:28 +0200

    Removed force save of groupPrice and reduced force save to unupdated members

--- backend/app/api/src/seeds/1773754928-force-save-members.ts
+++ backend/app/api/src/seeds/1773754928-force-save-members.ts
@@ -1,53 +1,32 @@
 import { Migration } from '@simonbackx/simple-database';
-import { Member, Registration } from '@stamhoofd/models';
-import { LoggingTools } from '@stamhoofd/utility';
+import { Member } from '@stamhoofd/models';
 import { SeedTools } from '../helpers/SeedTools.js';
 
 export default new Migration(async () => {
     process.stdout.write('\n');
 
     await migrateMembers();
-    await migrateRegistrations();
 });
 
 async function migrateMembers() {
     console.log('Start updating members.');
 
-    const batchProcessor = SeedTools.createBatchProcessor({
-        batchSize: 100,
+    let realUpdate = 0;
+    const result = await SeedTools.loop({
+        query: Member.select().where('updatedAt', '<', new Date(Date.now() - 1_000 * 60 * 60 * 5)),
+        batchSize: 200,
+        useTransactionPerBatch: true,
         action: async (member: Member) => {
             member.forceSaveProperty('details');
-            await member.save();
+
+            if (await member.save({
+                skipMarkSaved: true,
+                skipSendEvents: true,
+            })) {
+                realUpdate += 1;
+            }
         },
     });
 
-    const memberProgressLogger = await LoggingTools.createProgressLoggerFromQuery(Member.select());
-    batchProcessor.setProgressLogger(memberProgressLogger);
-
-    for await (const member of Member.select().all()) {
-        await batchProcessor.execute(member);
-    }
-
-    await batchProcessor.finish();
-}
-
-async function migrateRegistrations() {
-    console.log('Start updating registrations.');
-
-    const batchProcessor = SeedTools.createBatchProcessor({
-        batchSize: 100,
-        action: async (registration: Registration) => {
-            registration.forceSaveProperty('groupPrice');
-            await registration.save();
-        },
-    });
-
-    const registrationProgressLogger = await LoggingTools.createProgressLoggerFromQuery(Registration.select());
-    batchProcessor.setProgressLogger(registrationProgressLogger);
-
-    for await (const registration of Registration.select().all()) {
-        await batchProcessor.execute(registration);
-    }
-
-    await batchProcessor.finish();
+    console.log('Updated ' + realUpdate + ' members out of ' + result.total + ' looped members');
 }
