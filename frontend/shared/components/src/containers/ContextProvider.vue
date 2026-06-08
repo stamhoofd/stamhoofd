5a1901a1e74db4b7264a3fe4ab77c0d3f56032eb
Simon Backx <simon@stamhoofd.be>
2026-06-08 21:44:00 +0200

    ♻️ Refactored ContextProvider to composition API

--- frontend/shared/components/src/containers/ContextProvider.vue
+++ frontend/shared/components/src/containers/ContextProvider.vue
@@ -1,60 +1,57 @@
 <template>
     <!-- This div is not really needed, but causes bugs if we remove it from the DOM. Probably something Vue.js related (e.g. user keeps logged out, even if loggedIn = true and force reload is used) -->
-    <div class="context-provider-view" :data-organization-scope="context.$context?.organization?.id ?? 'null'">
+    <div class="context-provider-view" :data-organization-scope="organizationId ?? 'null'">
         <ComponentWithPropertiesInstance :key="root.key" :component="root" />
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import type { ComponentWithProperties } from '@simonbackx/vue-app-navigation';
 import { ComponentWithPropertiesInstance } from '@simonbackx/vue-app-navigation';
-import { Component, Prop, VueComponent } from '@simonbackx/vue-app-navigation/classes';
-import { isReactive } from 'vue';
+import { computed, isReactive, provide } from 'vue';
 
-@Component({
-    components: {
-        ComponentWithPropertiesInstance,
-    },
-    provide() {
-        if (isReactive(this.context)) {
-            // If this.context is a proxy, it will auto unwrap all the reference properties it has
-            // which will break reactivity on scalar values (e.g. Computed<null> will be unwrapped to null)
-            throw new Error('ContextProvider.context is reactive, which will break reactivity of the context properties');
-        }
+const props = withDefaults(defineProps<{
+    root: ComponentWithProperties;
+    context?: Record<string, unknown>;
+}>(), {
+    context: () => ({}),
+});
 
-        if (!this.context.stamhoofd_app) {
-            console.error('Missing stamhoofd_app in ContextProvider.context');
-        } else if (!this.context.$context) {
-            console.error('Missing $context in ContextProvider.context');
-        }
-
-        return this.context;
-    },
-})
-export default class ContextProvider extends VueComponent {
-    @Prop()
-    root!: ComponentWithProperties;
-
-    @Prop({ default: () => {
-        return {};
-    } })
-    context!: Record<string, unknown>;
-
-    async shouldNavigateAway() {
-        return await this.root.shouldNavigateAway();
-    }
-
-    returnToHistoryIndex() {
-        if (this.root) {
-            return this.root.returnToHistoryIndex();
-        }
-        return false;
-    }
-
-    beforeMount() {
-        if (this.root) {
-            this.root.assignHistoryIndex();
-        }
-    }
+if (isReactive(props.context)) {
+    // If props.context is a proxy, it will auto unwrap all the reference properties it has
+    // which will break reactivity on scalar values (e.g. Computed<null> will be unwrapped to null)
+    throw new Error('ContextProvider.context is reactive, which will break reactivity of the context properties');
 }
+
+if (!props.context.stamhoofd_app) {
+    console.error('Missing stamhoofd_app in ContextProvider.context');
+} else if (!props.context.$context) {
+    console.error('Missing $context in ContextProvider.context');
+}
+
+const organizationId = computed(() => (props.context.$context as any)?.organization?.id);
+
+for (const key in props.context) {
+    provide(key, props.context[key]);
+}
+
+if (props.root) {
+    props.root.assignHistoryIndex();
+}
+
+async function shouldNavigateAway() {
+    return await props.root.shouldNavigateAway();
+}
+
+function returnToHistoryIndex() {
+    if (props.root) {
+        return props.root.returnToHistoryIndex();
+    }
+    return false;
+}
+
+defineExpose({
+    shouldNavigateAway,
+    returnToHistoryIndex,
+});
 </script>
