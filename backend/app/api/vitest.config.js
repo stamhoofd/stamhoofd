98f84fb7d88c6a679b7b06883b4f7bd76372b869
Simon Backx <simon@stamhoofd.be>
2026-06-10 21:19:54 +0200

    Added vitest coverage script to api

--- backend/app/api/vitest.config.js
+++ backend/app/api/vitest.config.js
@@ -10,5 +10,11 @@ export default defineConfig({
         isolate: true,
         testTimeout: 10_000, // required for slow CI
         maxWorkers: 1, // For now we can't run parallel because all test files use the same database
+        coverage: {
+            provider: 'v8',
+            include: ['src/**'],
+            exclude: ['src/migrations/**'],
+            reporter: ['text', 'html', 'lcov'],
+        },
     },
 });
