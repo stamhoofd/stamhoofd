9ea5298b6e4a64f76ce38b6413c3c21e27a94d46
R De Vroom <288011320+rdevroom@users.noreply.github.com>
2026-06-15 15:01:11 +0100

    fix timezone bugs int tests

--- shared/structures/src/Platform.test.ts
+++ shared/structures/src/Platform.test.ts
@@ -4,32 +4,53 @@ import { PlatformMembershipTypeBehaviour, PlatformMembershipTypeConfig } from '.
 describe('PlatformMembershipTypeConfig', () => {
     describe('getMaximumEndDate', () => {
         test('returns the configured end date for period memberships', () => {
-            const endDate = new Date(2026, 4, 31, 23, 59, 59);
+            const endDate = Formatter.luxon().set({ year: 2026, month: 5, day: 31, hour: 23, minute: 59, second: 59, millisecond: 0 }).toJSDate();
+            const startDate = Formatter.luxon().set({ year: 2026, month: 5, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 }).toJSDate();
             const config = PlatformMembershipTypeConfig.create({
                 endDate,
                 maximumDays: 1,
             });
+            const maximumEndDate = config.getMaximumEndDate(
+                startDate,
+                PlatformMembershipTypeBehaviour.Period,
+            );
+            const maximumEndDateBrussels = Formatter.luxon(maximumEndDate);
 
-            expect(config.getMaximumEndDate(new Date(2026, 4, 1), PlatformMembershipTypeBehaviour.Period)).toBe(endDate);
+            expect(maximumEndDateBrussels.year).toBe(2026);
+            expect(maximumEndDateBrussels.month).toBe(5);
+            expect(maximumEndDateBrussels.day).toBe(31);
+            expect(maximumEndDateBrussels.hour).toBe(23);
+            expect(maximumEndDateBrussels.minute).toBe(59);
+            expect(maximumEndDateBrussels.second).toBe(59);
         });
 
         test('returns the configured end date for days memberships without maximum days', () => {
-            const endDate = new Date(2026, 4, 31, 23, 59, 59);
+            const endDate = Formatter.luxon().set({ year: 2026, month: 5, day: 31, hour: 23, minute: 59, second: 59, millisecond: 0 }).toJSDate();
+            const startDate = Formatter.luxon().set({ year: 2026, month: 5, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 }).toJSDate();
             const config = PlatformMembershipTypeConfig.create({
                 endDate,
                 maximumDays: null,
             });
+            const maximumEndDate = config.getMaximumEndDate(startDate, PlatformMembershipTypeBehaviour.Days);
+            const maximumEndDateBrussels = Formatter.luxon(maximumEndDate);
 
-            expect(config.getMaximumEndDate(new Date(2026, 4, 1), PlatformMembershipTypeBehaviour.Days)).toBe(endDate);
+            expect(maximumEndDateBrussels.year).toBe(2026);
+            expect(maximumEndDateBrussels.month).toBe(5);
+            expect(maximumEndDateBrussels.day).toBe(31);
+            expect(maximumEndDateBrussels.hour).toBe(23);
+            expect(maximumEndDateBrussels.minute).toBe(59);
+            expect(maximumEndDateBrussels.second).toBe(59);
         });
 
         test('calculates inclusive maximum days for days memberships', () => {
+            const endDate = Formatter.luxon().set({ year: 2026, month: 5, day: 31, hour: 23, minute: 59, second: 59, millisecond: 0 }).toJSDate();
+            const startDate = Formatter.luxon().set({ year: 2026, month: 5, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0 }).toJSDate();
             const config = PlatformMembershipTypeConfig.create({
-                endDate: new Date(2026, 4, 31, 23, 59, 59),
+                endDate,
                 maximumDays: 2,
             });
 
-            const maximumEndDate = config.getMaximumEndDate(new Date(2026, 4, 1), PlatformMembershipTypeBehaviour.Days);
+            const maximumEndDate = config.getMaximumEndDate(startDate, PlatformMembershipTypeBehaviour.Days);
             const maximumEndDateBrussels = Formatter.luxon(maximumEndDate);
 
             expect(maximumEndDateBrussels.year).toBe(2026);
