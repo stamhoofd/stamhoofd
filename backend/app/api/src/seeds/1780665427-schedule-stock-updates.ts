1b92add88115a2cead523390fee9f2ee77f9161e
Simon Backx <simon@stamhoofd.be>
2026-06-19 14:42:30 +0200

    Added queue awaiting in schedule stock update migration

--- backend/app/api/src/seeds/1780665427-schedule-stock-updates.ts
+++ backend/app/api/src/seeds/1780665427-schedule-stock-updates.ts
@@ -3,6 +3,7 @@ import { Registration } from '@stamhoofd/models';
 import { SeedTools } from '../helpers/SeedTools.js';
 import { RegistrationService } from '../services/RegistrationService.js';
 import { QueryableModel } from '@stamhoofd/sql';
+import { QueueHandler } from '@stamhoofd/queues';
 
 export default new Migration(async () => {
     if (STAMHOOFD.environment == 'test') {
@@ -15,12 +16,24 @@ export default new Migration(async () => {
         return;
     }
 
-    const result = await SeedTools.loop({
-        query: Registration.select(),
-        batchSize: 10_000,
+    const result = await SeedTools.loopBatched({
+        query: Registration.select('id'),
+        batchSize: 2_000,
         // dot not use transactions here: this breaks the stock behaviour
-        action: async (registration: Registration) => {
-            await RegistrationService.unsafeStockUpdate(registration);
+        batchAction: async (registrations: Registration[]) => {
+            for (const registration of registrations) {
+                await RegistrationService.scheduleStockUpdateAsync(registration.id);
+
+                if (QueryableModel.shutdownMigrations) {
+                    break;
+                }
+            }
+
+            // Immediate tasks scheduled on group updates
+            await QueueHandler.awaitAll();
+
+            // Recursive tasks
+            await QueueHandler.awaitAll();
 
             if (QueryableModel.shutdownMigrations) {
                 throw new Error('Stopping migration gracefully');
