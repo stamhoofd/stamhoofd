b804d70c4f9d1d0098a3e931f78303b33bd0f918
Simon Backx <simon@stamhoofd.be>
2026-06-19 14:41:57 +0200

    ⚡️ Splitted update cached balance migration into multiple migrations that should run faster

---
+++ backend/app/api/src/seeds/1760702452-update-balance-price-paid.ts
@@ -0,0 +1,19 @@
+import { Migration } from '@simonbackx/simple-database';
+import { BalanceItem } from '@stamhoofd/models';
+
+export default new Migration(async () => {
+    if (STAMHOOFD.environment === 'test') {
+        console.log('skipped in tests');
+        return;
+    }
+
+    if (STAMHOOFD.platformName !== 'stamhoofd') {
+        return;
+    }
+
+    if (STAMHOOFD.userMode !== 'organization') {
+        return;
+    }
+
+    await BalanceItem.updatePricePaid('all');
+});
--- backend/app/api/src/seeds/1760702454-update-cached-outstanding-balance-from-items.ts
+++ backend/app/api/src/seeds/1760702454-update-cached-outstanding-balance-from-items.ts
@@ -1,29 +1,66 @@
 import { Migration } from '@simonbackx/simple-database';
-import { logger } from '@simonbackx/simple-logging';
-import { BalanceItem } from '@stamhoofd/models';
-import { LoggingTools } from '@stamhoofd/utility';
-import { BalanceItemService } from '../services/BalanceItemService.js';
+import { CachedBalance, Member, Organization, Platform, Registration, User } from '@stamhoofd/models';
+import { SeedTools } from '../helpers/SeedTools.js';
 
 export default new Migration(async () => {
-    if (STAMHOOFD.environment == 'test') {
+    if (STAMHOOFD.environment === 'test') {
         console.log('skipped in tests');
         return;
     }
 
-    process.stdout.write('\n');
-    const progressLogger = await LoggingTools.createProgressLoggerFromQuery(BalanceItem.select(), { tag: 'Update-outstanding-balance' });
+    if (STAMHOOFD.platformName !== 'stamhoofd') {
+        return;
+    }
 
-    await logger.setContext({ tags: ['silent-seed', 'seed'] }, async () => {
-        for await (const items of BalanceItem.select().limit(1000).allBatched()) {
-            await BalanceItemService.updatePaidAndPending(items);
+    if (STAMHOOFD.userMode !== 'organization') {
+        return;
+    }
 
-            progressLogger.update(items.length);
-        }
+    // Loop all members
+    await SeedTools.loopBatched({
+        query: Organization.select(),
+        // useTransactionPerBatch: true,
+        batchSize: 100,
+        batchAction: async (organizations) => {
+            for (const organization of organizations) {
+                for await (const batch of Member.select('id').where('organizationId', organization.id).limit(1000).allBatched()) {
+                    await CachedBalance.updateForMembers(organization.id, batch.map(m => m.id));
+                }
+
+                for await (const batch of User.select('id').where('organizationId', organization.id).limit(1000).allBatched()) {
+                    await CachedBalance.updateForUsers(organization.id, batch.map(m => m.id));
+                }
+
+                for await (const batch of Registration.select('id').where('organizationId', organization.id).limit(1000).allBatched()) {
+                    await CachedBalance.updateForRegistrations(organization.id, batch.map(m => m.id));
+                }
+            }
+        },
     });
 
-    await BalanceItemService.flushAll();
+    const membershipOrganizationId = (await Platform.getShared()).membershipOrganizationId;
+    if (membershipOrganizationId) {
+        const membershipOrganization = await Organization.getByID(membershipOrganizationId, true);
+        await SeedTools.loopBatched({
+            query: Organization.select('id').where('id', '!=', membershipOrganization.id),
+            // useTransactionPerBatch: true,
+            batchSize: 1000,
+            batchAction: async (organizations) => {
+                await CachedBalance.updateForOrganizations(membershipOrganization.id, organizations.map(m => m.id));
+            },
+        });
+    }
 
-    console.log('Updated outstanding balance for ' + progressLogger.total + ' items');
+    // await logger.setContext({ tags: ['silent-seed', 'seed'] }, async () => {
+    //    for await (const items of BalanceItem.select().limit(10_000).allBatched()) {
+    //        await BalanceItemService.updatePaidAndPending(items);
+    //        await BalanceItemService.flushAll();
+    //
+    //        progressLogger.update(items.length);
+    //    }
+    // });
+
+    // console.log('Updated outstanding balance for ' + progressLogger.total + ' items');
 
     // Do something here
     return Promise.resolve();
