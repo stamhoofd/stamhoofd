530aabf314875fd4fc6c01133bee1b46c82301ae
R De Vroom <288011320+rdevroom@users.noreply.github.com>
2026-06-22 17:07:59 +0100

    StamhoofdDomains: require fields to be set

--- shared/types/src/StamhoofdDomains.ts
+++ shared/types/src/StamhoofdDomains.ts
@@ -1,24 +1,29 @@
 import type { LocalizedDomain } from './Localized.js';
 
+// Use `field: type | undefined` instead of `field?: type` to require setting
+// each field. This ensures fields are explicitly set to undefined, not
+// forgotten to be added.
 export type StamhoofdDomains = {
     dashboard: string; // requires both www + non-www DNS record
-    registration?: LocalizedDomain; // Optional. Set to undefined for platforms. requires wildcard prefix DNS
+    registration: LocalizedDomain | undefined; // Optional. Set to undefined for platforms. requires wildcard prefix DNS
     marketing: LocalizedDomain; // main landing page (used for linking back to website, documentation...)
-    documentation?: LocalizedDomain; // main landing page (used for linking back to website, documentation...)
-    webshop?: LocalizedDomain; // E.g. shop.stamhoofd.be
-    legacyWebshop?: string; // In the past, webshops were hosted on a subdomain. This is deprecated, but the links should still work. E.g. stamhoofd.shop for *.stamhoofd.shop
+    documentation: LocalizedDomain | undefined; // main landing page (used for linking back to website, documentation...)
+    webshop: LocalizedDomain | undefined; // E.g. shop.stamhoofd.be
+    legacyWebshop: string | undefined; // In the past, webshops were hosted on a subdomain. This is deprecated, but the links should still work. E.g. stamhoofd.shop for *.stamhoofd.shop
     api: string; // requires wildcard prefix DNS
     rendererApi: string;
-    sgvLoginUrl?: string;
-    sgvAdminUrl?: string;
+
+    // Scouts & Gidsen Vlaanderen
+    sgvLoginUrl: string | undefined;
+    sgvAdminUrl: string | undefined;
 
     // MX + SPF (both for email) + A record for webshops
-    webshopCname?: string;
+    webshopCname: string | undefined;
 
     // MX + SPF (both for email) + A record for registration
     registrationCname: LocalizedDomain;
 
     // Default email domain for emails sent from organizations
-    defaultTransactionalEmail?: LocalizedDomain;
-    defaultBroadcastEmail?: LocalizedDomain;
+    defaultTransactionalEmail: LocalizedDomain | undefined;
+    defaultBroadcastEmail: LocalizedDomain | undefined;
 };
