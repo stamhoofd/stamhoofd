66155e6c65841203a23c98bbce37ab2c617ac4d1
Benjamin De Letter <benjamin@stamhoofd.be>
2026-06-25 09:15:20 +0200

    redirect registration to dashboard if no members package

--- frontend/app/dashboard/src/views/login/NoPermissionsView.vue
+++ frontend/app/dashboard/src/views/login/NoPermissionsView.vue
@@ -4,14 +4,16 @@
 
         <main class="center">
             <h1>{{ $t('%WC') }}</h1>
-            <p>{{ $t('%WD') }} {{ organization.name }}{{ $t('%WE') }}</p>
+            <p>{{ $t('%WD') }} {{ organization.name }}{{ membersPackage ? $t('%WE') : '.' }}</p>
         </main>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { useMembersPackage } from '@stamhoofd/components/hooks/useMembersPackage';
 import { useRequiredOrganization } from '@stamhoofd/components/hooks/useOrganization.ts';
 import STNavigationBar from '@stamhoofd/components/navigation/STNavigationBar.vue';
 
 const organization = useRequiredOrganization();
+const membersPackage = useMembersPackage();
 </script>
