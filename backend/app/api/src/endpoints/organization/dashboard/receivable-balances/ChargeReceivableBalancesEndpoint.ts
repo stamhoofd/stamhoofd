1126df919d2c4f4993d786a7256f482b2bb1e0e8
Simon Backx <simon@stamhoofd.be>
2026-07-05 18:22:44 +0200

    🐛 Reduced concurrency issues while creating charges for receivable balances

--- backend/app/api/src/endpoints/organization/dashboard/receivable-balances/ChargeReceivableBalancesEndpoint.ts
+++ backend/app/api/src/endpoints/organization/dashboard/receivable-balances/ChargeReceivableBalancesEndpoint.ts
@@ -12,6 +12,7 @@ import { PaymentService } from '../../../../services/PaymentService.js';
 import { SimpleError } from '@simonbackx/simple-errors';
 import { PaymentMandateService } from '../../../../services/PaymentMandateService.js';
 import { BalanceItemService } from '../../../../services/BalanceItemService.js';
+import { QueueHandler } from '@stamhoofd/queues';
 
 type Params = Record<string, never>;
 type Query = CountFilteredRequest;
@@ -37,98 +38,116 @@ export class ChargeReceivableBalancesEndpoint extends Endpoint<Params, Query, Bo
     async handle(request: DecodedRequest<Params, Query, Body>) {
         const sellingOrganization = await Context.setOrganizationScope();
         const { user } = await Context.authenticate();
-        const query = await GetReceivableBalancesEndpoint.buildQuery(request.query);
 
-        for await (const cachedBalance of query.all()) {
-            if (cachedBalance.organizationId !== sellingOrganization.id) {
-                throw new SimpleError({
-                    code: 'wrong_organization',
-                    message: 'Cannot charge a cached balance from a different organization',
-                });
-            }
-            // todo
-            const items = await CachedBalance.balanceForObjects(cachedBalance.organizationId, [cachedBalance.objectId], cachedBalance.objectType);
+        if (!await Context.auth.canManageFinances(sellingOrganization.id)) {
+            throw Context.auth.error();
+        }
+        const queueId = 'charge-receivable-balances/' + sellingOrganization.id;
 
-            if (items.length === 0) {
-                console.log('Nothing to charge for', cachedBalance.id);
-                continue;
-            }
-
-            const map: Map<BalanceItem, number> = new Map();
-            let total = 0;
-            for (const i of items) {
-                map.set(i, i.priceOpen);
-                total += i.priceOpen;
-            }
-            if (total <= 0) {
-                continue;
-            }
-
-            let payingOrganization: Organization | null = null;
-            let customerUser: User | null = null;
-            if (cachedBalance.objectType === ReceivableBalanceType.organization) {
-                const p = await Organization.getByID(cachedBalance.objectId);
-                if (!p || !(await Context.auth.hasFullAccess(p))) {
-                    console.error('Unexpected missing paying organization id', cachedBalance);
-                    continue;
-                }
-                payingOrganization = p;
-            }
-
-            if (cachedBalance.objectType === ReceivableBalanceType.user || cachedBalance.objectType === ReceivableBalanceType.userWithoutMembers) {
-                const p = await User.getByID(cachedBalance.objectId);
-                if (!p || !(Context.auth.checkScope(p.organizationId))) {
-                    console.error('Unexpected missing customer user id', cachedBalance);
-                    continue;
-                }
-                customerUser = p;
-            }
-
-            const mandates = await PaymentMandateService.getMandates({
-                sellingOrganization,
-                user: customerUser,
-                payingOrganization,
-            });
-
-            const mandate = mandates.find(m => m.isDefault);
-
-            if (!mandate) {
-                // Not possible
-                console.error('No mandates found for', cachedBalance.id);
-                continue;
-            }
-
-            customerUser = customerUser ?? (payingOrganization ? (await payingOrganization.getFullAdmins())[0] : null);
-            const customer = PaymentCustomer.create({
-                firstName: customerUser?.firstName,
-                lastName: customerUser?.lastName,
-                email: customerUser?.email ?? (payingOrganization ? (await payingOrganization.getReplyEmails())[0].email : null),
-                company: payingOrganization?.defaultCompanies[0],
-            });
-
-            await PaymentService.createPayment({
-                balanceItems: map,
-                checkout: {
-                    paymentMethod: PaymentMethod.Unknown,
-                    totalPrice: null,
-                    customer,
-                    cancelUrl: null,
-                    redirectUrl: null,
-                },
-                user: customerUser,
-                adminUserId: user.id,
-                organization: sellingOrganization,
-                payingOrganization,
-                serviceFeeType: 'system',
-                createMandate: null,
-                useMandate: mandate,
-                paymentConfiguration: sellingOrganization.meta.registrationPaymentConfiguration,
-                privatePaymentConfiguration: sellingOrganization.privateMeta.registrationPaymentConfiguration,
+        if (QueueHandler.isRunning(queueId)) {
+            throw new SimpleError({
+                code: 'charge_pending',
+                message: 'Already pending charge',
+                human: $t('Er is al een aanrekening bezig, even geduld voor je een tweede aanrekening aanrekend.'),
             });
         }
 
-        // Make sure the user can refresh data and see the updated cached amounts
-        await BalanceItemService.flushCaches(sellingOrganization.id);
+        await QueueHandler.schedule(queueId, async () => {
+            const query = await GetReceivableBalancesEndpoint.buildQuery(request.query);
+
+            for await (const cachedBalance of query.all()) {
+                if (cachedBalance.organizationId !== sellingOrganization.id) {
+                    throw new SimpleError({
+                        code: 'wrong_organization',
+                        message: 'Cannot charge a cached balance from a different organization',
+                    });
+                }
+                // todo
+                const items = await CachedBalance.balanceForObjects(cachedBalance.organizationId, [cachedBalance.objectId], cachedBalance.objectType);
+
+                if (items.length === 0) {
+                    console.log('Nothing to charge for', cachedBalance.id);
+                    continue;
+                }
+
+                const map: Map<BalanceItem, number> = new Map();
+                let total = 0;
+                for (const i of items) {
+                    map.set(i, i.priceOpen);
+                    total += i.priceOpen;
+                }
+                if (total <= 0) {
+                    continue;
+                }
+
+                let payingOrganization: Organization | null = null;
+                let customerUser: User | null = null;
+                if (cachedBalance.objectType === ReceivableBalanceType.organization) {
+                    const p = await Organization.getByID(cachedBalance.objectId);
+                    if (!p || !(await Context.auth.hasFullAccess(p))) {
+                        console.error('Unexpected missing paying organization id', cachedBalance);
+                        continue;
+                    }
+                    payingOrganization = p;
+                }
+
+                if (cachedBalance.objectType === ReceivableBalanceType.user || cachedBalance.objectType === ReceivableBalanceType.userWithoutMembers) {
+                    const p = await User.getByID(cachedBalance.objectId);
+                    if (!p || !(Context.auth.checkScope(p.organizationId))) {
+                        console.error('Unexpected missing customer user id', cachedBalance);
+                        continue;
+                    }
+                    customerUser = p;
+                }
+
+                const mandates = await PaymentMandateService.getMandates({
+                    sellingOrganization,
+                    user: customerUser,
+                    payingOrganization,
+                });
+
+                const mandate = mandates.find(m => m.isDefault);
+
+                if (!mandate) {
+                // Not possible
+                    console.error('No mandates found for', cachedBalance.id);
+                    continue;
+                }
+
+                customerUser = customerUser ?? (payingOrganization ? (await payingOrganization.getFullAdmins())[0] : null);
+                const customer = PaymentCustomer.create({
+                    firstName: customerUser?.firstName,
+                    lastName: customerUser?.lastName,
+                    email: customerUser?.email ?? (payingOrganization ? (await payingOrganization.getReplyEmails())[0].email : null),
+                    company: payingOrganization?.defaultCompanies[0],
+                });
+
+                await PaymentService.createPayment({
+                    balanceItems: map,
+                    checkout: {
+                        paymentMethod: PaymentMethod.Unknown,
+                        totalPrice: null,
+                        customer,
+                        cancelUrl: null,
+                        redirectUrl: null,
+                    },
+                    user: customerUser,
+                    adminUserId: user.id,
+                    organization: sellingOrganization,
+                    payingOrganization,
+                    serviceFeeType: 'system',
+                    createMandate: null,
+                    useMandate: mandate,
+                    paymentConfiguration: sellingOrganization.meta.registrationPaymentConfiguration,
+                    privatePaymentConfiguration: sellingOrganization.privateMeta.registrationPaymentConfiguration,
+                });
+
+                // Clear cache while looping, to update the table while we are charging
+                // Make sure the user can refresh data and see the updated cached amounts
+                await BalanceItemService.flushCaches(sellingOrganization.id);
+            }
+        });
+
         return new Response(undefined, 201);
     }
 }
