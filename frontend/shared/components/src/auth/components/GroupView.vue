06ffdf723ba9d2d6cad4811e03c1724124abcf14
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-07-07 15:26:38 +0200

    ✨ Added menu options to GroupOverview
    fixes STA-1321

--- frontend/shared/components/src/auth/components/GroupView.vue
+++ frontend/shared/components/src/auth/components/GroupView.vue
@@ -24,50 +24,143 @@
 
             <p v-if="group.settings.description" class="style-description pre-wrap" v-text="group.settings.description" />
 
-            <STList class="group-info-list">
-                <STListItem class="right-description">
-                    {{ $t('%Vc') }}
-
-                    <template #right>
-                        <template v-if="group.settings.displayStartEndTime">
-                            {{ formatDateTime(group.settings.startDate) }} - {{ formatDateTime(group.settings.endDate) }}
+            <div class="container">
+                <STList>
+                    <STListItem>
+                        <template #left>
+                            <span class="icon event" />
                         </template>
-                        <template v-else>
-                            {{ formatDate(group.settings.startDate) }} - {{ formatDate(group.settings.endDate) }}
+                        <h2 class="style-title-list">
+                            <span v-if="group.settings.displayStartEndTime">{{ formatDateTime(group.settings.startDate) }} - {{ formatDateTime(group.settings.endDate) }}</span>
+                            <span v-else>{{ formatDate(group.settings.startDate) }} - {{ formatDate(group.settings.endDate) }}</span>
+                        </h2>
+                    </STListItem>
+                    <STListItem v-if="group.settings.location">
+                        <template #left>
+                            <span class="icon location" />
+                        </template>
+                        <h2 class="style-title-list">
+                            {{ group.settings.location }}
+                        </h2>
+                    </STListItem>
+                    <STListItem v-if="who">
+                        <template #left>
+                            <span class="icon filter" />
+                        </template>
+                        <h2 class="style-title-list">
+                            {{ who }}
+                        </h2>
+                    </STListItem>
+                    <template v-if="priceList.length === 1">
+                        <template v-for="(price, index) of priceList" :key="index">
+                            <STListItem class="right-stack">
+                                <template #left>
+                                    <span class="icon receive small" />
+                                </template>
+                                <h2 class="style-title-list">
+                                    {{ price.text }}
+                                </h2>
+                                <p v-if="price.description" class="style-description-small">
+                                    {{ price.description }}
+                                </p>
+                                <template #right>
+                                    <span>{{ price.price }}</span>
+                                </template>
+                            </STListItem>
+                            <STListItem v-if="price.reducedPrice !== null" class="right-stack">
+                                <template #left>
+                                    <span class="icon label small" />
+                                </template>
+                                <h2 class="style-title-list">
+                                    {{ $t('%1Vy') }}
+                                </h2>
+
+                                <p class="style-description-small">
+                                    {{ $t('%1cu') }}
+                                </p>
+                                <template #right>
+                                    <span>{{ price.reducedPrice }}</span>
+                                </template>
+                            </STListItem>
                         </template>
                     </template>
-                </STListItem>
-                <STListItem v-if="group.settings.location" class="right-description">
-                    {{ $t('%1Ye') }}
+                </STList>
+            </div>
 
-                    <template #right>
-                        {{ group.settings.location }}
+            <div v-if="priceList.length > 1" class="container">
+                <hr>
+                <h2>
+                    {{ $t('Tarieven') }}
+                </h2>
+                <STList>
+                    <template v-for="(price, index) of priceList" :key="index">
+                        <STListItem class="right-stack">
+                            <h2 class="style-title-list">
+                                {{ price.text }}
+                            </h2>
+                            <p v-if="price.description" class="style-description-small">
+                                {{ price.description }}
+                            </p>
+                            <template #right>
+                                <span>{{ price.price }}</span>
+                            </template>
+                        </STListItem>
+                        <STListItem v-if="price.reducedPrice !== null" class="right-stack">
+                            <h2 class="style-title-list">
+                                {{ price.text }} - {{ financialSupportSettings.priceName }}
+                            </h2>
+
+                            <p class="style-description-small">
+                                {{ $t('%1cu') }}
+                            </p>
+                            <template #right>
+                                <span>{{ price.reducedPrice }}</span>
+                            </template>
+                        </STListItem>
                     </template>
-                </STListItem>
-                <STListItem v-if="who" class="right-description wrap">
-                    {{ $t('%1Wm') }}
+                </STList>
+            </div>
 
-                    <template #right>
-                        <div v-text="who" />
-                    </template>
-                </STListItem>
+            <template v-for="menu of optionMenuList" :key="menu.id">
+                <hr>
+                <div class="container">
+                    <h2>{{ menu.name }}</h2>
 
-                <STListItem v-for="(price, index) of priceList" :key="index">
-                    <h3>{{ price.text }}</h3>
-                    <p v-if="price.description" class="style-description-small">
-                        {{ price.description }}
-                    </p>
+                    <STList>
+                        <template v-for="option of menu.optionList" :key="option.id">
+                            <STListItem class="right-stack">
+                                <h2 class="style-title-list">
+                                    {{ option.text }}
+                                </h2>
+                                <p v-if="option.description" class="style-description-small">
+                                    {{ option.description }}
+                                </p>
+                                <template v-if="menu.shouldShowPrices" #right>
+                                    <span>{{ option.price }}</span>
+                                </template>
+                            </STListItem>
+                            <STListItem v-if="menu.shouldShowPrices && option.reducedPrice !== null" class="right-stack">
+                                <h2 class="style-title-list">
+                                    {{ option.text }} - {{ financialSupportSettings.priceName }}
+                                </h2>
 
-                    <template #right>
-                        <div class="style-description" v-text="price.price" />
-                    </template>
-                </STListItem>
-            </STList>
+                                <p class="style-description-small">
+                                    {{ $t('%1cu') }}
+                                </p>
+                                <template #right>
+                                    <span>{{ option.reducedPrice }}</span>
+                                </template>
+                            </STListItem>
+                        </template>
+                    </STList>
+                </div>
+            </template>
         </main>
     </div>
 </template>
 
 <script setup lang="ts">
+import { useFinancialSupportSettings } from '#groups/hooks/useFinancialSupportSettings.ts';
 import { useOrganization } from '#hooks/useOrganization.ts';
 import STList from '#layout/STList.vue';
 import STListItem from '#layout/STListItem.vue';
@@ -85,6 +178,11 @@ const props = defineProps<{
 
 const organization = useOrganization();
 
+const { enabled: isFinancialSupportEnabled, financialSupportSettings } = useFinancialSupportSettings({
+    group: computed(() => props.group),
+    externalOrganization: organization,
+});
+
 function formatDateTime(date: Date) {
     return Formatter.dateTime(date);
 }
@@ -97,26 +195,78 @@ function groupName(id: string): string {
     return organization.value?.period.groups.find(g => g.id === id)?.settings.name.toString() ?? $t('%Gr');
 }
 
+function getStockText<T extends { getRemainingStock: (group: Group) => number | null }>(item: T): string {
+    const remainingStock = item.getRemainingStock(props.group);
+    if (remainingStock === null) {
+        return '';
+    }
+
+    if (remainingStock <= 0) {
+        return $t('%Um');
+    }
+
+    return remainingStock !== 1 ? $t('%1d9', { count: remainingStock }) : $t('Nog één plaats');
+}
+
 const priceList = computed(() => {
-    const list: { text: string; description: string; price: string }[] = [];
+    const list: { text: string; description: string; price: string; reducedPrice: string | null }[] = [];
 
     for (const price of props.group.settings.getFilteredPrices()) {
-        list.push({
-            text: price.name.toString() || $t('%1IP'),
-            description: '',
-            price: Formatter.price(price.price.price),
-        });
+        const remainingStockText = getStockText(price);
 
-        if (price.price.reducedPrice !== null && price.price.reducedPrice !== price.price.price) {
-            list.push({
-                text: $t('%1Vy'),
-                description: $t('%1cu'),
-                price: Formatter.price(price.price.reducedPrice),
-            });
-        }
+        list.push({
+            text: Formatter.capitalizeFirstLetter(price.name.toString()) || $t('%1IP'),
+            description: remainingStockText,
+            price: Formatter.price(price.price.price),
+            reducedPrice: isFinancialSupportEnabled.value && price.price.reducedPrice !== null && price.price.reducedPrice !== price.price.price ? Formatter.price(price.price.reducedPrice) : null,
+        });
     }
 
     return list;
+});
+
+const optionMenuList = computed(() => {
+    const menuList: { id: string; name: string; shouldShowPrices: boolean; optionList: { id: string; text: string; description: string; price: string; reducedPrice: string | null }[] }[] = [];
+
+    for (const optionMenu of props.group.settings.optionMenus) {
+        const optionList: { id: string; text: string; description: string; price: string; reducedPrice: string | null }[] = [];
+
+        const visibleOptions = optionMenu.options.filter(o => !o.hidden);
+
+        // only show prices if at least one option has a price
+        const shouldShowPrices = visibleOptions.some(o => o.price.price !== 0 || (o.price.reducedPrice !== null && o.price.reducedPrice !== 0));
+
+        // only show if at least one option is relevant (= has a price or a stock)
+        if (shouldShowPrices || visibleOptions.some(o => o.getRemainingStock(props.group) !== null)) {
+            for (const option of visibleOptions) {
+                if (option.hidden) {
+                    continue;
+                }
+
+                const remainingStockText = getStockText(option);
+
+                optionList.push({
+                    id: option.id,
+                    text: option.name.toString(),
+                    description: remainingStockText,
+                    price: Formatter.price(option.price.price),
+                    reducedPrice: isFinancialSupportEnabled.value && option.price.reducedPrice !== null && option.price.reducedPrice !== option.price.price ? Formatter.price(option.price.reducedPrice) : null,
+
+                });
+            }
+
+            if (optionList.length) {
+                menuList.push({
+                    id: optionMenu.id,
+                    name: optionMenu.name.toString(),
+                    shouldShowPrices,
+                    optionList,
+                });
+            }
+        }
+    }
+
+    return menuList;
 });
 
 const infoBox = computed(() => {
