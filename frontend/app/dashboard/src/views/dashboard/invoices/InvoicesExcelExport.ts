74f21c26803833990c68ef8e9d7bebec2233d0b9
Simon Backx <simon@stamhoofd.be>
2026-07-08 15:32:07 +0200

    ✨ Updated invoices Excel export with settlement columns

--- frontend/app/dashboard/src/views/dashboard/invoices/InvoicesExcelExport.ts
+++ frontend/app/dashboard/src/views/dashboard/invoices/InvoicesExcelExport.ts
@@ -1,4 +1,5 @@
 import type { InvoiceStruct } from '@stamhoofd/structures';
+import { PaymentMethod, PaymentMethodHelper } from '@stamhoofd/structures';
 import { Formatter } from '@stamhoofd/utility';
 import XLSX from 'xlsx';
 import type { RowValue } from '../../../classes/ExcelHelper';
@@ -10,7 +11,7 @@ export class InvoicesExcelExport {
      */
     static createSheet(invoices: InvoiceStruct[]): XLSX.WorkSheet {
         /// Should we repeat all the duplicate fields for multiple lines in an order?
-        
+
         // Columns
         const wsData: RowValue[][] = [
             [
@@ -24,15 +25,17 @@ export class InvoicesExcelExport {
                 'BTW',
                 'Afrondingsverschil',
                 'Totaal',
+                'Betaling',
+                'Betaalmethode',
                 'Uitbetalingsdatum',
                 'Uitbetalingsmededeling',
-                'Uitbetalingsbedrag'
+                'Uitbetalingsbedrag',
             ],
         ];
 
         for (const invoice of invoices) {
-            wsData.push([
-                (invoice.number?? '/')+'',
+            const invoiceColumns: RowValue[] = [
+                (invoice.number ?? '/') + '',
                 invoice.invoicedAt ? Formatter.dateIso(invoice.invoicedAt) : '/',
                 invoice.customer.dynamicName,
                 invoice.customer.company?.companyNumber ?? '',
@@ -54,15 +57,40 @@ export class InvoicesExcelExport {
                     value: invoice.totalWithVAT / 100_00,
                     format: '€0.00',
                 },
+            ];
+            const emptyInvoiceColumns: RowValue[] = invoiceColumns.map(() => '');
 
-                // todo (can be more than one date)
-                'Uitbetalingsdatum',
-                'Uitbetalingsmededeling',
-                'Uitbetalingsbedrag'
-            ]);
+            if (invoice.payments.length === 0) {
+                wsData.push([
+                    ...invoiceColumns,
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                    '/',
+                ]);
+                continue;
+            }
+
+            for (const [index, payment] of invoice.payments.entries()) {
+                wsData.push([
+                    ...(index === 0 ? invoiceColumns : emptyInvoiceColumns),
+                    {
+                        value: payment.price / 100_00,
+                        format: '€0.00',
+                    },
+                    PaymentMethodHelper.getNameCapitalized(payment.method),
+                    payment.method === PaymentMethod.Transfer ? 'Overschrijving' : (payment.settlement?.settledAt ? Formatter.dateIso(payment.settlement.settledAt) : '/'),
+                    payment.method === PaymentMethod.Transfer ? (payment.transferDescription ?? '/') : (payment.settlement?.reference ?? '/'),
+                    {
+                        value: payment.settlement?.amount ? payment.settlement.amount / 100_00 : 0,
+                        format: '€0.00',
+                    },
+                ]);
+            }
         }
 
-       return ExcelHelper.buildWorksheet(wsData, {
+        return ExcelHelper.buildWorksheet(wsData, {
             defaultColumnWidth: 13,
         });
     }
@@ -71,6 +99,6 @@ export class InvoicesExcelExport {
         const wb = XLSX.utils.book_new();
         /* Add the worksheet to the workbook */
         XLSX.utils.book_append_sheet(wb, this.createSheet(invoices), 'Facturen');
-        return XLSX.write(wb, { type: 'array' }) as Promise<ArrayBuffer>
+        return XLSX.write(wb, { type: 'array' }) as Promise<ArrayBuffer>;
     }
 }
