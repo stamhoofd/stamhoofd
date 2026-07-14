018e87253725d976e17dc94938d74d82052d6fd2
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-07-14 22:35:09 +0200

    Refactored code with switch

--- frontend/shared/components/src/filters/filter-builders/record-categories.ts
+++ frontend/shared/components/src/filters/filter-builders/record-categories.ts
@@ -15,121 +15,127 @@ export function getFilterBuildersForRecordCategories(categories: RecordCategory[
         const categoryPrefix = category.name + ' → ';
 
         for (const record of category.records) {
-            if (record.type === RecordType.Text || record.type === RecordType.Textarea) {
-                allForCategory.push(
-                    new StringFilterBuilder({
-                        name: prefix + categoryPrefix + record.name,
-                        key: 'value',
-                        wrapper: {
-                            recordAnswers: {
-                                [record.id]: FilterWrapperMarker,
-                            },
-                        },
-                    }),
-                );
-            }
-
-            if (record.type === RecordType.Integer || record.type === RecordType.Price) {
-                allForCategory.push(
-                    new NumberFilterBuilder({
-                        name: prefix + categoryPrefix + record.name,
-                        key: 'value',
-                        type: record.type === RecordType.Price ? NumberFilterFormat.Currency : NumberFilterFormat.Number,
-                        wrapper: {
-                            recordAnswers: {
-                                [record.id]: FilterWrapperMarker,
-                            },
-                        },
-                    }),
-                );
-            }
-
-            if (record.type === RecordType.Checkbox) {
-                const extra = options?.includeNullable
-                    ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
-                    : [];
-                allForCategory.push(
-                    new MultipleChoiceFilterBuilder({
-                        name: prefix + categoryPrefix + record.name,
-                        options: [
-                            ...extra,
-                            new MultipleChoiceUIFilterOption($t('%BM'), true),
-                            new MultipleChoiceUIFilterOption($t('%BN'), false),
-                        ],
-                        wrapper: {
-                            recordAnswers: {
-                                [record.id]: {
-                                    selected: { $in: FilterWrapperMarker },
+            switch (record.type) {
+                case RecordType.Text:
+                case RecordType.Textarea:
+                {
+                    allForCategory.push(
+                        new StringFilterBuilder({
+                            name: prefix + categoryPrefix + record.name,
+                            key: 'value',
+                            wrapper: {
+                                recordAnswers: {
+                                    [record.id]: FilterWrapperMarker,
                                 },
                             },
-                        },
-                    }),
-                );
-            }
+                        }),
+                    );
+                    break;
+                }
+                case RecordType.Integer:
+                case RecordType.Price: {
+                    allForCategory.push(
+                        new NumberFilterBuilder({
+                            name: prefix + categoryPrefix + record.name,
+                            key: 'value',
+                            type: record.type === RecordType.Price ? NumberFilterFormat.Currency : NumberFilterFormat.Number,
+                            wrapper: {
+                                recordAnswers: {
+                                    [record.id]: FilterWrapperMarker,
+                                },
+                            },
+                        }),
+                    );
+                    break;
+                }
+                case RecordType.Checkbox: {
+                    const extra = options?.includeNullable
+                        ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
+                        : [];
+                    allForCategory.push(
+                        new MultipleChoiceFilterBuilder({
+                            name: prefix + categoryPrefix + record.name,
+                            options: [
+                                ...extra,
+                                new MultipleChoiceUIFilterOption($t('%BM'), true),
+                                new MultipleChoiceUIFilterOption($t('%BN'), false),
+                            ],
+                            wrapper: {
+                                recordAnswers: {
+                                    [record.id]: {
+                                        selected: { $in: FilterWrapperMarker },
+                                    },
+                                },
+                            },
+                        }),
+                    );
+                    break;
+                }
+                case RecordType.ChooseOne: {
+                    const extra = options?.includeNullable
+                        ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
+                        : [];
 
-            if (record.type === RecordType.ChooseOne) {
-                const extra = options?.includeNullable
-                    ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
-                    : [];
-
-                allForCategory.push(
-                    new MultipleChoiceFilterBuilder({
-                        name: prefix + categoryPrefix + record.name,
-                        options: [
-                            ...extra,
-                            ...record.choices.map(c => new MultipleChoiceUIFilterOption(c.name.toString(), c.id)),
-                        ],
-                        wrapper: {
-                            recordAnswers: {
-                                [record.id]: {
-                                    selectedChoice: {
-                                        id: {
-                                            $in: FilterWrapperMarker,
+                    allForCategory.push(
+                        new MultipleChoiceFilterBuilder({
+                            name: prefix + categoryPrefix + record.name,
+                            options: [
+                                ...extra,
+                                ...record.choices.map(c => new MultipleChoiceUIFilterOption(c.name.toString(), c.id)),
+                            ],
+                            wrapper: {
+                                recordAnswers: {
+                                    [record.id]: {
+                                        selectedChoice: {
+                                            id: {
+                                                $in: FilterWrapperMarker,
+                                            },
                                         },
                                     },
                                 },
                             },
-                        },
-                        additionalUnwrappers: [
-                            { recordAnswers: {
-                                [record.id]: {
-                                    selectedChoices: {
-                                        id: {
-                                            $in: FilterWrapperMarker,
+                            additionalUnwrappers: [
+                                { recordAnswers: {
+                                    [record.id]: {
+                                        selectedChoices: {
+                                            id: {
+                                                $in: FilterWrapperMarker,
+                                            },
                                         },
                                     },
-                                },
-                            } },
-                        ],
-                    }),
-                );
-            }
+                                } },
+                            ],
+                        }),
+                    );
+                    break;
+                }
+                case RecordType.MultipleChoice: {
+                    const extra = options?.includeNullable
+                        ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
+                        : [];
 
-            if (record.type === RecordType.MultipleChoice) {
-                const extra = options?.includeNullable
-                    ? [new MultipleChoiceUIFilterOption($t('%1CJ'), null)]
-                    : [];
-
-                allForCategory.push(
-                    new MultipleChoiceFilterBuilder({
-                        name: prefix + categoryPrefix + record.name,
-                        options: [
-                            ...extra,
-                            ...record.choices.map(c => new MultipleChoiceUIFilterOption(c.name.toString(), c.id)),
-                        ],
-                        wrapper: {
-                            recordAnswers: {
-                                [record.id]: {
-                                    selectedChoices: {
-                                        id: {
-                                            $in: FilterWrapperMarker,
+                    allForCategory.push(
+                        new MultipleChoiceFilterBuilder({
+                            name: prefix + categoryPrefix + record.name,
+                            options: [
+                                ...extra,
+                                ...record.choices.map(c => new MultipleChoiceUIFilterOption(c.name.toString(), c.id)),
+                            ],
+                            wrapper: {
+                                recordAnswers: {
+                                    [record.id]: {
+                                        selectedChoices: {
+                                            id: {
+                                                $in: FilterWrapperMarker,
+                                            },
                                         },
                                     },
                                 },
                             },
-                        },
-                    }),
-                );
+                        }),
+                    );
+                    break;
+                }
             }
         }
 
