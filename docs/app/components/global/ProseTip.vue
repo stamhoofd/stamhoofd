693131f871fa247bc5c28b442c9d9c309ac92e9a
Benjamin De Letter <benjamin@stamhoofd.be>
2026-07-16 15:14:39 +0200

    Use stamhoofd boxes for notes, tips, cautions and warnings

---
+++ docs/app/components/global/ProseTip.vue
@@ -0,0 +1,7 @@
+<!-- Overrides @nuxt/ui's ::tip callout. Must stay in components/global/: MDC resolves
+     components at runtime and only sees globally registered ones. -->
+<template>
+    <div class="success-box">
+        <slot mdc-unwrap="p" />
+    </div>
+</template>
