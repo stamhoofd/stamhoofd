307d82ab4bc7407be7615adeca82ca60d9c0d8c2
Benjamin De Letter <benjamin@stamhoofd.be>
2026-07-17 13:52:43 +0200

    Override sidebar to allow collapsable table of contents

---
+++ docs/app/components/docs/DocsAsideLeftBody.vue
@@ -0,0 +1,16 @@
+<template>
+    <UContentNavigation
+        :collapsible="true"
+        :default-open="false"
+        :highlight="contentNavVariants.highlight ?? true"
+        :highlight-color="contentNavVariants.highlightColor"
+        :variant="contentNavVariants.variant ?? 'link'"
+        :color="contentNavVariants.color"
+        :navigation="sidebarNavigation"
+    />
+</template>
+<script setup lang="ts">
+const { sidebarNavigation } = useSubNavigation();
+
+const contentNavVariants = useUIConfig('contentNavigation');
+</script>
