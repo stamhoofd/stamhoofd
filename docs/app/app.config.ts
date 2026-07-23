7c1e493b9854a7b410edec81a94411a5e8873ae3
Benjamin De Letter <benjamin@stamhoofd.be>
2026-07-23 11:09:24 +0200

    add stamhoofd branding to docs

---
+++ docs/app/app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+    header: {
+        title: 'Stamhoofd',
+        logo: {
+            alt: 'Stamhoofd',
+            light: '/logo-horizontal.svg',
+            dark: '/logo-horizontal-dark.svg',
+            favicon: '/favicon.svg',
+        },
+    },
+});
