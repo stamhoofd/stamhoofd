825212799a19b3e32bc576855ebd8d4c577523f1
Tijl Schipper <tijl.schipper@gmail.com>
2026-08-26 13:23:50 +0200

    ✨ Added filter for groupPrice (#764)
    fixes STA-1386
    
    Signed-off-by: Tijl Schipper <tijl.schipper@gmail.com>

--- backend/app/api/src/sql-filters/registrations.ts
+++ backend/app/api/src/sql-filters/registrations.ts
@@ -55,6 +55,14 @@ export const registrationFilterCompilers: SQLFilterDefinitions = {
             }),
         },
     ),
+    groupPrice: {
+        ...baseSQLFilterCompilers,
+        id: createColumnFilter({
+            expression: SQL.jsonExtract(SQL.column('groupPrice'), '$.value.id'),
+            type: SQLValueType.JSONString,
+            nullable: false,
+        }),
+    },
     /**
      * There is a bug in MySQL 8 that is fixed in 9.3
      * where EXISTS (select * from json_table(...)) does not work
