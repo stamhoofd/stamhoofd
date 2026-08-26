bfbbf4cfdd26ae7ec53ffd239cd8a7d22044fcf5
Benjamin De Letter <benjamin@stamhoofd.be>
2026-08-26 12:40:45 +0200

    ✨ Allow roles to access registrations of any period

--- backend/app/api/src/helpers/AdminPermissionChecker.ts
+++ backend/app/api/src/helpers/AdminPermissionChecker.ts
@@ -495,7 +495,6 @@ export class AdminPermissionChecker {
         }
 
         if (organizationPermissions.hasAccess(PermissionLevel.Full)) {
-            // Only full permissions; because non-full doesn't have access to other periods
             return true;
         }
 
@@ -509,16 +508,6 @@ export class AdminPermissionChecker {
             // No full access: cannot access deactivated registrations
             if (permissionLevel !== PermissionLevel.Read) {
                 // Not allowed to edit registrations that are deleted
-                return false;
-            }
-        }
-
-        const organization = await this.getOrganization(registration.organizationId);
-
-        if (registration.periodId !== organization.periodId) {
-            if (STAMHOOFD.userMode === 'organization' || registration.periodId !== this.platform.period.id) {
-                // We already checked for full permissions - and we don't have full permissions
-                // so that also means no permissions for registrations in other periods
                 return false;
             }
         }
