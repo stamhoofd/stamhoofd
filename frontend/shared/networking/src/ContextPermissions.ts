3ca9c05783debee66ed75b6b5ac644ac6eb48866
Benjamin De Letter <benjamin@stamhoofd.be>
2026-09-07 18:32:34 +0200

    Let grants that are not limited to the current period reach any period

--- frontend/shared/networking/src/ContextPermissions.ts
+++ frontend/shared/networking/src/ContextPermissions.ts
@@ -154,6 +154,14 @@ export class ContextPermissions {
         return this.hasAccessRight(AccessRight.ManageEmailTemplates);
     }
 
+    isPeriodInUse(periodId: string, organization: Organization): boolean {
+        if (periodId === organization.period.period.id) {
+            return true;
+        }
+
+        return STAMHOOFD.userMode !== 'organization' && periodId === this.platform.period.id;
+    }
+
     canAccessGroup(group: Group, permissionLevel: PermissionLevel = PermissionLevel.Read, organization?: Organization | null) {
         if (organization === undefined || (organization === null && this.organization)) {
             organization = this.organization;
@@ -163,17 +171,19 @@ export class ContextPermissions {
             return this.hasFullPlatformAccess();
         }
 
-        if (group.periodId !== organization.period.period.id) {
-            if (STAMHOOFD.userMode === 'organization' || group.periodId !== this.platform.period.id) {
-                if (!this.hasFullAccess()) {
-                    return false;
-                }
-            }
-        }
-
-        const permissions = this.getPermissionsForOrganization(organization);
+        let permissions = this.getPermissionsForOrganization(organization);
         if (!permissions) {
             return false;
+        }
+
+        const isPeriodInUse = this.isPeriodInUse(group.periodId, organization);
+
+        if (!isPeriodInUse) {
+            permissions = permissions.clone();
+
+            for (const resources of permissions.resources.values()) {
+                resources.delete(PermissionsResourceKey.CurrentPeriod);
+            }
         }
 
         if (permissions.hasResourceAccess(PermissionsResourceType.Groups, group.id, permissionLevel)) {
@@ -190,12 +200,12 @@ export class ContextPermissions {
             }
         }
 
-        if (group.type === GroupType.EventRegistration && group.event && group.event.organizationId === organization.id) {
+        if (isPeriodInUse && group.type === GroupType.EventRegistration && group.event && group.event.organizationId === organization.id) {
             // we'll need to check the event permissions
             return this.canWriteEventForOrganization(group.event, organization);
         }
 
-        if (group.type === GroupType.WaitingList && group.parentGroup && group.parentGroup.type === GroupType.EventRegistration && group.parentGroup.event && group.parentGroup.event.organizationId === organization.id) {
+        if (isPeriodInUse && group.type === GroupType.WaitingList && group.parentGroup && group.parentGroup.type === GroupType.EventRegistration && group.parentGroup.event && group.parentGroup.event.organizationId === organization.id) {
             // we'll need to check the event permissions
             return this.canWriteEventForOrganization(group.parentGroup.event, organization);
         }
