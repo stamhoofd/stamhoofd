0646f72610fcdd9fd9c97e70b45cb9d3974b1bc7
Simon Backx <simon@stamhoofd.be>
2026-09-14 17:22:33 +0200

    🔥 Dropped old seeds

--- backend/app/api/src/seeds/0000000001-throw.ts
+++
@@ -1,5 +0,0 @@
-import { Migration } from '@simonbackx/simple-database';
-
-export default new Migration(async () => {
-    // throw new Error('Stop here')
-});
--- backend/app/api/src/seeds/1783097277-update-member-last-registered.sql
+++
@@ -1,6 +0,0 @@
-UPDATE `members` m
-SET m.lastRegisteredAt = (
-    SELECT MAX(r.registeredAt)
-    FROM registrations r
-    WHERE r.memberId = m.id
-);
