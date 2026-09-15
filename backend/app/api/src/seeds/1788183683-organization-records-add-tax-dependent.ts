9e9593e30dc0debdf15dc5ec6eb91ce1531a0b42
Bjarne <77971615+BjarneBackx@users.noreply.github.com>
2026-09-15 15:13:44 +0200

    Updated seed for keeo and ravot

--- backend/app/api/src/seeds/1788183683-organization-records-add-tax-dependent.ts
+++ backend/app/api/src/seeds/1788183683-organization-records-add-tax-dependent.ts
@@ -1,10 +1,26 @@
 import { Migration } from '@simonbackx/simple-database';
+import { Organization, Platform } from '@stamhoofd/models';
 import { SeedTools } from '../helpers/SeedTools.js';
-import { Organization } from '@stamhoofd/models';
 
 export default new Migration(async () => {
     if (STAMHOOFD.environment === 'test') {
         console.log('skipped in tests');
+        return;
+    }
+
+    // for these platforms the data for tax certificates should always be on
+    if (['keeo', 'ravot'].includes(STAMHOOFD.platformName.toLowerCase())) {
+        const platform = await Platform.getForEditing();
+        platform.config.recordsConfiguration.taxDependent = true;
+        await platform.save();
+        await Platform.clearCache();
+
+        console.log('Enabled tax certificate data on the platform: ' + STAMHOOFD.platformName);
+        return;
+    }
+
+    if (STAMHOOFD.userMode === 'platform') {
+        console.log('Skipped seed organization-records-add-tax-dependent for platform: ' + STAMHOOFD.platformName);
         return;
     }
 
